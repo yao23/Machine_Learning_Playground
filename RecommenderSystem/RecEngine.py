3b5f7bb4da078bc5210d400b9ea95164fea910fa
yao <myspiritcrazy@gmail.com>
2017-12-01 12:23:44 -0800

    refactor code to provide recommendations

--- RecommenderSystem/RecEngine.py
+++ RecommenderSystem/RecEngine.py
@@ -4,6 +4,9 @@ from ModelStore import ModelStore
 import logging
 
 from DatabaseInterface import DatabaseInterface
+from Models.SimilarItemModel import SimilarItemModel
+from Models.CFmodel import CFmodel
+from Models.MostPopularModel import MostPopularModel
 
 class RecEngine(object):
 	logging.basicConfig(level=logging.INFO)
@@ -14,13 +17,22 @@ class RecEngine(object):
 		self.userActivityTable = userActivityTable
 
 	def provideRecommendation(self, request):
+		recommendations = []
 		[userType, userId, request] = self.userAnalyzer.analyze(request, userActivityDB=DatabaseInterface.dbTable[
 			'user_activity'])
 		if userType == "anonymous":
 			print "anonymous user: provide popular movies"
+			recommendations.append(MostPopularModel(self.modelStore.getModel(ModelStore.MP_MODEL_KEY)).provideRec())
 		elif userType == "new":
 			print "new registered user: cold start"
+			recommendations.append(MostPopularModel(self.modelStore.getModel(ModelStore.MP_MODEL_KEY)).provideRec())
+			recommendations.append(SimilarItemModel(self.modelStore.getModel(ModelStore.SI_MODEL_KEY)).provideRec())
 		else:
 			print "old user: recommend based on history"
+			recommendations = []
+			recommendations.append(MostPopularModel(self.modelStore.getModel(ModelStore.MP_MODEL_KEY)).provideRec())
+			recommendations.append(SimilarItemModel(self.modelStore.getModel(ModelStore.SI_MODEL_KEY)).provideRec())
+			recommendations.append(CFmodel(self.modelStore.getModel(ModelStore.CF_MODEL_KEY)).provideRec(request.userId))
+		return recommendations
 
 
