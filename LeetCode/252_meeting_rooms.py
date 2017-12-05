33125c54f9d0322cfb4549c5f24867af350d896a
yao <myspiritcrazy@gmail.com>
2017-12-05 08:15:19 -0800

    Meeting Rooms

---
+++ LeetCode/252_meeting_rooms.py
@@ -0,0 +1,20 @@
+# Definition for an interval.
+# class Interval(object):
+#     def __init__(self, s=0, e=0):
+#         self.start = s
+#         self.end = e
+
+class Solution(object):
+    # beats 42.09%
+    def canAttendMeetings(self, intervals):
+        """
+        :type intervals: List[Interval]
+        :rtype: bool
+        """
+        intervals.sort(key=lambda x: x.start)
+
+        for i in range(1, len(intervals)):
+            if intervals[i].start < intervals[i - 1].end:
+                return False
+
+        return True
