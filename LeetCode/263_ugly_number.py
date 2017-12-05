ccf29f11e8383891cb786256509619e1dbdb8291
yao <myspiritcrazy@gmail.com>
2017-12-05 08:24:13 -0800

    Ugly Number

---
+++ LeetCode/263_ugly_number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    # beats 38.32%
+    def isUgly(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        if num <= 0:
+            return False
+        for x in [2, 3, 5]:
+            while num % x == 0:
+                num = num / x
+        return num == 1
