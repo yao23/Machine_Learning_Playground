d3099a1102bcbc8857be6f2e3580ad46d103aa16
yao <myspiritcrazy@gmail.com>
2017-12-05 10:12:06 -0800

    Create Maximum Number

---
+++ LeetCode/321_create_maximum_number.py
@@ -0,0 +1,25 @@
+class Solution(object):
+    # beats 67.26%
+    def maxNumber(self, nums1, nums2, k):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :type k: int
+        :rtype: List[int]
+        """
+        def prep(nums, k):
+            drop = len(nums) - k
+            out = []
+            for num in nums:
+                while drop and out and out[-1] < num:
+                    out.pop()
+                    drop -= 1
+                out.append(num)
+            return out[:k]
+
+        def merge(a, b):
+            return [max(a, b).pop(0) for _ in a+b]
+
+        return max(merge(prep(nums1, i), prep(nums2, k-i))
+                   for i in range(k+1)
+                   if i <= len(nums1) and k-i <= len(nums2))
