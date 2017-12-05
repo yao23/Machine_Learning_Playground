df4dfe1d268033fee89383c10864ac2c4403cd6b
yao <myspiritcrazy@gmail.com>
2017-12-05 07:16:43 -0800

    Number of Digit One

---
+++ LeetCode/233_number_of_digit_one.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    # beats 27.91%
+    def countDigitOne(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n <= 0:
+            return 0
+        q, x, ans = n, 1, 0
+        while q > 0:
+            digit = q % 10
+            q /= 10
+            ans += q * x
+            if digit == 1:
+                ans += n % x + 1
+            elif digit > 1:
+                ans += x
+            x *= 10
+        return ans
