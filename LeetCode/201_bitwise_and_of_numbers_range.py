aedffdaba8d60c42184774ea765e203169a8766a
yao <myspiritcrazy@gmail.com>
2017-12-04 23:11:58 -0800

    Bitwise AND of Numbers Range

---
+++ LeetCode/201_bitwise_and_of_numbers_range.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def rangeBitwiseAnd(self, m, n):
+        """
+        :type m: int
+        :type n: int
+        :rtype: int
+        """
+        # beats 62.55%
+        i = 0
+        while m != n:
+            m >>= 1
+            n >>= 1
+            i += 1
+        return n << i
