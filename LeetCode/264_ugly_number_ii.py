99ee146e5eb22a6b2bd92ebd08bba3922981ac18
yao <myspiritcrazy@gmail.com>
2017-12-05 08:25:39 -0800

    Ugly Number II

---
+++ LeetCode/264_ugly_number_ii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    # beats 81.39%
+    def nthUglyNumber(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        ugly = [1]
+        i2, i3, i5 = 0, 0, 0
+        while n > 1:
+            u2, u3, u5 = 2 * ugly[i2], 3 * ugly[i3], 5 * ugly[i5]
+            umin = min((u2, u3, u5))
+            if umin == u2:
+                i2 += 1
+            if umin == u3:
+                i3 += 1
+            if umin == u5:
+                i5 += 1
+            ugly.append(umin)
+            n -= 1
+        return ugly[-1]
