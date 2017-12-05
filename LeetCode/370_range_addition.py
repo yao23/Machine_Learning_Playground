b0c91767dc198856136abba2c97f3a90101a23df
yao <myspiritcrazy@gmail.com>
2017-12-05 11:06:45 -0800

    Range Addition

---
+++ LeetCode/370_range_addition.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    # beats 18.26%
+    def getModifiedArray(self, length, updates):
+        """
+        :type length: int
+        :type updates: List[List[int]]
+        :rtype: List[int]
+        """
+        res = [0] * (length + 1)
+        for u in updates:
+            res[u[0]] += u[2]
+            res[u[1]+1] -= u[2]
+        sum = 0
+        for i in range(length):
+            sum += res[i]
+            res[i] = sum
+        res.pop()
+        return res
