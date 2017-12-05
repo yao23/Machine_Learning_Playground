7890d30e7ccb0b88d7b17400277fb55804079557
yao <myspiritcrazy@gmail.com>
2017-12-05 10:10:32 -0800

    Generalized Abbreviation

---
+++ LeetCode/320_generalized_abbreviation.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    # beats 76.68%
+    def generateAbbreviations(self, word):
+        """
+        :type word: str
+        :rtype: List[str]
+        """
+        def helper(word, pos, cur, count, result):
+            if len(word) == pos:
+                # Once we reach the end, append current to the result
+                result.append(cur + str(count) if count > 0 else cur)
+            else:
+                # Skip current position, and increment count
+                helper(word, pos + 1, cur, count + 1, result)
+                # Include current position, and zero-out count
+                helper(word, pos + 1, cur + (str(count) if count > 0 else '') + word[pos], 0, result)
+
+        result = []
+        helper(word, 0, '', 0, result)
+        return result
