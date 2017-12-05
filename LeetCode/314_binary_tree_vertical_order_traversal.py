b096c4a5d37d73d0e143bb956acb504ff7a54f99
yao <myspiritcrazy@gmail.com>
2017-12-05 10:06:48 -0800

    Binary Tree Vertical Order Traversal

---
+++ LeetCode/314_binary_tree_vertical_order_traversal.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+import collections
+
+class Solution(object):
+    # beats 47.63%
+    def verticalOrder(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        cols = collections.defaultdict(list)
+        queue = [(root, 0)]
+        for node, i in queue:
+            if node:
+                cols[i].append(node.val)
+                queue += (node.left, i - 1), (node.right, i + 1)
+        return [cols[i] for i in sorted(cols)]
