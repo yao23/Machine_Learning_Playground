53de68399293cd92ecd40800fe61e75272cd6d0b
yao <myspiritcrazy@gmail.com>
2017-12-05 10:29:48 -0800

    Intersection of Two Arrays II

---
+++ LeetCode/350_intersection_of_two_arrays_ii.py
@@ -0,0 +1,12 @@
+from collections import Counter
+
+class Solution(object):
+    # beats 75.88%
+    def intersect(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        c1, c2 = Counter(nums1), Counter(nums2)
+        return sum([[num] * min(c1[num], c2[num]) for num in c1 & c2], [])
