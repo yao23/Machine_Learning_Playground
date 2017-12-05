d51d0e7876171e33a608344ac3873b029d0e8bc2
yao <myspiritcrazy@gmail.com>
2017-12-05 08:19:36 -0800

    Single Number III

---
+++ LeetCode/260_single_number_iii.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    # beats 82.88%
+    def singleNumber(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        xor = 0
+        a = 0
+        b = 0
+        for num in nums:
+            xor ^= num
+        mask = 1
+        while(xor&mask == 0):
+            mask = mask << 1
+        for num in nums:
+            if num&mask:
+                a ^= num
+            else:
+                b ^= num
+        return [a, b]
