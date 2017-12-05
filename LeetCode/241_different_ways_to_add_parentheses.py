c66cf0a2a7eb1f5f71c76b0b9fa67104042015ea
yao <myspiritcrazy@gmail.com>
2017-12-05 07:18:16 -0800

    Different Ways to Add Parentheses

---
+++ LeetCode/241_different_ways_to_add_parentheses.py
@@ -0,0 +1,26 @@
+class Solution(object):
+    # beats 72.26%
+    def diffWaysToCompute(self, input):
+        """
+        :type input: str
+        :rtype: List[int]
+        """
+        if input.isdigit():
+            return [int(input)]
+        res = []
+        for i in xrange(len(input)):
+            if input[i] in "-+*":
+                res1 = self.diffWaysToCompute(input[:i])
+                res2 = self.diffWaysToCompute(input[i + 1:])
+                for j in res1:
+                    for k in res2:
+                        res.append(self.helper(j, k, input[i]))
+        return res
+
+    def helper(self, m, n, op):
+        if op == "+":
+            return m + n
+        elif op == "-":
+            return m - n
+        else:
+            return m * n
