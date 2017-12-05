37e726fd0ff2a10e00c68e973089ddfe97eb2d97
yao <myspiritcrazy@gmail.com>
2017-12-05 10:51:06 -0800

    Count Numbers with Unique Digits

---
+++ LeetCode/357_count_numbers_with_unique_digits.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    # beats 90.53%
+    def countNumbersWithUniqueDigits(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        choices = [9, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+        ans, product = 1, 1
+
+        for i in range(n if n <= 10 else 10):
+            product *= choices[i]
+            ans += product
+
+        return ans
