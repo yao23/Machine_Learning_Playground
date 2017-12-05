a393373593cb13a77c91bf91b8864b4865e1bb93
yao <myspiritcrazy@gmail.com>
2017-12-05 10:23:13 -0800

    Power of Four

---
+++ LeetCode/342_power_of_four.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    # beats 89.32%
+    def isPowerOfFour(self, num):
+        """
+        :type num: int
+        :rtype: bool
+        """
+        return num != 0 and num &(num-1) == 0 and num & 1431655765== num
