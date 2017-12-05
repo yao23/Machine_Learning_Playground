7bac89de56d707f80d3a2704bf64c302d8c04e85
yao <myspiritcrazy@gmail.com>
2017-12-05 07:20:03 -0800

    Strobogrammatic Number II

---
+++ LeetCode/247_strobogrammatic_number_ii.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    # beats 81.54%
+    def findStrobogrammatic(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        evenMidCandidate = ["11", "69", "88", "96", "00"]
+        oddMidCandidate = ["0", "1", "8"]
+        if n == 1:
+            return oddMidCandidate
+        if n == 2:
+            return evenMidCandidate[:-1]
+        if n % 2:
+            pre, midCandidate = self.findStrobogrammatic(n - 1), oddMidCandidate
+        else:
+            pre, midCandidate = self.findStrobogrammatic(n - 2), evenMidCandidate
+        premid = (n - 1) / 2
+        return [p[:premid] + c + p[premid:] for c in midCandidate for p in pre]
