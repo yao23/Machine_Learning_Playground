78b4ffcd6d19989a4902f7853e5ae868912b79a7
yao <myspiritcrazy@gmail.com>
2017-12-05 07:03:12 -0800

    Rectangle Area

---
+++ LeetCode/223_rectangle_area.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def computeArea(self, A, B, C, D, E, F, G, H):
+        """
+        :type A: int
+        :type B: int
+        :type C: int
+        :type D: int
+        :type E: int
+        :type F: int
+        :type G: int
+        :type H: int
+        :rtype: int
+        """
+        # beats 73.31%
+        overlap = max(min(C,G)-max(A,E), 0)*max(min(D,H)-max(B,F), 0)
+        return (A-C)*(B-D) + (E-G)*(F-H) - overlap
