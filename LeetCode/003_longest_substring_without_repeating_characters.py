73f81f65058be1e2535d4e8ebbadf94651a4a1fd
yao <myspiritcrazy@gmail.com>
2017-12-07 08:26:41 -0800

    refactor code

--- LeetCode/003_longest_substring_without_repeating_characters.py
+++ LeetCode/003_longest_substring_without_repeating_characters.py
@@ -1,20 +1,20 @@
 class Solution(object):
     # @return an integer
-    # beats 86.09
+    # beats 86.09%
     def lengthOfLongestSubstring(self, s):
         """
         :type s: str
         :rtype: int
         """
-        start = maxLength = 0
-        usedChar = {}
+        start = max_length = 0
+        used_char = {}
 
         for i in range(len(s)):
-            if s[i] in usedChar and start <= usedChar[s[i]]:
-                start = usedChar[s[i]] + 1
+            if s[i] in used_char and start <= used_char[s[i]]:
+                start = used_char[s[i]] + 1
             else:
-                maxLength = max(maxLength, i - start + 1)
+                max_length = max(max_length, i - start + 1)
 
-            usedChar[s[i]] = i
+            used_char[s[i]] = i
 
-        return maxLength
+        return max_length
