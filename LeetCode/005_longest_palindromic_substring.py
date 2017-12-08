a623339e139147dfb0c9507c3d4be3bb129a289f
yao <myspiritcrazy@gmail.com>
2017-12-08 08:44:55 -0800

    explanation (refactor) for longest palindromic substring

--- LeetCode/005_longest_palindromic_substring.py
+++ LeetCode/005_longest_palindromic_substring.py
@@ -25,8 +25,9 @@ class Solution(object):
 # and that new maxPalindrome includes this new character. Proof: if on adding 1 character,
 # maxPalindromeLen increased by 3 or more, say the new maxPalindromeLen is Q, and the old maxPalindromeLen is P,
 # and Q>=P+3. Then it would mean, even without this new character, there would be a palindromic substring ending
-# in the last character, whose length is at least Q-2. Since Q-2 would be >P, this contradicts the condition that
-# P is the maxPalindromeLen without the additional character.
+# in the last character, whose length is at least Q-2 (remove 1 character for odd length string or 2 for odd/even length
+# string and the rest string whose length is Q-1 or Q-2 is still palindromic, Q-1 or Q-2 is still larger then P).
+# Since Q-2 would be >P, this contradicts the condition that P is the maxPalindromeLen without the additional character.
 
 # So, it becomes simple, you only need to scan from beginning to the end, adding one character at a time,
 # keeping track of maxPalindromeLen, and for each added character, you check if the substrings ending with
