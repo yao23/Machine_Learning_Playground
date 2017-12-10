4f6a213905d722ea8f690a34a6d576e473c59c4b
yao <myspiritcrazy@gmail.com>
2017-12-09 23:14:39 -0800

    refactor code in WebServer for recommender system

--- RecommenderSystem/Webserver.py
+++ RecommenderSystem/Webserver.py
@@ -11,63 +11,81 @@ from ModelStore import ModelStore
 
 
 class WebServer(object):
-	logging.basicConfig(level=logging.INFO)
+    logging.basicConfig(level=logging.INFO)
 
-	def __init__(self, configMap):
-		self.db = DatabaseInterface(configMap['data_dir'])
-		self.numberToServe = configMap['numberToServe']
-		self.log = logging.getLogger(__name__)
-		self.userAnalyzer = UserAnalyzer
-		self.ModelStore = ModelStore
-		self.Ranker = Ranker(numberToServe=self.numberToServe, database=self.db)
-		self.RecEngine = RecEngine(self.userAnalyzer, self.ModelStore, DatabaseInterface.dbTable['user_activity'])
-		self.OnlineLearner = OnlineLearner(self.db, self.ModelStore)
-		self.OfflineLearner = OfflineLearner(self.db, self.ModelStore)
+    def __init__(self, configMap):
+        self.db = DatabaseInterface(configMap['data_dir'])
+        # numberToServe: the number of items finally served to the users
+        self.numberToServe = configMap['numberToServe']
+        self.log = logging.getLogger(__name__)
 
-	# numberToServe: the number of items finally served to the users
-	def start(self):
-		# each object here simulates the API calls through network
-		# passing an object A to the constructor of B means A will communication to B
-        DatabaseInterface.startEngine()
-        OnlineLearner.trainModel()
+    def start(self):
+        # each object here simulates the API calls through network
+        # passing an object A to the constructor of B means A will communication to B
+        self.db.startEngine()
+        self.ranker = Ranker(self.numberToServe, self.db)
+        self.userAnalyzer = UserAnalyzer()
+        self.modelStore = ModelStore()
+        self.offlineLearner = OfflineLearner(self.db, self.modelStore)
+        self.onlineLearner = OnlineLearner(self.db, self.modelStore)
+        self.offlineLearner.trainModel()
+        # when we start the webserver, we should let offline learner to train the models,
+        # such that, after the start(), we can start to give recommendation
+        self.recEngine = RecEngine(self.userAnalyzer, self.modelStore,
+                                   self.db.extract(DatabaseInterface.USER_ACTIVITY_KEY))
 
+    def getAction(self, action):
+        assert (isinstance(action, Action))
+        # taking the action from users
+        self.onlineLearner.trainModel(action)
+        # analyze action type, and save the registered user's action
+        actionType = self.userAnalyzer.analyzeAction(action)
+        if actionType == "registered":
+            self.log.info("Recording action %s" % action)
+            self.db.putAction(action)
 
-	def getAction(self,action):
-		self.db.putAction(action=action)
-		self.OnlineLearner.pushModel(model=self.ModelStore.getModel(key=ModelStore.KNN_MODEL_KEY), userId=action.userId)
-		self.OfflineLearner.pushModel(model=self.ModelStore.getModel(key=ModelStore.KNN_MODEL_KEY), memberId=action.userId)
+    def provideRecommendation(self, request):
+        # return the ID's for the recommended items
+        assert (isinstance(request, Request))
+        # provide recommendations to user
+        self.log.info("responding to request: %s" % request)
+        recommendations = self.recEngine.provideRecommendation(request)
+        recsReranked = self.ranker.rerank(recommendations)
+        return recsReranked  # a list of item ids
 
-	def provideRecommendation(self, request):
-		# return the ID's for the recommended items
-		recommendations = self.RecEngine.provideRecommendation(request)
-		self.Ranker.rerank((request.userId, recommendations))
+    def renderRecommendation(self, request):
+        assert (isinstance(request, Request))
+        recsReranked = self.provideRecommendation(request)
+        # for the purpose of testing, we sort the index, output item names
+        # output is ordered by the id value
+        return self.db.extract(DatabaseInterface.INVENTORY_KEY).loc[recsReranked].sort_index()
 
-	def renderRecommendation(self, request):
-		self.provideRecommendation(request=request)
+    def increment(self):
+        self.log.info("incrementing the system, update the models")
+        # increment the whole system by one day, trigger offline training
+        self.offlineLearner.trainModel()
+        self.modelStore.cleanOnlineModel()
+        self.recEngine.resetCache()
 
-	def increment(self):
-		self.log.info("incrementing the system, update the models")
-		# increment the whole system by one day, trigger offline training
-		self.OfflineLearner.trainModel()
-		self.ModelStore.cleanOnlineModel()
+    def getFromInventory(self, itemId):
+        return self.db.extract(DatabaseInterface.INVENTORY_KEY).loc[itemId]
 
-	def getFromInventory(self, itemId):
-		return self.db.extract(DatabaseInterface.INVENTORY_KEY).loc[itemId]
 
 # simulate a web request
 class Request(object):
-	def __init__(self, userId):
-		self.userId = userId
+    def __init__(self, userId):
+        self.userId = userId
 
-	def __str__(self):
-		return "request for user: "+str(self.userId)
+    def __str__(self):
+        return "request for user: " + str(self.userId)
+
 
 # simulate a tracking event or a user's rating
 class Action(object):
-	def __init__(self, userId, itemId,rating):
-		self.userId = userId
-		self.itemId = itemId
-		self.rating = rating
+    def __init__(self, userId, itemId, rating):
+        self.userId = userId
+        self.itemId = itemId
+        self.rating = rating
 
-	def __str__(self):
-		return "user: %s, item: %s, rating %s" %(self.userId, self.itemId, self.rating)
+    def __str__(self):
+        return "user: %s, item: %s, rating %s" % (self.userId, self.itemId, self.rating)
