790443840baba778db5128f4c69c41cd9007fd45
yao <myspiritcrazy@gmail.com>
2017-12-16 13:57:54 -0800

    fix bug to calculate width

--- LeetCode/011_container_with_most_water.py
+++ LeetCode/011_container_with_most_water.py
@@ -18,7 +18,7 @@ class Solution(object):
         right = len(height) - 1
         water = 0
         while left < right:
-            water = max(water, (height[right] - height[left]) * min(height[left], height[right]))
+            water = max(water, (right - left) * min(height[left], height[right]))
             if height[left] < height[right]:
                 left += 1
             else:
