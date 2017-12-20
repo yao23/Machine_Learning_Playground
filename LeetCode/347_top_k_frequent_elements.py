557183a53b260471d9c738b0a1689cf5b89c7bac
yao <myspiritcrazy@gmail.com>
2017-12-20 10:23:49 -0800

    Refactor code for Top K Frequent Elements

--- LeetCode/347_top_k_frequent_elements.py
+++ LeetCode/347_top_k_frequent_elements.py
@@ -1,4 +1,6 @@
 import collections
+import heapq
+
 
 class Solution(object):
     # beats 71.68%
@@ -13,3 +15,55 @@ class Solution(object):
         # and the second item of the tuple is the count
         # Thus, the built-in zip function could be used to extract the first item from the tuples
         return zip(*collections.Counter(nums).most_common(k))[0]
+
+    # dictionary + min heap
+    def topKFrequent1(self, nums, k):
+        # beats 71.68%
+        freq = {}
+        freq_list = []
+        for num in nums:
+            if num in freq:
+                freq[num] = freq[num] + 1
+            else:
+                freq[num] = 1
+
+        for key in freq.keys():
+            freq_list.append((-freq[key], key)) # -freq[key] is easier for sorting when heapify
+        heapq.heapify(freq_list)
+        topk = []
+        for i in range(0, k):
+            topk.append(heapq.heappop(freq_list)[1])
+        return topk
+
+    # dictionary + quick select
+    def topKFrequent2(nums, k):
+        # beats 63.71%
+        def quick_select(left, right):
+            pivot = left
+            l, r = left, right
+            while l < r:
+                while l < r and counts[r][1] <= counts[pivot][1]:
+                    r -= 1
+                while l < r and counts[l][1] >= counts[pivot][1]:
+                    l += 1
+                counts[l], counts[r] = counts[r], counts[l]
+            counts[left], counts[l] = counts[l], counts[left]
+
+            if l + 1 == k:
+                return counts[:l + 1]
+            elif l + 1 < k:
+                return quick_select(l + 1, right)
+            else:
+                return quick_select(left, l - 1)
+
+        if not nums:
+            return []
+
+        # Get the counts.
+        counts = {}
+        for x in nums:
+            counts[x] = counts.setdefault(x, 0) + 1
+
+        counts = counts.items()
+        # Use quick select to get the top k counts.
+        return [c[0] for c in quick_select(0, len(counts) - 1)]
