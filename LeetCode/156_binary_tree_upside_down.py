ea5881619c4d2c5ff41539899ccac1280c762cb9
yao <myspiritcrazy@gmail.com>
2017-12-20 10:00:11 -0800

    Refactor code for Upside Down Binary Tree

--- LeetCode/156_binary_tree_upside_down.py
+++ LeetCode/156_binary_tree_upside_down.py
@@ -1,9 +1,10 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution(object):
     # beats 24.21%
@@ -14,9 +15,32 @@ class Solution(object):
         """
         if not root or not root.left:
             return root
-        lRoot = self.upsideDownBinaryTree(root.left)
-        rMost = lRoot
-        while rMost.right:
-            rMost = rMost.right
-        root, rMost.left, rMost.right = lRoot, root.right, TreeNode(root.val)
+        l_root = self.upsideDownBinaryTree(root.left)
+        r_most = l_root
+        while r_most.right:
+            r_most = r_most.right
+        root = l_root
+        r_most.left = root.right
+        r_most.right = TreeNode(root.val)
         return root
+
+    def upsideDownBinaryTree1(self, root):
+        # beats 55.79%
+        if not root or not root.left:
+            return root
+
+        parent = None
+        right = None
+        cur = root
+
+        while cur:
+            left = cur.left
+
+            cur.left = right
+            right = cur.right  # avoid miss (override) cur.right in next step
+            cur.right = parent
+
+            parent = cur
+            cur = left
+
+        return parent
