bf9f8148c44e3b2d900db72ada5f21ba2234ff4d
yao <myspiritcrazy@gmail.com>
2017-12-20 16:13:05 -0800

    refactor code for Permutation II

--- LeetCode/047_permutations_ii.py
+++ LeetCode/047_permutations_ii.py
@@ -5,12 +5,15 @@ class Solution(object):
         :type nums: List[int]
         :rtype: List[List[int]]
         """
+        # [1,2,3]
+        # [[1]] => [[2,1],[1,2]] => [[3,2,1],[2,3,1],[2,1,3],[3,1,2],[1,3,2],[1,2,3]]
         ans = [[]]
         for n in nums:
             new_ans = []
             for l in ans:
-                for i in xrange(len(l)+1):
-                    new_ans.append(l[:i]+[n]+l[i:])
-                    if i<len(l) and l[i]==n: break              #handles duplication
+                for i in xrange(len(l) + 1):
+                    new_ans.append(l[:i] + [n] + l[i:])
+                    if i < len(l) and l[i] == n:
+                        break              # handles duplication
             ans = new_ans
         return ans
