c16dc6b6606551497219141804503f2cde0a2096
yao <myspiritcrazy@gmail.com>
2017-12-20 23:08:41 -0800

    Two Sum IV

---
+++ LeetCode/653_two_sum_iv.py
@@ -0,0 +1,28 @@
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def findTarget(self, root, k):
+        """
+        :type root: TreeNode
+        :type k: int
+        :rtype: bool
+        """
+        # beats 69.48%
+        if not root:
+            return False
+        bfs, s = [root], set()
+        for i in bfs:
+            if k - i.val in s:
+                return True
+            s.add(i.val)
+            if i.left:
+                bfs.append(i.left)
+            if i.right:
+                bfs.append(i.right)
+        return False
