9437747026ff035244032a4ecd6c17cab12e0768
yao <myspiritcrazy@gmail.com>
2017-12-21 13:31:37 -0800

    refactor code for Clone Graph

--- LeetCode/133_clone_graph.py
+++ LeetCode/133_clone_graph.py
@@ -1,8 +1,9 @@
 # Definition for a undirected graph node
-# class UndirectedGraphNode:
-#     def __init__(self, x):
-#         self.label = x
-#         self.neighbors = []
+class UndirectedGraphNode:
+    def __init__(self, x):
+        self.label = x
+        self.neighbors = []
+
 
 class Solution:
     # @param node, a undirected graph node
@@ -13,15 +14,14 @@ class Solution:
             return node
         root = UndirectedGraphNode(node.label)
         stack = [node]
-        visit = {}
-        visit[node.label] = root
+        visit = {node.label: root}  # map original node to new one
         while stack:
             top = stack.pop()
 
             for n in top.neighbors:
                 if n.label not in visit:
                     stack.append(n)
-                    visit[n.label] = UndirectedGraphNode(n.label)
+                    visit[n.label] = UndirectedGraphNode(n.label)  # map original node neighbor to new one
                 visit[top.label].neighbors.append(visit[n.label])
 
         return root
