0df772c0f00b7e84d87834e41880f1d6917c859c
yao <myspiritcrazy@gmail.com>
2017-12-24 09:38:33 -0800

    refactor code for Minimum Window Substring

--- LeetCode/076_minimum_window_substring.py
+++ LeetCode/076_minimum_window_substring.py
@@ -9,20 +9,20 @@ class Solution(object):
         :type t: str
         :rtype: str
 
-        current window is s[i:j] and the result window is s[I:J].
+        current window is s[cur_left:cur_right] and the result window is s[res_left:res_right].
         In need[c] I store how many times I need character c (can be negative) and missing tells how many characters
         are still missing. In the loop, first add the new character to the window.
         Then, if nothing is missing, remove as much as possible from the window start and then update the result.
         """
         need, missing = collections.Counter(t), len(t)
-        i = I = J = 0
-        for j, c in enumerate(s, 1):
+        cur_left = res_left = res_right = 0
+        for cur_right, c in enumerate(s, 1):
             missing -= need[c] > 0
             need[c] -= 1
             if not missing:  # find string t
-                while i < j and need[s[i]] < 0:
-                    need[s[i]] += 1
-                    i += 1
-                if not J or j - i <= J - I:  # find smaller window
-                    I, J = i, j
-        return s[I:J]
+                while cur_left < cur_right and need[s[cur_left]] < 0:
+                    need[s[cur_left]] += 1
+                    cur_left += 1
+                if not res_right or cur_right - cur_left <= res_right - res_left:  # find smaller window
+                    res_left, res_right = cur_left, cur_right
+        return s[res_left:res_right]
