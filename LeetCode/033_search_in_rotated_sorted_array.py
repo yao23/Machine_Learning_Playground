13824441ae136ca2a7dde7c3fd8ea638be2330c0
yao <myspiritcrazy@gmail.com>
2017-12-23 23:59:46 -0800

    add a solution for Search In Rotated Sorted Array

--- LeetCode/033_search_in_rotated_sorted_array.py
+++ LeetCode/033_search_in_rotated_sorted_array.py
@@ -1,7 +1,33 @@
 class Solution(object):
+    # https://discuss.leetcode.com/topic/13096/python-binary-search-solution-o-logn-48ms
+    # beats 98.63%
+    def search(self, nums, target):
+        if not nums:
+            return -1
+
+        low, high = 0, len(nums) - 1
+
+        while low <= high:
+            mid = low + (high - low) / 2
+            if target == nums[mid]:
+                return mid
+
+            if nums[low] <= nums[mid]:
+                if nums[low] <= target <= nums[mid]:
+                    high = mid - 1
+                else:
+                    low = mid + 1
+            else:
+                if nums[mid] <= target <= nums[high]:
+                    low = mid + 1
+                else:
+                    high = mid - 1
+
+        return -1
+
     # https://discuss.leetcode.com/topic/34467/pretty-short-c-java-ruby-python/2
     # beats 28.80%
-    def search(self, nums, target):
+    def search1(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
