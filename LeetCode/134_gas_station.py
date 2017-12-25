41ed5c68933ba928d6cc7cad901967eb96d80f8d
yao <myspiritcrazy@gmail.com>
2017-12-25 11:49:57 -0800

    add a solution for Gas Station

--- LeetCode/134_gas_station.py
+++ LeetCode/134_gas_station.py
@@ -16,3 +16,30 @@ class Solution(object):
                 balance = 0
                 position = (i + 1)
         return position
+
+    # beats 73.08%
+    def canCompleteCircuit1(self, gas, cost):
+        """
+        :type gas: List[int]
+        :type cost: List[int]
+        :rtype: int
+        """
+        len_gas = len(gas)
+        len_cost = len(cost)
+        if len_gas == 0 or len_cost == 0 or sum(gas) < sum(cost):
+            return -1
+
+        start = len_gas - 1
+        end = 0
+        gas_left = (gas[start] - cost[start])
+        while end < start:
+            if gas_left < 0:  # need more gas in start
+                start -= 1
+                gas_left += (gas[start] - cost[start])
+            else:  # continue
+                gas_left += (gas[end] - cost[end])
+                end += 1
+        if gas_left >= 0:
+            return start
+        else:
+            return -1
