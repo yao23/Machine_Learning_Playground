9d5536b1841ef1a9936ac02386510b8f22a300cb
yao <myspiritcrazy@gmail.com>
2017-12-25 11:38:39 -0800

    refactor code for Gas Station

--- LeetCode/134_gas_station.py
+++ LeetCode/134_gas_station.py
@@ -9,10 +9,10 @@ class Solution(object):
         if len(gas) == 0 or len(cost) == 0 or sum(gas) < sum(cost):
             return -1
         position = 0
-        balance = 0 # current tank balance
+        balance = 0  # current tank balance
         for i in range(len(gas)):
-            balance += gas[i] - cost[i] # update balance
-            if balance < 0: # balance drops to negative, reset the start position
+            balance += (gas[i] - cost[i])  # update balance
+            if balance < 0:  # balance drops to negative, reset the start position
                 balance = 0
-                position = i+1
+                position = (i + 1)
         return position
