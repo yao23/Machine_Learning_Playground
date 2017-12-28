bb6b5c1452452e453c59c4412939344ececf01e4
yao <myspiritcrazy@gmail.com>
2017-12-28 10:58:18 -0800

    add a solution with Binary Search Tree for Count of Smaller Numbers After Self

--- LeetCode/315_count_of_smaller_numbers_after_self.py
+++ LeetCode/315_count_of_smaller_numbers_after_self.py
@@ -77,6 +77,21 @@ class Solution(object):
             tree.update(hash_table[nums[i]] + 1, 1)
         return r[::-1]
 
+    def countSmaller4(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+
+        solution with Binary Search Tree
+
+        beats 43.55%
+        """
+        tree = BinarySearchTree()
+        return [
+                   tree.insert(nums[i], tree.root)
+                   for i in xrange(len(nums) - 1, -1, -1)
+               ][::-1]
+
 
 class BinaryIndexedTree(object):
     def __init__(self, n):
@@ -142,3 +157,41 @@ class SegmentTree(object):
             return root.val
 
         return sum([self.sum(start, end, c) for c in root.children])
+
+
+class BinarySearchTreeNode(object):
+    def __init__(self, val):
+        self.val = val
+        self.left = None
+        self.right = None
+        self.count = 1
+        self.leftTreeSize = 0
+
+
+class BinarySearchTree(object):
+    def __init__(self):
+        self.root = None
+
+    def insert(self, val, root):
+        if not root:
+            self.root = BinarySearchTreeNode(val)
+            return 0
+
+        if val == root.val:
+            root.count += 1
+            return root.leftTreeSize
+
+        if val < root.val:
+            root.leftTreeSize += 1
+
+            if not root.left:
+                root.left = BinarySearchTreeNode(val)
+                return 0
+            return self.insert(val, root.left)
+
+        if not root.right:
+            root.right = BinarySearchTreeNode(val)
+            return root.count + root.leftTreeSize
+
+        return root.count + root.leftTreeSize + self.insert(
+            val, root.right)
