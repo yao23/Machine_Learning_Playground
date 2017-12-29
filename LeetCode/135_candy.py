09d72202dd67a9cbf180164b9f498e306bb7b4b7
yao <myspiritcrazy@gmail.com>
2017-12-29 11:06:58 -0800

    refactor code for Candy

--- LeetCode/135_candy.py
+++ LeetCode/135_candy.py
@@ -6,17 +6,18 @@ class Solution(object):
         """
         :type ratings: List[int]
         :rtype: int
+
+        use two pass scan from left to right and vice versa to keep the candy level up to now
+        similar to like the Trapping Rain Water question
         """
-        # use two pass scan from left to right and vice versa to keep the candy level up to now
-        # similar to like the Trapping Rain Water question
-        res = [1]*len(ratings) # also compatable with [] input
-        lbase = rbase = 1
+        res = [1] * len(ratings)  # also compatible with [] input
+        left_base = right_base = 1
         # left scan
         for i in xrange(1, len(ratings)):
-            lbase = lbase + 1 if ratings[i] > ratings[i-1] else 1
-            res[i] = lbase
+            left_base = left_base + 1 if (ratings[i] > ratings[i - 1]) else 1
+            res[i] = left_base
         # right scan
         for i in xrange(len(ratings)-2, -1, -1):
-            rbase = rbase + 1 if ratings[i] > ratings[i+1] else 1
-            res[i] = max(rbase, res[i])
+            right_base = right_base + 1 if (ratings[i] > ratings[i + 1]) else 1
+            res[i] = max(right_base, res[i])
         return sum(res)
