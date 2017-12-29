5da55b5ba97b6675c529fa9cae61109a4094d64b
yao <myspiritcrazy@gmail.com>
2017-12-29 10:42:04 -0800

    add a solution for Frog Jump

--- LeetCode/403_frog_jump.py
+++ LeetCode/403_frog_jump.py
@@ -68,3 +68,21 @@ class Solution:
         # set, that means that it is possible to get to
         # the last stone
         return d[stones[-1]] != set()
+
+    def canCross2(self, stones):
+        """
+        :type stones: List[int]
+        :rtype: bool
+
+        beats 36.84%
+        """
+        if stones[1] != 1:
+            return False
+        d = {x: set() for x in stones}
+        d[1].add(1)
+        for x in stones[:-1]:
+            for j in d[x]:
+                for k in range(j - 1, j + 2):
+                    if k > 0 and (x + k in d):
+                        d[x + k].add(k)
+        return bool(d[stones[-1]])
