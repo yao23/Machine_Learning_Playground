8504bbd6a42c685876867bb70a23f495861122d9
yao <myspiritcrazy@gmail.com>
2017-12-28 22:50:06 -0800

    refactor code for Jump Game II

--- LeetCode/045_jump_game_ii.py
+++ LeetCode/045_jump_game_ii.py
@@ -10,10 +10,10 @@ class Solution(object):
         n, start, end, step = len(nums), 0, 0, 0
         while end < n - 1:
             step += 1
-            maxend = end + 1
+            max_end = end + 1
             for i in range(start, end + 1):
                 if i + nums[i] >= n - 1:
                     return step
-                maxend = max(maxend, i + nums[i])
-            start, end = end + 1, maxend
+                max_end = max(max_end, i + nums[i])
+            start, end = end + 1, max_end
         return step
