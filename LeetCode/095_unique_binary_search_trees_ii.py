6bfff4a9e1d48cbb235dee6f1448ad5d8d5b1c60
yao <myspiritcrazy@gmail.com>
2017-12-29 22:39:28 -0800

    add a solution for Unique Binary Search Trees II

--- LeetCode/095_unique_binary_search_trees_ii.py
+++ LeetCode/095_unique_binary_search_trees_ii.py
@@ -49,3 +49,27 @@ class Solution(object):
                     result.append(node)
         return result
 
+    def generateTrees2(self, n):
+        """
+        :type n: int
+        :rtype: List[TreeNode]
+
+        beats 37.50%
+        """
+
+        def node(val, left, right):
+            node = TreeNode(val)
+            node.left = left
+            node.right = right
+            return node
+
+        def trees(first, last):
+            return [node(root, left, right)
+                    for root in range(first, last + 1)
+                    for left in trees(first, root - 1)
+                    for right in trees(root + 1, last)] or [None]
+
+        if n == 0:
+            return []
+        else:
+            return trees(1, n)
