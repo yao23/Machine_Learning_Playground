b3cfe85a9e7226ea968f8f7caf1ba14b30d760e4
yao <myspiritcrazy@gmail.com>
2018-01-07 11:21:38 -0800

    add a solution for Serialize and Deserialize Binary Search Tree

--- LeetCode/449_serialize_and_deserialize_bst.py
+++ LeetCode/449_serialize_and_deserialize_bst.py
@@ -14,6 +14,60 @@ class Codec:
         :type root: TreeNode
         :rtype: str
 
+        https://discuss.leetcode.com/topic/80043/concise-iterative-python-solution-using-stack-beat-99/3
+        utilize BST feature (if-else block in deserialize() function)
+
+        beats 93.27%
+        """
+        res = []
+        stack = []
+        cur = root
+        while stack or cur:
+            if cur:
+                res.append(cur.val)
+                stack.append(cur)
+                cur = cur.left
+            else:
+                cur = stack.pop()
+                cur = cur.right
+        return ' '.join(map(str, res))
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+
+        :type data: str
+        :rtype: TreeNode
+        """
+        stack = []
+        data_arr = map(int, data.split())
+        if len(data_arr) == 0:
+            return None
+        root = TreeNode(data_arr[0])
+        cur = root
+
+        for value in data_arr[1:]:
+            if value < cur.val:
+                cur.left = TreeNode(value)
+                stack.append(cur)
+                cur = cur.left
+            else:
+                while stack and stack[-1].val < value:
+                    cur = stack.pop()
+
+                cur.right = TreeNode(value)
+                cur = cur.right
+
+        return root
+
+
+class Codec1:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+
+        :type root: TreeNode
+        :rtype: str
+
         beats 52.86%
         """
         res = ""
