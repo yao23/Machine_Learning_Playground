4d6233f26d07b629a2f5afe1148dc28f7f422a66
yao <myspiritcrazy@gmail.com>
2018-01-06 22:52:40 -0800

    Starry Night

---
+++ Games/StarryNight.py
@@ -0,0 +1,27 @@
+import turtle as t
+from random import randint, random
+
+
+def draw_star(points, size, col, x, y):
+    t.penup()
+    t.goto(x, y)
+    t.pendown()
+    angle = 180 - (180 / points)
+    t.color(col)
+    t.begin_fill()
+    for i in range(points):
+        t.forward(size)
+        t.right(angle)
+    t.end_fill()
+
+# main code
+t.Screen().bgcolor('dark blue')
+
+while True:
+    ranPts = randint(2, 5) * 2 + 1
+    ranSize = randint(10, 50)
+    ranCol = (random(), random(), random())
+    ranX = randint(-350, 300)
+    ranY = randint(-250, 250)
+
+    draw_star(ranPts, ranSize, ranCol, ranX, ranY)
