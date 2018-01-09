f386f4a8013922c9c889d60178bd671d5a5e8455
yao <myspiritcrazy@gmail.com>
2018-01-09 07:45:42 -0800

    Juke Box

---
+++ OOD/JukeBox.py
@@ -0,0 +1,107 @@
+class User:
+    name = ""
+    user_id = 0
+
+    def __init__(self, name, user_id):
+        self.name = name
+        self.user_id = user_id
+
+    def get_name(self):
+        return self.name
+
+    def set_name(self, name):
+        self.name = name
+
+    def get_id(self):
+        return self.user_id
+
+    def set_id(self, user_id):
+        self.user_id = user_id
+
+
+class Song:
+    name = ""
+
+    def __init__(self, name):
+        self.name = name
+
+    def get_name(self):
+        return self.name
+
+
+class SongSelector:
+    current_song = None
+
+    def __init__(self, song):
+        self.current_song = song
+
+    def set_song(self, song):
+        self.current_song = song
+
+    def get_current_song(self):
+        return self.current_song
+
+
+class PlayList:
+    song = None
+    song_list = []
+
+    def __init__(self, song, song_list):
+        self.song = song
+        self.song_list = song_list
+
+    def get_next_song(self):
+        return self.song_list[-1]
+
+    def add_to_list(self, song):
+        return self.song_list.append(song)
+
+
+class CD:
+    name = ""
+
+    def __init__(self, name):
+        self.name = name
+
+
+class CDPlayer:
+    play_list = []
+    cd = ""
+
+    def get_playlist(self):
+        return self.play_list
+
+    def set_playlist(self, play_list):
+        self.play_list = play_list
+
+    def get_cd(self):
+        return self.cd
+
+    def set_cd(self, cd):
+        self.cd = cd
+
+    def __init__(self, play_list, cd):
+        self.play_list = play_list
+        self.cd = cd
+
+    def play_song(self, song):
+        print("playing song: %s" % song)
+
+
+class JukeBox:
+    cd_player = None
+    user = None
+    cd_collection = {}
+    song_selector = None
+
+    def __init__(self, cd_player, user, cd_collection, song_selector):
+        self.cd_player = cd_player
+        self.user = user
+        self.cd_collection = cd_collection
+        self.song_selector = song_selector
+
+    def get_current_song(self):
+        return self.song_selector.get_current_song()
+
+    def set_user(self, user):
+        self.user = user
