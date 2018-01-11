d2195c9887e7d4e8d64d45432224626b6ba47f7a
yao <myspiritcrazy@gmail.com>
2018-01-10 23:06:21 -0800

    refactor code for Merge K Sorted Lists

--- LeetCode/023_merge_k_sorted_lists.py
+++ LeetCode/023_merge_k_sorted_lists.py
@@ -1,9 +1,12 @@
-# Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
 from Queue import PriorityQueue
+
+
+# Definition for singly-linked list.
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution(object):
     # beats 32.93%
@@ -16,9 +19,11 @@ class Solution(object):
         curr = dummy
         q = PriorityQueue()
         for node in lists:
-            if node: q.put((node.val,node))
-        while q.qsize()>0:
+            if node:
+                q.put((node.val, node))
+        while q.qsize() > 0:
             curr.next = q.get()[1]
-            curr=curr.next
-            if curr.next: q.put((curr.next.val, curr.next))
+            curr = curr.next
+            if curr.next:
+                q.put((curr.next.val, curr.next))
         return dummy.next
