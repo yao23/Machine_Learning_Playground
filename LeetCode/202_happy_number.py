2a22d4d5f705ad56cf671fb2b85c422f7fba9bca
yao <myspiritcrazy@gmail.com>
2018-01-15 21:51:09 -0800

    add a solution and documentation for Happy Number

--- LeetCode/202_happy_number.py
+++ LeetCode/202_happy_number.py
@@ -1,9 +1,10 @@
 class Solution(object):
-    # beats 40.96%
     def isHappy(self, n):
         """
         :type n: int
         :rtype: bool
+
+        beats 40.96%
         """
         mem = set()
         while n != 1:
@@ -14,3 +15,18 @@ class Solution(object):
                 mem.add(n)
         else:
             return True
+
+    def isHappy1(self, n):
+        """
+        :type n: int
+        :rtype: bool
+
+        endless cycle means has some replication (repeated number) when calculate square sum
+
+        beats 54.70%
+        """
+        seen = set()
+        while n not in seen:
+            seen.add(n)
+            n = sum([int(x) ** 2 for x in str(n)])
+        return n == 1
