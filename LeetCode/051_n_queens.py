5f8843a4ec919187bf07251937de72a96fbe199d
yao <myspiritcrazy@gmail.com>
2018-01-18 21:50:59 -0800

    refactor code and documentation for N Queens

--- LeetCode/051_n_queens.py
+++ LeetCode/051_n_queens.py
@@ -1,18 +1,19 @@
 class Solution(object):
-    # beats 86.88%
     def solveNQueens(self, n):
         """
         :type n: int
         :rtype: List[List[str]]
+
+        beats 86.88%
         """
         def DFS(queens, xy_dif, xy_sum):
             p = len(queens)
-            if p==n:
+            if p == n:
                 result.append(queens)
                 return None
             for q in range(n):
-                if q not in queens and p-q not in xy_dif and p+q not in xy_sum:
-                    DFS(queens+[q], xy_dif+[p-q], xy_sum+[p+q])
+                if (q not in queens) and (p - q not in xy_dif) and (p + q not in xy_sum):
+                    DFS(queens + [q], xy_dif + [p - q], xy_sum + [p + q])
         result = []
-        DFS([],[],[])
-        return [ ["."*i + "Q" + "."*(n-i-1) for i in sol] for sol in result]
+        DFS([], [], [])
+        return [["." * i + "Q" + "." * (n-i-1) for i in sol] for sol in result]
