a732a3c245dfce9af82668094f1e2dfe54c1b973
yao <myspiritcrazy@gmail.com>
2018-01-18 23:05:59 -0800

    refactor code (add a solution) and documentation for Major Element

--- LeetCode/169_majority_element.py
+++ LeetCode/169_majority_element.py
@@ -1,8 +1,36 @@
 class Solution(object):
-    # beats 89.88%
     def majorityElement(self, nums):
         """
         :type nums: List[int]
         :rtype: int
+
+        major is always in middle index after sort the input array
+
+        beats 89.88%
         """
         return sorted(nums)[len(nums)/2]
+
+    def majorityElement1(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+
+        https://leetcode.com/problems/majority-element/discuss/51613/
+        if current element is equal to major then contribute to the counter
+        otherwise it should decrease the counter
+        when counter is zero, it means a new major number start
+
+        beats 51.59%
+        """
+        len_nums = len(nums)
+        major = nums[0]
+        count = 1
+        for i in range(1, len_nums):
+            if count == 0:
+                major = nums[i]
+                count = 1
+            elif major == nums[i]:
+                count += 1
+            else:
+                count -= 1
+        return major
