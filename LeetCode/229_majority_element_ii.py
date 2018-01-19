13b7157d9802bbe24c49023cd4321b342e2145f1
yao <myspiritcrazy@gmail.com>
2018-01-18 23:15:08 -0800

    refactor code (add a solution) and documentation for Major Element II

--- LeetCode/229_majority_element_ii.py
+++ LeetCode/229_majority_element_ii.py
@@ -1,9 +1,20 @@
 class Solution(object):
-    # beats 58.89%
     def majorityElement(self, nums):
         """
         :type nums: List[int]
         :rtype: List[int]
+
+        https://discuss.leetcode.com/topic/17564/boyer-moore-majority-vote-algorithm-and-my-elaboration/8
+
+        1. there are no elements that appears more than n/3 times, then whatever the algorithm got from 1st round
+        wound be rejected in the second round.
+        2. there are only one elements that appears more than n/3 times, after 1st round one of the candidate must be
+        that appears more than n/3 times(<2n/3 other elements could only pair out for <n/3 times), the other candidate
+        is not necessarily be the second most frequent but it would be rejected in 2nd round.
+        3. there are two elments appears more than n/3 times, candidates would contain both of them. (<n/3 other
+        elements couldn't pair out any of the majorities.)
+
+        beats 58.89%
         """
         if not nums:
             return []
@@ -19,5 +30,4 @@ class Solution(object):
                 candidate2, count2 = n, 1
             else:
                 count1, count2 = count1 - 1, count2 - 1
-        return [n for n in (candidate1, candidate2)
-                        if nums.count(n) > len(nums) // 3]
+        return [n for n in (candidate1, candidate2) if nums.count(n) > len(nums) // 3]
