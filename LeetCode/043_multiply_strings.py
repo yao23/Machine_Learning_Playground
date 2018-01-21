7d40552e9fc392456b6bdd284e9644c53d1a2102
yao <myspiritcrazy@gmail.com>
2018-01-21 15:44:39 -0800

    refactor code and documentation for Multiply String

--- LeetCode/043_multiply_strings.py
+++ LeetCode/043_multiply_strings.py
@@ -1,16 +1,21 @@
 class Solution(object):
-    # beats 29.37%
     def multiply(num1, num2):
+        """
+        :param num2:
+        :return:
+
+        beats 29.37%
+        """
         product = [0] * (len(num1) + len(num2))
         pos = len(product) - 1
 
         for n1 in reversed(num1):
-            tempPos = pos
+            temp_pos = pos
             for n2 in reversed(num2):
-                product[tempPos] += int(n1) * int(n2)
-                product[tempPos - 1] += product[tempPos] / 10
-                product[tempPos] %= 10
-                tempPos -= 1
+                product[temp_pos] += int(n1) * int(n2)
+                product[temp_pos - 1] += product[temp_pos] / 10
+                product[temp_pos] %= 10
+                temp_pos -= 1
             pos -= 1
 
         pt = 0
@@ -19,12 +24,13 @@ class Solution(object):
 
         return ''.join(map(str, product[pt:]))
 
-    # beats 52.57%
     def multiply0(self, num1, num2):
         """
         :type num1: str
         :type num2: str
         :rtype: str
+
+        beats 52.57%
         """
         res = [0] * (len(num1) + len(num2))
         for i, e1 in enumerate(reversed(num1)):
@@ -33,5 +39,6 @@ class Solution(object):
                 res[i + j + 1] += res[i + j] / 10
                 res[i + j] %= 10
 
-        while len(res) > 1 and res[-1] == 0: res.pop()
+        while len(res) > 1 and res[-1] == 0:
+            res.pop()
         return ''.join(map(str, res[::-1]))
