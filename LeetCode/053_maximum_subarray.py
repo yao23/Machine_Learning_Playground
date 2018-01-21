38ea86d3ed831be7b941848efe7634a6cceb8dde
yao <myspiritcrazy@gmail.com>
2018-01-21 15:47:43 -0800

    refactor code and documentation for Maximum Subarray

--- LeetCode/053_maximum_subarray.py
+++ LeetCode/053_maximum_subarray.py
@@ -1,16 +1,19 @@
 class Solution(object):
-    # beats 78.72%
     def maxSubArray(self, nums):
         """
         :type nums: List[int]
         :rtype: int
+
+        only non-negative number contribute to the maximum sub-array
+
+        beats 78.72%
         """
         if not nums:
             return 0
 
-        curSum = maxSum = nums[0]
+        cur_sum = max_sum = nums[0]
         for num in nums[1:]:
-            curSum = max(num, curSum + num)
-            maxSum = max(maxSum, curSum)
+            cur_sum = max(num, cur_sum + num)
+            max_sum = max(max_sum, cur_sum)
 
-        return maxSum
+        return max_sum
