7f8cbcfa437d6e38d5f3bf34a05021ac5c22363c
yao <myspiritcrazy@gmail.com>
2018-01-21 15:58:07 -0800

    refactor code and documentation for Jump Game

--- LeetCode/055_jump_game.py
+++ LeetCode/055_jump_game.py
@@ -1,30 +1,32 @@
 class Solution(object):
-    # beats 65.04%
     def canJump(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
+
+        beats 65.04%
         """
         m = 0
         for i, n in enumerate(nums):
             if i > m:
                 return False
-            m = max(m, i+n)
+            m = max(m, i + n)
         return True
 
-    # beats 49.12%
     def canJump1(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
+        beats 49.12%
         """
         return reduce(lambda m, (i, n): max(m, i + n) * (i <= m), enumerate(nums, 1), 1) > 0
 
-    # beats 98.78%
     def canJump2(self, nums):
         """
         :type nums: List[int]
         :rtype: bool
+
+        beats 98.78%
         """
         goal = len(nums) - 1
         for i in range(len(nums))[::-1]:
