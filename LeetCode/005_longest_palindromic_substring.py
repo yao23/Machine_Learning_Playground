188d8fbdaebf31ae969c3e2a9f777b474e9a37bc
yao <myspiritcrazy@gmail.com>
2018-01-20 22:40:06 -0800

    refactor code and documentation for Longest Palindromic Substring

--- LeetCode/005_longest_palindromic_substring.py
+++ LeetCode/005_longest_palindromic_substring.py
@@ -1,34 +1,36 @@
 class Solution(object):
-    # @return a string
-    # beats 98.77%
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
+
+        Basic thought is simple. when you increase s by 1 character, you could only increase maxPalindromeLen by 1 or 2,
+        and that new maxPalindrome includes this new character. Proof: if on adding 1 character,
+        maxPalindromeLen increased by 3 or more, say the new maxPalindromeLen is Q, and the old maxPalindromeLen is P,
+        and Q>=P+3. Then it would mean, even without this new character, there would be a palindromic substring ending
+        in the last character, whose length is at least Q-2 (remove 1 character for odd length string or 2 for odd/even
+        length string and the rest string whose length is Q-1 or Q-2 is still palindromic, Q-1 or Q-2 is still larger
+        than P). Since Q-2 would be >P, this contradicts the condition that P is the maxPalindromeLen without the
+        additional character.
+
+        So, it becomes simple, you only need to scan from beginning to the end, adding one character at a time,
+        keeping track of maxPalindromeLen, and for each added character, you check if the substrings ending with
+        this new character, with length P+1 or P+2, are palindromes, and update accordingly.
+
+        beats 98.77%
+
         """
         if len(s) == 0:
             return 0
-        maxLen = 1
+        max_len = 1
         start = 0
-        for i in xrange(len(s)):
-            if i - maxLen >= 1 and s[i - maxLen - 1:i + 1] == s[i - maxLen - 1:i + 1][::-1]:
-                start = i - maxLen - 1
-                maxLen += 2
+        for i in range(len(s)):
+            if i - max_len >= 1 and s[i - max_len - 1:i + 1] == s[i - max_len - 1:i + 1][::-1]:
+                start = i - max_len - 1
+                max_len += 2
                 continue
 
-            if i - maxLen >= 0 and s[i - maxLen:i + 1] == s[i - maxLen:i + 1][::-1]:
-                start = i - maxLen
-                maxLen += 1
-        return s[start:start + maxLen]
-
-# Basic thought is simple. when you increase s by 1 character, you could only increase maxPalindromeLen by 1 or 2,
-# and that new maxPalindrome includes this new character. Proof: if on adding 1 character,
-# maxPalindromeLen increased by 3 or more, say the new maxPalindromeLen is Q, and the old maxPalindromeLen is P,
-# and Q>=P+3. Then it would mean, even without this new character, there would be a palindromic substring ending
-# in the last character, whose length is at least Q-2 (remove 1 character for odd length string or 2 for odd/even length
-# string and the rest string whose length is Q-1 or Q-2 is still palindromic, Q-1 or Q-2 is still larger then P).
-# Since Q-2 would be >P, this contradicts the condition that P is the maxPalindromeLen without the additional character.
-
-# So, it becomes simple, you only need to scan from beginning to the end, adding one character at a time,
-# keeping track of maxPalindromeLen, and for each added character, you check if the substrings ending with
-# this new character, with length P+1 or P+2, are palindromes, and update accordingly.
+            if i - max_len >= 0 and s[i - max_len:i + 1] == s[i - max_len:i + 1][::-1]:
+                start = i - max_len
+                max_len += 1
+        return s[start:start + max_len]
