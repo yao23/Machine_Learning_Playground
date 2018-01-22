536495eaba5098967eee7fba59d3e8698275f67b
yao <myspiritcrazy@gmail.com>
2018-01-21 16:02:44 -0800

    refactor code and documentation for Unique Paths

--- LeetCode/062_unique_paths.py
+++ LeetCode/062_unique_paths.py
@@ -1,13 +1,14 @@
 class Solution(object):
-    # beats 29.74%
     def uniquePaths(self, m, n):
         """
         :type m: int
         :type n: int
         :rtype: int
+
+        beats 29.74%
         """
         aux = [[1 for x in range(n)] for x in range(m)]
         for i in range(1, m):
             for j in range(1, n):
-                aux[i][j] = aux[i][j-1]+aux[i-1][j]
+                aux[i][j] = aux[i][j-1] + aux[i-1][j]
         return aux[-1][-1]
