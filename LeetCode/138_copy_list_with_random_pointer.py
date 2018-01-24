495026cfd4dccb3230275bf2c5505f55a2aaf597
yao <myspiritcrazy@gmail.com>
2018-01-23 21:55:46 -0800

    refactor code and documentation for Copy List with Random Pointer

--- LeetCode/138_copy_list_with_random_pointer.py
+++ LeetCode/138_copy_list_with_random_pointer.py
@@ -1,18 +1,21 @@
+import collections
+
+
 # Definition for singly-linked list with a random pointer.
-# class RandomListNode(object):
-#     def __init__(self, x):
-#         self.label = x
-#         self.next = None
-#         self.random = None
+class RandomListNode(object):
+    def __init__(self, x):
+        self.label = x
+        self.next = None
+        self.random = None
+
 
 class Solution(object):
-    # @param head, a RandomListNode
-    # @return a RandomListNode
-    # beats 98.82%
     def copyRandomList(self, head):
         """
         :type head: RandomListNode
         :rtype: RandomListNode
+
+        beats 98.82%
         """
         dic = collections.defaultdict(lambda: RandomListNode(0))
         dic[None] = None
