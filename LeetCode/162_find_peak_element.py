60a0042eff4ab91fd945a2d4b04affd8f5d365a2
yao <myspiritcrazy@gmail.com>
2018-01-24 22:40:28 -0800

    refactor code and documentation for Find Peak Element

--- LeetCode/162_find_peak_element.py
+++ LeetCode/162_find_peak_element.py
@@ -1,39 +1,41 @@
 class Solution(object):
-    # beats 25.20%
     def findPeakElement(self, nums):
         """
         :type nums: List[int]
         :rtype: int
+
+        beats 25.20%
         """
         left = 0
         right = len(nums)-1
 
         # handle condition 3
-        while left < right-1:
-            mid = (left+right)/2
-            if nums[mid] > nums[mid+1] and nums[mid] > nums[mid-1]:
+        while left < right - 1:
+            mid = (left + right) / 2
+            if nums[mid] > nums[mid + 1] and nums[mid] > nums[mid - 1]:
                 return mid
 
-            if nums[mid] < nums[mid+1]:
-                left = mid+1
+            if nums[mid] < nums[mid + 1]:
+                left = mid + 1
             else:
-                right = mid-1
+                right = mid - 1
 
-        #handle condition 1 and 2
+        # handle condition 1 and 2
         return left if nums[left] >= nums[right] else right
 
 
 """
-Basic Idea: Binary search
+Basic Idea: Binary search (brute force with O(n) solution, optimize to O(logn)
 
 Elaboration: 
- if an element(not the right-most one) is smaller than its right neighbor, then there must be a peak element on its right, because the elements on its right is either 
+ if an element (not the right-most one) is smaller than its right neighbor, then there must be a peak element on its 
+ right, because the elements on its right is either 
    1. always increasing  -> the right-most element is the peak
    2. always decreasing  -> the left-most element is the peak
    3. first increasing then decreasing -> the pivot point is the peak
    4. first decreasing then increasing -> the left-most element is the peak  
 
-   Therefore, we can find the peak only on its right elements( cut the array to half)
+   Therefore, we can find the peak only on its right elements (cut the array to half)
 
    The same idea applies to that an element(not the left-most one) is smaller than its left neighbor.
 
