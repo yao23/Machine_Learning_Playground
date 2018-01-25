5837a77d1c829a44ff2c5eec4275c22367647fd5
yao <myspiritcrazy@gmail.com>
2018-01-24 22:22:01 -0800

    refactor code and documentation for Max Points on a Line

--- LeetCode/149_max_points_on_a_line.py
+++ LeetCode/149_max_points_on_a_line.py
@@ -6,34 +6,38 @@
 
 import collections
 
+
 class Solution(object):
-    # beats 66.88%
     def maxPoints(self, points):
         """
         :type points: List[Point]
         :rtype: int
+
+        beats 66.88%
         """
-        if len(points) <= 2: return len(points)
-        d = collections.defaultdict(int) # (x,y) : count
+        if len(points) <= 2:
+            return len(points)
+        d = collections.defaultdict(int)  # (x,y) : count
         result = 0
         for i in range(len(points)):
             d.clear()
             overlap = 0
-            curmax = 0
+            cur_max = 0
             for j in range(i+1, len(points)):
                 dx = points[j].x - points[i].x
                 dy = points[j].y - points[i].y
                 if dx == 0 and dy == 0:
                     overlap += 1
                     continue
-                gcd = self.getGcd(dx, dy)
+                gcd = self.get_gcd(dx, dy)
                 dx //= gcd
                 dy //= gcd
-                d[(dx,dy)] += 1
-                curmax = max(curmax, d[(dx,dy)])
-            result = max(result, curmax+overlap+1)
+                d[(dx, dy)] += 1
+                cur_max = max(cur_max, d[(dx, dy)])
+            result = max(result, cur_max+overlap+1)
         return result
 
-    def getGcd(self, a, b):
-        if b == 0: return a
-        return self.getGcd(b, a%b)
+    def get_gcd(self, a, b):
+        if b == 0:
+            return a
+        return self.get_gcd(b, a % b)
