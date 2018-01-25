b9b83e5e0ed9b780de1284aee908e7901a13c8ad
yao <myspiritcrazy@gmail.com>
2018-01-24 23:14:53 -0800

    refactor code and documentation for Largest Number

--- LeetCode/179_largest_number.py
+++ LeetCode/179_largest_number.py
@@ -1,9 +1,18 @@
 class Solution:
-    # @param {integer[]} nums
-    # @return {string}
+    """
+    @param {integer[]} nums
+    @return {string}
 
-    # beats 91.37%
+    The actual sort procedure is done by Python, the cpm just let Python know how to compare the them.
+    For example, if you compare '3' and '30', then the cpm will try to compare '330' and '303', then it'll decide 3 is
+    bigger than 30 because '330' is bigger than '303'.
+
+    https://leetcode.com/problems/largest-number/discuss/53270/
+    https://leetcode.com/problems/largest-number/discuss/53162/
+
+    beats 91.37%
+    """
     def largestNumber(self, nums):
         nums = [str(x) for x in nums]
-        nums.sort(cmp=lambda x, y: cmp(y+x, x+y))
+        nums.sort(cmp=lambda x, y: cmp(y + x, x + y))
         return ''.join(nums).lstrip('0') or '0'
