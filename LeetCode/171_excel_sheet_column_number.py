3fb23efc46b19823532190c0a47dbc47ab74364a
yao <myspiritcrazy@gmail.com>
2018-01-24 22:58:12 -0800

    refactor code and documentation for Excel Sheet Column Number

--- LeetCode/171_excel_sheet_column_number.py
+++ LeetCode/171_excel_sheet_column_number.py
@@ -1,8 +1,17 @@
-def titleToNumber(self, s):
-    """
-    :type s: str
-    :rtype: int
-    """
+class Solution:
+    def titleToNumber(self, s):
+        """
+        :type s: str
+        :rtype: int
 
-    # beats 23.59%
-    return sum((ord(char) - 64) * (26 ** exp) for exp, char in enumerate(s[::-1]))
+        A -> 1
+        B -> 2
+        C -> 3
+        ...
+        Z -> 26
+        AA -> 27
+        AB -> 28
+
+        beats 23.59%
+        """
+        return sum((ord(char) - 64) * (26 ** exp) for exp, char in enumerate(s[::-1]))
