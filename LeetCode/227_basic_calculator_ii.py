79133873119d16be31b19a95c909d752de16bbce
yao <myspiritcrazy@gmail.com>
2018-01-25 07:41:53 -0800

    refactor code and documentation for Basic Calculator II

--- LeetCode/227_basic_calculator_ii.py
+++ LeetCode/227_basic_calculator_ii.py
@@ -1,29 +1,36 @@
 class Solution(object):
-    # beats 95.56%
     def calculate(self, s):
         """
         :type s: str
         :rtype: int
+
+        "3+2*2" = 7
+        " 3/2 " = 1
+        " 3+5 / 2 " = 5
+
+        collect all immediate results and add them finally
+
+        beats 95.56%
         """
         if not s:
             return "0"
         stack, num, sign = [], 0, "+"
-        for i in xrange(len(s)):
+        for i in range(len(s)):
             if s[i].isdigit():
-                num = num*10+ord(s[i])-ord("0")
-            if (not s[i].isdigit() and not s[i].isspace()) or i == len(s)-1:
+                num = num * 10 + ord(s[i]) - ord("0")
+            if (not s[i].isdigit() and not s[i].isspace()) or i == len(s) - 1:
                 if sign == "-":
                     stack.append(-num)
                 elif sign == "+":
                     stack.append(num)
                 elif sign == "*":
-                    stack.append(stack.pop()*num)
+                    stack.append(stack.pop() * num)
                 else:
                     tmp = stack.pop()
-                    if tmp//num < 0 and tmp%num != 0:
-                        stack.append(tmp//num+1)
+                    if tmp // num < 0 and tmp % num != 0:
+                        stack.append(tmp // num + 1)
                     else:
-                        stack.append(tmp//num)
+                        stack.append(tmp // num)
                 sign = s[i]
                 num = 0
         return sum(stack)
