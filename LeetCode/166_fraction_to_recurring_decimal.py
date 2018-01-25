f1386ba3897ffe46224443e244d0e113b6e4d1ff
yao <myspiritcrazy@gmail.com>
2018-01-24 22:54:16 -0800

    refactor code and documentation for Fraction to Recurring Decimal

--- LeetCode/166_fraction_to_recurring_decimal.py
+++ LeetCode/166_fraction_to_recurring_decimal.py
@@ -1,21 +1,24 @@
 class Solution(object):
-    # beats 18.08%
     def fractionToDecimal(self, numerator, denominator):
         """
         :type numerator: int
         :type denominator: int
         :rtype: str
+
+        use a stack to map remainders to their positions
+
+        beats 18.08%
         """
         n, remainder = divmod(abs(numerator), abs(denominator))
-        sign = '-' if numerator*denominator < 0 else ''
-        result = [sign+str(n), '.']
+        sign = '-' if numerator * denominator < 0 else ''
+        result = [sign + str(n), '.']
         stack = []
         while remainder not in stack:
             stack.append(remainder)
-            n, remainder = divmod(remainder*10, abs(denominator))
+            n, remainder = divmod(remainder * 10, abs(denominator))
             result.append(str(n))
 
         idx = stack.index(remainder)
-        result.insert(idx+2, '(')
+        result.insert(idx + 2, '(')
         result.append(')')
         return ''.join(result).replace('(0)', '').rstrip('.')
