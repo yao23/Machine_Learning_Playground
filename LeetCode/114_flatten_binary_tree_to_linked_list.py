38cd876d386dc644172bd532b6447245be7d0217
yao <myspiritcrazy@gmail.com>
2018-01-24 20:29:01 -0800

    refactor code and documentation for Flatten Binary Tree to Linked List

--- LeetCode/114_flatten_binary_tree_to_linked_list.py
+++ LeetCode/114_flatten_binary_tree_to_linked_list.py
@@ -5,21 +5,20 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution(object):
-    # @param {TreeNode} root
-    # @return {void} Do not return anything, modify root in-place instead.
-    # beats 75.80%
     def flatten(self, root):
         """
         :type root: TreeNode
         :rtype: void Do not return anything, modify root in-place instead.
-        """
-        '''
+
         1. flatten left subtree
         2. find left subtree's tail
         3. set root's left to None, root's right to root'left, tail's right to root.right
         4. flatten the original right subtree
-        '''
+
+        beats 75.80%
+        """
         # escape condition
         if not root:
             return
@@ -35,3 +34,19 @@ class Solution(object):
             root.left, root.right, tail.right = None, root.left, right
         # flatten right subtree
         self.flatten(right)
+
+    prev = None
+    def flatten1(self, root):
+        """
+        :type root: TreeNode
+        :rtype: void Do not return anything, modify root in-place instead.
+
+        beats 44.12%
+        """
+        if root is None:
+            return
+        self.flatten(root.right)
+        self.flatten(root.left)
+        root.right = self.prev
+        root.left = None
+        self.prev = root
