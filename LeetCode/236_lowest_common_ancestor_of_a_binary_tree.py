8a0fb48891b2a3460598814ae3390a039da688aa
yao <myspiritcrazy@gmail.com>
2018-01-27 17:00:12 -0800

    refactor code and documentation for Lowest Common Ancestor of a Binary Tree

--- LeetCode/236_lowest_common_ancestor_of_a_binary_tree.py
+++ LeetCode/236_lowest_common_ancestor_of_a_binary_tree.py
@@ -5,16 +5,18 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution(object):
-    # beats 14.54%
     def lowestCommonAncestor(self, root, p, q):
         """
         :type root: TreeNode
         :type p: TreeNode
         :type q: TreeNode
         :rtype: TreeNode
+
+        beats 14.54%
         """
-        if root in (None, p, q): return root
-        left, right = (self.lowestCommonAncestor(kid, p, q)
-                       for kid in (root.left, root.right))
+        if root in (None, p, q):
+            return root
+        left, right = (self.lowestCommonAncestor(kid, p, q) for kid in (root.left, root.right))
         return root if left and right else left or right
