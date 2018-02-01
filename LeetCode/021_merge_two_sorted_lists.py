6bf47f546f0f233c67602f1e2c6f74446d54ae9b
yao <myspiritcrazy@gmail.com>
2018-01-31 23:23:17 -0800

    refactor code and documentation for Merge Two Sorted Lists

--- LeetCode/021_merge_two_sorted_lists.py
+++ LeetCode/021_merge_two_sorted_lists.py
@@ -1,8 +1,9 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution(object):
     # in-place, iteratively
