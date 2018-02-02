25d54e67ad57d1fa71e77e838a9c4c822deb3c4c
yao <myspiritcrazy@gmail.com>
2018-02-02 08:16:53 -0800

    refactor code and documentation for Remove Element

--- LeetCode/027_remove_element.py
+++ LeetCode/027_remove_element.py
@@ -1,15 +1,16 @@
 class Solution(object):
-    # beats 93.58%
     def removeElement(self, nums, val):
         """
         :type nums: List[int]
         :type val: int
         :rtype: int
+
+        beats 93.58%
         """
         start, end = 0, len(nums) - 1
         while start <= end:
             if nums[start] == val:
                 nums[start], nums[end], end = nums[end], nums[start], end - 1
             else:
-                start +=1
+                start += 1
         return start
