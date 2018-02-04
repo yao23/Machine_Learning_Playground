dfc6f14bc6d90c8a15c907ea7295c1abf4d795cc
yao <myspiritcrazy@gmail.com>
2018-02-03 23:15:54 -0800

    refactor code and documentation for Implement Strstr()

--- LeetCode/028_implement_strstr().py
+++ LeetCode/028_implement_strstr().py
@@ -1,10 +1,11 @@
 class Solution(object):
-    # beats 96.62%
     def strStr(self, haystack, needle):
         """
         :type haystack: str
         :type needle: str
         :rtype: int
+
+        beats 96.62%
         """
         for i in range(len(haystack) - len(needle)+1):
             if haystack[i:i+len(needle)] == needle:
