40f402cfe939bf2e50868b750473afce10fbc3ae
yao <myspiritcrazy@gmail.com>
2018-02-04 23:56:45 -0800

    refactor code and documentation for Search for A Range

--- LeetCode/034_search_for_a_range.py
+++ LeetCode/034_search_for_a_range.py
@@ -1,13 +1,14 @@
 class Solution(object):
-    # @param A, a list of integers
-    # @param target, an integer to be searched
-    # @return a list of length 2, [index1, index2]
-    # beats 39.47%
     def searchRange(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: List[int]
+
+        @param A, a list of integers
+        @param target, an integer to be searched
+        @return a list of length 2, [index1, index2]
+        beats 39.47%
         """
         start = self.binary_search(nums, target-0.5)
         if not target in nums[start:start+1]:
