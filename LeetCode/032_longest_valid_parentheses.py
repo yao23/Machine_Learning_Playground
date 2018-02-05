f3ddd7ed0b8012c7f5b7796787a695011040c457
yao <myspiritcrazy@gmail.com>
2018-02-04 23:55:59 -0800

    refactor code and documentation for Longest Valid Parentheses

--- LeetCode/032_longest_valid_parentheses.py
+++ LeetCode/032_longest_valid_parentheses.py
@@ -1,9 +1,9 @@
 class Solution(object):
-    # beats 55.07%
     def longestValidParentheses(self, s):
         """
         :type s: str
         :rtype: int
+        beats 55.07%
         """
         # use 1D DP
         # dp[i] records the longestValidParenthese EXACTLY ENDING at s[i]
@@ -18,11 +18,10 @@ class Solution(object):
                 # case 2: (())
                 # i-dp[i-1]-1 is the index of last "(" not paired until this ")"
                 elif i-dp[i-1]-1 >= 0 and s[i-dp[i-1]-1] == '(':
-                    if dp[i-1] > 0: # content within current matching pair is valid
-                    # add nearest parentheses pairs + 2 + parentheses before last "("
+                    if dp[i-1] > 0:  # content within current matching pair is valid
+                        # add nearest parentheses pairs + 2 + parentheses before last "("
                         dp[i] = dp[i-1] + 2 + dp[i-dp[i-1]-2]
-                    else:
-                    # otherwise is 0
+                    else:  # otherwise is 0
                         dp[i] = 0
                 max_to_now = max(max_to_now, dp[i])
         return max_to_now
