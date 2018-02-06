a75850c9742ead2243350f7d7e6fe91f62cb01b5
yao <myspiritcrazy@gmail.com>
2018-02-05 21:26:17 -0800

    refactor code and documentation for Search Insert Position

--- LeetCode/035_search_insert_position.py
+++ LeetCode/035_search_insert_position.py
@@ -1,9 +1,10 @@
 class Solution(object):
-    # beats 60.70%
     def searchInsert(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
+
+        beats 60.70%
         """
-        return len([x for x in nums if x<target])
+        return len([x for x in nums if x < target])
