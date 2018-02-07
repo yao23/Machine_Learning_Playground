23d16c9fd5abf57458d0557f18e1ad25cc53b9bf
yao <myspiritcrazy@gmail.com>
2018-02-06 22:29:27 -0800

    refactor code and documentation for First Missing Positive

--- LeetCode/041_first_missing_positive.py
+++ LeetCode/041_first_missing_positive.py
@@ -1,25 +1,25 @@
 class Solution(object):
-    # beats 34.49%
     def firstMissingPositive(self, nums):
         """
         :type nums: List[int]
         :rtype: int
-        """
-        """
+
         Basic idea:
         1. for any array whose length is l, the first missing positive must be in range [1,...,l+1], 
             so we only have to care about those elements in this range and remove the rest.
         2. we can use the array index as the hash to restore the frequency of each number within 
-             the range [1,...,l+1] 
+             the range [1,...,l+1]
+
+        beats 34.49%
         """
         nums.append(0)
         n = len(nums)
-        for i in range(len(nums)): #delete those useless elements
-            if nums[i]<0 or nums[i]>=n:
-                nums[i]=0
-        for i in range(len(nums)): #use the index as the hash to record the frequency of each number
-            nums[nums[i]%n]+=n
-        for i in range(1,len(nums)):
-            if nums[i]/n==0:
+        for i in range(len(nums)):  # delete those useless elements
+            if nums[i] < 0 or nums[i] >= n:
+                nums[i] = 0
+        for i in range(len(nums)):  # use the index as the hash to record the frequency of each number
+            nums[nums[i] % n] += n
+        for i in range(1, len(nums)):
+            if nums[i]/n == 0:
                 return i
         return n
