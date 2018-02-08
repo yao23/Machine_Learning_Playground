f1d845cfb0e3351fbae17bf74b6d459d5a1c2a47
yao <myspiritcrazy@gmail.com>
2018-02-07 21:21:24 -0800

    refactor code and documentation for Count Primes

--- LeetCode/204_count_primes.py
+++ LeetCode/204_count_primes.py
@@ -49,3 +49,21 @@ class Solution(object):
                 for j in range(i * i, n, i):
                     primes[j] = False
         return sum(primes)
+
+    def countPrimes2(self, n):
+        """
+        :type n: int
+        :rtype: int
+
+        use 1, 0 instead of True, False, it will be faster .
+
+        beats 100.00%
+        """
+        if n < 2:
+            return 0
+        s = [1] * n
+        s[0] = s[1] = 0
+        for i in range(2, int(n ** 0.5) + 1):
+            if s[i] == 1:
+                s[i * i:n:i] = [0] * len(s[i * i:n:i])
+        return sum(s)
