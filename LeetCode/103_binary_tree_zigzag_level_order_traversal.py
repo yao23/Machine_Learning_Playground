5520094fc1d667b8fcf49918198737d3303b12f0
yao <myspiritcrazy@gmail.com>
2018-02-08 10:00:52 -0800

    refactor code and documentation for Binary Tree Zigzag Level Order Traversal

--- LeetCode/103_binary_tree_zigzag_level_order_traversal.py
+++ LeetCode/103_binary_tree_zigzag_level_order_traversal.py
@@ -5,22 +5,27 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution(object):
-    # beats 47.51%
     def zigzagLevelOrder(self, root):
         """
         :type root: TreeNode
         :rtype: List[List[int]]
+
+        beats 47.51%
         """
-        if not root: return []
-        res, temp, stack, flag=[], [], [root], 1
+        if not root:
+            return []
+        res, temp, stack, flag = [], [], [root], 1
         while stack:
-            for i in xrange(len(stack)):
-                node=stack.pop(0)
-                temp+=[node.val]
-                if node.left: stack+=[node.left]
-                if node.right: stack+=[node.right]
-            res+=[temp[::flag]]
-            temp=[]
-            flag*=-1
+            for i in range(len(stack)):
+                node = stack.pop(0)
+                temp += [node.val]
+                if node.left:
+                    stack += [node.left]
+                if node.right:
+                    stack += [node.right]
+            res += [temp[::flag]]
+            temp = []
+            flag *= -1
         return res
