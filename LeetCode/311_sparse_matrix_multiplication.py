1582342e4108ee6aec5c8e3c5bfa3e51c92554da
yao <myspiritcrazy@gmail.com>
2018-02-08 22:56:28 -0800

    refactor code and documentation for Sparse Matrix Multiplication

--- LeetCode/311_sparse_matrix_multiplication.py
+++ LeetCode/311_sparse_matrix_multiplication.py
@@ -1,24 +1,29 @@
 class Solution(object):
-    # beats 28.07%
     def multiply(self, A, B):
         """
         :type A: List[List[int]]
         :type B: List[List[int]]
         :rtype: List[List[int]]
+
+        https://discuss.leetcode.com/topic/32970/ac-soluiton-code
+
+        beats 28.07%
         """
-        if A is None or B is None: return None
-        m, n, l = len(A), len(A[0]), len(B[0])
-        if len(B) != n:
+        if A is None or B is None:
+            return None
+        row_num_a, col_num_a, col_num_b = len(A), len(A[0]), len(B[0])
+        if len(B) != col_num_a:
             raise Exception("A's column number must be equal to B's row number.")
-        C = [[0 for _ in range(l)] for _ in range(m)]
-        tableB = {}
+        result = [[0 for _ in range(col_num_b)] for _ in range(row_num_a)]
+        table_b = {}
         for k, row in enumerate(B):
-            tableB[k] = {}
-            for j, eleB in enumerate(row):
-                if eleB: tableB[k][j] = eleB
+            table_b[k] = {}
+            for j, element_b in enumerate(row):
+                if element_b:
+                    table_b[k][j] = element_b
         for i, row in enumerate(A):
-            for k, eleA in enumerate(row):
-                if eleA:
-                    for j, eleB in tableB[k].iteritems():
-                        C[i][j] += eleA * eleB
-        return C
+            for k, element_a in enumerate(row):
+                if element_a:
+                    for j, element_b in table_b[k].iteritems():
+                        result[i][j] += element_a * element_b
+        return result
