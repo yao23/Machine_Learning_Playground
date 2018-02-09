cef112f47315aad7a621eda10cbb07ea59ccff87
yao <myspiritcrazy@gmail.com>
2018-02-08 23:40:58 -0800

    refactor code and documentation for Longest Increasing Subsequence

--- LeetCode/300_longest_increasing_subsequence.py
+++ LeetCode/300_longest_increasing_subsequence.py
@@ -1,5 +1,4 @@
 class Solution(object):
-    # beats 82.38%
     def lengthOfLIS(self, nums):
         """
         :type nums: List[int]
@@ -8,8 +7,8 @@ class Solution(object):
         https://leetcode.com/problems/longest-increasing-subsequence/discuss/74824/
         https://www.geeksforgeeks.org/longest-monotonically-increasing-subsequence-size-n-log-n/
 
-        tails is an array storing the smallest tail of all increasing subsequences with length i+1 in tails[i].
-        For example, say we have nums = [4,5,6,3], then all the available increasing subsequences are:
+        tails is an array storing the smallest tail of all increasing sub-sequences with length i+1 in tails[i].
+        For example, say we have nums = [4,5,6,3], then all the available increasing sub-sequences are:
         len = 1   :      [4], [5], [6], [3]   => tails[0] = 3
         len = 2   :      [4, 5], [5, 6]       => tails[1] = 5
         len = 3   :      [4, 5, 6]            => tails[2] = 6
@@ -20,6 +19,8 @@ class Solution(object):
         (1) if x is larger than all tails, append it, increase the size by 1
         (2) if tails[i-1] < x <= tails[i], update tails[i]
         Doing so will maintain the tails invariant. The the final answer is just the size.
+
+        beats 82.38%
 
         """
         tails = [0] * len(nums)
