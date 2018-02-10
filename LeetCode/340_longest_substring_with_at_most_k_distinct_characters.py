d3742b0d47b7f7b9252a2b4fa9fec5be2a89c69c
yao <myspiritcrazy@gmail.com>
2018-02-09 23:45:36 -0800

    refactor code and documentation for Longest Substring with At Most K Distinct Characters

--- LeetCode/340_longest_substring_with_at_most_k_distinct_characters.py
+++ LeetCode/340_longest_substring_with_at_most_k_distinct_characters.py
@@ -1,22 +1,25 @@
-"""
-The general idea is to iterate over string s.
-Always put the character c and its location i in the dictionary d.
-1) If the sliding window contains less than or equal to k distinct characters, simply record the return value, and move on.
-2) Otherwise, we need to remove a character from the sliding window.
-   Here's how to find the character to be removed:
-   Because the values in d represents the rightmost location of each character in the sliding window, in order to find the longest substring T, we need to locate the smallest location, and remove it from the dictionary, and then record the return value.
-"""
-
 class Solution(object):
-    # beats 95.04%
     def lengthOfLongestSubstringKDistinct(self, s, k):
         """
         :type s: str
         :type k: int
         :rtype: int
+
+        The general idea is to iterate over string s.
+        Always put the character c and its location i in the dictionary d.
+        1) If the sliding window contains less than or equal to k distinct characters, simply record the return value,
+        and move on.
+        2) Otherwise, we need to remove a character from the sliding window.
+           Here's how to find the character to be removed:
+           Because the values in d represents the rightmost location of each character in the sliding window, in order
+           to find the longest substring T, we need to locate the smallest location, and remove it from the dictionary,
+           and then record the return value.
+
+        Use dictionary d to keep track of (character, location) pair,
+        where location is the rightmost location that the character appears at
+
+        beats 95.04%
         """
-        # Use dictionary d to keep track of (character, location) pair,
-        # where location is the rightmost location that the character appears at
         d = {}
         low, ret = 0, 0
         for i, c in enumerate(s):
