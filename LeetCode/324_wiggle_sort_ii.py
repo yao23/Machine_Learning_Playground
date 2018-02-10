303483fd4587233e5c8768030c52c24e6946b81a
yao <myspiritcrazy@gmail.com>
2018-02-09 22:26:30 -0800

    refactor code and documentation for Wiggle Sort II

--- LeetCode/324_wiggle_sort_ii.py
+++ LeetCode/324_wiggle_sort_ii.py
@@ -4,6 +4,11 @@ class Solution(object):
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
 
+        nums: [1, 5, 1, 1, 6, 4]
+        sort: [1, 1, 1, 4, 5, 6]
+        nums[half::-1] => [1, 1, 1]
+        nums[:half:-1] => [6, 5, 4]
+
         beats 96.09%
         """
         nums.sort()
@@ -14,6 +19,11 @@ class Solution(object):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
+
+        nums: [1, 5, 1, 1, 6, 4]
+        sort: [1, 1, 1, 4, 5, 6]
+        nums[:half][::-1] => [1, 1, 1]
+        nums[half:][::-1] => [6, 5, 4]
 
         beats 48.53%
         """
