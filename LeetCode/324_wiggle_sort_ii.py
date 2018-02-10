48b9bf4d886f6b9c89384c709a81f7cd016db0bf
yao <myspiritcrazy@gmail.com>
2018-02-09 21:00:09 -0800

    refactor code and documentation for Wiggle Sort II

--- LeetCode/324_wiggle_sort_ii.py
+++ LeetCode/324_wiggle_sort_ii.py
@@ -1,19 +1,21 @@
 class Solution(object):
-    # beats 96.09%
     def wiggleSort(self, nums):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
+
+        beats 96.09%
         """
         nums.sort()
         half = len(nums[::2]) - 1
         nums[::2], nums[1::2] = nums[half::-1], nums[:half:-1]
 
-    # beats 48.53%
     def wiggleSort1(self, nums):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
+
+        beats 48.53%
         """
         nums.sort()
         half = len(nums[::2])
