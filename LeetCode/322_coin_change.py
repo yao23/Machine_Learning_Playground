2bf32f7e9d1c89d27cd317b20ee945662923a4ed
yao <myspiritcrazy@gmail.com>
2018-02-09 16:37:13 -0800

    refactor code and documentation for Coin Change

--- LeetCode/322_coin_change.py
+++ LeetCode/322_coin_change.py
@@ -1,10 +1,12 @@
-# This solution is inspired by the BFS solution for problem Perfect Square.
-# Since it is to find the least coin solution (like a shortest path from 0 to amount),
-# using BFS gives results much faster than DP.
-# https://discuss.leetcode.com/topic/26262/short-python-solution-using-bfs
-
 class Solution(object):
-    # beats 97.75%
+    """
+    This solution is inspired by the BFS solution for problem Perfect Square.
+    Since it is to find the least coin solution (like a shortest path from 0 to amount),
+    using BFS gives results much faster than DP.
+    https://discuss.leetcode.com/topic/26262/short-python-solution-using-bfs
+
+    beats 97.75%
+    """
     def coinChange(self, coins, amount):
         """
         :type coins: List[int]
@@ -15,20 +17,20 @@ class Solution(object):
             return 0
         value1 = [0]
         value2 = []
-        nc =  0
-        visited = [False]*(amount+1)
+        nc = 0
+        visited = [False] * (amount + 1)
         visited[0] = True
         while value1:
             nc += 1
             for v in value1:
                 for coin in coins:
-                    newval = v + coin
-                    if newval == amount:
+                    new_val = v + coin
+                    if new_val == amount:
                         return nc
-                    elif newval > amount:
+                    elif new_val > amount:
                         continue
-                    elif not visited[newval]:
-                        visited[newval] = True
-                        value2.append(newval)
+                    elif not visited[new_val]:
+                        visited[new_val] = True
+                        value2.append(new_val)
             value1, value2 = value2, []
         return -1
