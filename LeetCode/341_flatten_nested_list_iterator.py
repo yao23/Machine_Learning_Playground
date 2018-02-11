c07ef5ec827ac36211d15062fa77ef341cb58d64
yao <myspiritcrazy@gmail.com>
2018-02-10 18:33:58 -0800

    refactor code and documentation for Flatten Nested List Iterator

--- LeetCode/341_flatten_nested_list_iterator.py
+++ LeetCode/341_flatten_nested_list_iterator.py
@@ -71,6 +71,46 @@ class NestedIterator(object):
                 s.append([x.getList(), 0])
         return False
 
+
+class NestedIterator1(object):
+    """
+    Generator
+
+    https://leetcode.com/problems/flatten-nested-list-iterator/discuss/80247/Python-Generators-solution
+
+    beats 83.76%
+    """
+    def __init__(self, nestedList):
+        self.peek = None
+
+        def gen(nestedList):
+            for x in nestedList:
+                if x.isInteger():
+                    yield x.getInteger()
+                else:
+                    for y in gen(x.getList()):
+                        yield y
+
+        self.gen = gen(nestedList)
+
+    def next(self):
+        if self.peek is None:
+            return next(self.gen)
+        else:
+            tmp = self.peek
+            self.peek = None
+            return tmp
+
+    def hasNext(self):
+        if self.peek is None:
+            try:
+                self.peek = next(self.gen)
+                return True
+            except StopIteration:
+                return False
+        else:
+            return True
+
 # Your NestedIterator object will be instantiated and called as such:
 # i, v = NestedIterator(nestedList), []
 # while i.hasNext(): v.append(i.next())
