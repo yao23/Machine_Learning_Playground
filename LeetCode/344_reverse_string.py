191d280706976071be646332db9d5c4de057c0a5
yao <myspiritcrazy@gmail.com>
2018-02-10 21:10:29 -0800

    refactor code and documentation for Reverse String

--- LeetCode/344_reverse_string.py
+++ LeetCode/344_reverse_string.py
@@ -7,3 +7,19 @@ class Solution(object):
         beats 51.55%
         """
         return s[::-1]
+
+    def reverseString1(self, s):
+        """
+        :type s: str
+        :rtype: str
+
+        beats 19.20%
+        """
+        char_arr = list(s)
+        left, right = 0, len(char_arr) - 1
+        while left < right:
+            char_arr[left], char_arr[right] = char_arr[right], char_arr[left]
+            left += 1
+            right -= 1
+
+        return "".join(char_arr)
