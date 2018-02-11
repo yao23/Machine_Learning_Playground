c6df87beb9e1ff59c58420fc6899fb88188c1534
yao <myspiritcrazy@gmail.com>
2018-02-10 23:34:39 -0800

    refactor code and documentation for Random Pick Index

--- LeetCode/398_random_pick_index.py
+++ LeetCode/398_random_pick_index.py
@@ -22,6 +22,39 @@ class Solution(object):
         """
         return random.choice([k for k, v in enumerate(self.nums) if v == target])
 
+
+class Solution1(object):
+    """
+    https://leetcode.com/problems/random-pick-index/discuss/88153/Python-reservoir-sampling-solution.
+
+    reservoir sampling
+
+    beast 46.55%
+    """
+
+    def __init__(self, nums):
+        """
+
+        :type nums: List[int]
+        :type numsSize: int
+        """
+        self.nums = nums
+
+    def pick(self, target):
+        """
+        :type target: int
+        :rtype: int
+        """
+        res = None
+        count = 0
+        for i, x in enumerate(self.nums):
+            if x == target:
+                count += 1
+                chance = random.randint(1, count)
+                if chance == count:
+                    res = i
+        return res
+
 # Your Solution object will be instantiated and called as such:
 # obj = Solution(nums)
 # param_1 = obj.pick(target)
