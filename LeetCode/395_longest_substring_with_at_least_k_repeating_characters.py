a4e0dbe6e9d2798be789c0e992ea74dd8f51303a
yao <myspiritcrazy@gmail.com>
2018-02-10 23:24:40 -0800

    refactor code and documentation for Longest Substring with At Least K Repeating Characters

--- LeetCode/395_longest_substring_with_at_least_k_repeating_characters.py
+++ LeetCode/395_longest_substring_with_at_least_k_repeating_characters.py
@@ -34,3 +34,43 @@ class Solution(object):
             if s.count(c) < k:
                 return max(self.longestSubstring(t, k) for t in s.split(c))
         return len(s)
+
+    def longestSubstring2(self, s, k):
+        """
+        :type s: str
+        :type k: int
+        :rtype: int
+
+        beats 63.47%
+        """
+        if len(s) < k:
+            return 0
+        my_dict, my_set = {}, set()
+        for c in s:
+            if c in my_dict.keys():
+                my_dict[c] += 1
+            else:
+                my_dict[c] = 1
+            if my_dict[c] >= k:
+                my_set.discard(c)
+            else:
+                my_set.add(c)
+        if len(my_set) == 0:
+            return len(s)
+        intervals, start = [], 0
+        while start < len(s):
+            if s[start] not in my_set:
+                i = start
+                while start < len(s):
+                    if s[start] not in my_set:
+                        start += 1
+                    else:
+                        break
+                intervals.append((i, start))
+            else:
+                start += 1
+        g_max = 0
+        for interval in intervals:
+            g_max = max(g_max, self.longestSubstring(s[interval[0]:interval[1]], k))
+        return g_max
+
