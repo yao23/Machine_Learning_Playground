9bbd00bab1c35b20e1d21df1257fb83a91c15833
yao <myspiritcrazy@gmail.com>
2018-02-10 21:12:53 -0800

    refactor code and documentation for Reverse String

--- LeetCode/344_reverse_string.py
+++ LeetCode/344_reverse_string.py
@@ -4,6 +4,8 @@ class Solution(object):
         :type s: str
         :rtype: str
 
+        https://leetcode.com/problems/reverse-string/discuss/80946/Python-(3-solutions:-Recursive-Classic-Pythonic)
+
         beats 51.55%
         """
         return s[::-1]
@@ -12,6 +14,8 @@ class Solution(object):
         """
         :type s: str
         :rtype: str
+
+        left, right pointers
 
         beats 19.20%
         """
@@ -23,3 +27,16 @@ class Solution(object):
             right -= 1
 
         return "".join(char_arr)
+
+    def reverseString2(self, s):
+        """
+        :type s: str
+        :rtype: str
+
+        recursive
+        beats 1.97%
+        """
+        str_len = len(s)
+        if str_len < 2:
+            return s
+        return self.reverseString(s[str_len / 2:]) + self.reverseString(s[:str_len / 2])
