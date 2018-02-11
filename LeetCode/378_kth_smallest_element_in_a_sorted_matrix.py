26c77f3d077ac6db04f437b056ac8d9a46da25b1
yao <myspiritcrazy@gmail.com>
2018-02-10 21:44:04 -0800

    refactor code and documentation for Kth Smallest Element in a Sorted Matrix

--- LeetCode/378_kth_smallest_element_in_a_sorted_matrix.py
+++ LeetCode/378_kth_smallest_element_in_a_sorted_matrix.py
@@ -1,17 +1,22 @@
-# https://discuss.leetcode.com/topic/52912/binary-search-heap-and-sorting-comparison-with-concise-code-and-1-liners-python-72-ms
+import bisect
+
+
 class Solution(object):
-    # beats 82.46%
     def kthSmallest(self, matrix, k):
         """
         :type matrix: List[List[int]]
         :type k: int
         :rtype: int
+
+        https://discuss.leetcode.com/topic/52912/binary-search-heap-and-sorting-comparison-with-concise-code-and-1-liners-python-72-ms
+
+        beats 82.46%
         """
         lo, hi = matrix[0][0], matrix[-1][-1]
-        while lo<hi:
-            mid = (lo+hi)//2
+        while lo < hi:
+            mid = (lo + hi) // 2
             if sum(bisect.bisect_right(row, mid) for row in matrix) < k:
-                lo = mid+1
+                lo = mid + 1
             else:
                 hi = mid
         return lo
