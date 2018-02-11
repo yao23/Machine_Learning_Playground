ea30dbf9bb05c7ef28bf46aea07c84166eb46794
yao <myspiritcrazy@gmail.com>
2018-02-11 00:01:14 -0800

    refactor code and documentation for Fraction Addition and Subtraction

--- LeetCode/592_fraction_addition_and_subtraction.py
+++ LeetCode/592_fraction_addition_and_subtraction.py
@@ -37,3 +37,27 @@ class Solution(object):
                 ans = add(ans, num)
                 start = i
         return ans if ans[0] != "+" else ans[1:]
+
+    def fractionAddition1(self, expression):
+        """
+        :type expression: str
+        :rtype: str
+
+        https://leetcode.com/problems/fraction-addition-and-subtraction/discuss/103383/two-python-solutions
+
+        beats 12.93%
+        """
+        def gcd(x, y):
+            while y:
+                x, y = y, x % y
+            return x
+
+        nums = map(int, re.findall('[+-]?\\d+', expression))
+        A, B = 0, 1
+        for a, b in zip(nums[::2], nums[1::2]):
+            A = A * b + B * a
+            B *= b
+            g = gcd(A, B)
+            A /= g
+            B /= g
+        return '{0}/{1}'.format(A, B)
