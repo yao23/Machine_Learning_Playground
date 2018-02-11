13aaae62c66bb0154c00a5716c4ce003e24f4d16
yao <myspiritcrazy@gmail.com>
2018-02-10 21:46:30 -0800

    refactor code and documentation for Sum of Two Integers

--- LeetCode/371_sum_of_two_integers.py
+++ LeetCode/371_sum_of_two_integers.py
@@ -1,15 +1,16 @@
 class Solution(object):
-    # beats 29.67%
     def getSum(self, a, b):
         """
         :type a: int
         :type b: int
         :rtype: int
+
+        beats 29.67%
         """
         # 32 bits integer max
-        MAX = 0x7FFFFFFF
+        max_int = 0x7FFFFFFF
         # 32 bits interger min
-        MIN = 0x80000000
+        min_int = 0x80000000
         # mask to get last 32 bits
         mask = 0xFFFFFFFF
         while b != 0:
@@ -17,4 +18,4 @@ class Solution(object):
             a, b = (a ^ b) & mask, ((a & b) << 1) & mask
         # if a is negative, get a's 32 bits complement positive first
         # then get 32-bit positive's Python complement negative
-        return a if a <= MAX else ~(a ^ mask)
+        return a if a <= max_int else ~(a ^ mask)
