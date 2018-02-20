84704b8634ab89b8a5d68bd0addb2772260da171
yao <myspiritcrazy@gmail.com>
2018-02-19 22:05:21 -0800

    refactor code and documentation for Valid Number

--- LeetCode/065_valid_number.py
+++ LeetCode/065_valid_number.py
@@ -1,32 +1,33 @@
 class Solution(object):
-    # beats 47.82%
     def isNumber(self, s):
         """
         :type s: str
         :rtype: bool
+
+        beats 47.82%
         """
         #define a DFA
         state = [{},
-              {'blank': 1, 'sign': 2, 'digit':3, '.':4},
-              {'digit':3, '.':4},
-              {'digit':3, '.':5, 'e':6, 'blank':9},
-              {'digit':5},
-              {'digit':5, 'e':6, 'blank':9},
-              {'sign':7, 'digit':8},
-              {'digit':8},
-              {'digit':8, 'blank':9},
-              {'blank':9}]
-        currentState = 1
+              {'blank': 1, 'sign': 2, 'digit': 3, '.': 4},
+              {'digit': 3, '.': 4},
+              {'digit': 3, '.': 5, 'e': 6, 'blank': 9},
+              {'digit': 5},
+              {'digit': 5, 'e': 6, 'blank': 9},
+              {'sign': 7, 'digit': 8},
+              {'digit': 8},
+              {'digit': 8, 'blank': 9},
+              {'blank': 9}]
+        current_state = 1
         for c in s:
-            if c >= '0' and c <= '9':
+            if '0' <= c <= '9':
                 c = 'digit'
             if c == ' ':
                 c = 'blank'
             if c in ['+', '-']:
                 c = 'sign'
-            if c not in state[currentState].keys():
+            if c not in state[current_state].keys():
                 return False
-            currentState = state[currentState][c]
-        if currentState not in [3,5,8,9]:
+            current_state = state[current_state][c]
+        if current_state not in [3, 5, 8, 9]:
             return False
         return True
