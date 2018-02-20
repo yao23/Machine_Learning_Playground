0d1c8b44c15ee84e1f2366923d3ff09effd81eee
yao <myspiritcrazy@gmail.com>
2018-02-19 22:09:23 -0800

    refactor code and documentation for Sqrt

--- LeetCode/069_sqrt(x).py
+++ LeetCode/069_sqrt(x).py
@@ -1,16 +1,17 @@
 class Solution(object):
-    # beats 41.93%
     def mySqrt(self, x):
         """
         :type x: int
         :rtype: int
+
+        beats 41.93%
         """
-        l, r = 0, x
-        while l <= r:
-            mid = l + (r-l)//2
-            if mid * mid <= x < (mid+1)*(mid+1):
+        left, right = 0, x
+        while left <= right:
+            mid = left + (right - left) // 2
+            if mid * mid <= x < (mid + 1) * (mid + 1):
                 return mid
             elif x < mid * mid:
-                r = mid
+                right = mid
             else:
-                l = mid + 1
+                left = mid + 1
