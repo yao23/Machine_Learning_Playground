5f5fa4d4ee4189b74f6dd4e8c8e9cd2ce0dbe33d
yao <myspiritcrazy@gmail.com>
2018-02-19 22:07:17 -0800

    refactor code and documentation for Add Binary

--- LeetCode/067_add_binary.py
+++ LeetCode/067_add_binary.py
@@ -1,16 +1,19 @@
 class Solution(object):
-    # beats 19.91%
     def addBinary(self, a, b):
         """
         :type a: str
         :type b: str
         :rtype: str
+
+        beats 19.91%
         """
-        if len(a)==0: return b
-        if len(b)==0: return a
+        if len(a) == 0:
+            return b
+        if len(b) == 0:
+            return a
         if a[-1] == '1' and b[-1] == '1':
-            return self.addBinary(self.addBinary(a[0:-1],b[0:-1]),'1')+'0'
+            return self.addBinary(self.addBinary(a[0:-1], b[0:-1]), '1')+'0'
         if a[-1] == '0' and b[-1] == '0':
-            return self.addBinary(a[0:-1],b[0:-1])+'0'
+            return self.addBinary(a[0:-1], b[0:-1]) + '0'
         else:
-            return self.addBinary(a[0:-1],b[0:-1])+'1'
+            return self.addBinary(a[0:-1], b[0:-1]) + '1'
