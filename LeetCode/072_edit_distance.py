eca1d45156cd7546cddc7dae1b24e7c308f93383
yao <myspiritcrazy@gmail.com>
2018-02-20 22:56:59 -0800

    refactor code and documentation for Edit Distance

--- LeetCode/072_edit_distance.py
+++ LeetCode/072_edit_distance.py
@@ -1,11 +1,12 @@
 class Solution(object):
-    # O(m*n) space
-    # beats 54.30%
     def minDistance(self, word1, word2):
         """
         :type word1: str
         :type word2: str
         :rtype: int
+
+        O(m*n) space
+        beats 54.30%
         """
         l1, l2 = len(word1) + 1, len(word2) + 1
         dp = [[0 for _ in xrange(l2)] for _ in xrange(l1)]
@@ -18,9 +19,15 @@ class Solution(object):
                 dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + (word1[i - 1] != word2[j - 1]))
         return dp[-1][-1]
 
-    # O(n) space with rolling array
-    # beats 87.02%
     def minDistance1(self, word1, word2):
+        """
+        :param word1:
+        :param word2:
+        :return:
+
+        O(n) space with rolling array
+        beats 87.02%
+        """
         l1, l2 = len(word1) + 1, len(word2) + 1
         pre = [0 for _ in xrange(l2)]
         for j in xrange(l2):
