f2074cbd9322159976b0cc6c7b2db14e6ff57095
yao <myspiritcrazy@gmail.com>
2018-02-22 22:03:55 -0800

    refactor code and documentation for Remove Duplicates from Sorted Array II

--- LeetCode/080_remove_duplicates_from_sorted_array_ii.py
+++ LeetCode/080_remove_duplicates_from_sorted_array_ii.py
@@ -1,13 +1,14 @@
 class Solution(object):
-    # beats 96.31%
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
         :rtype: int
+
+        beats 96.31%
         """
         i = 0
         for n in nums:
-            if i < 2 or n > nums[i-2]:
+            if i < 2 or n > nums[i - 2]:
                 nums[i] = n
                 i += 1
         return i
