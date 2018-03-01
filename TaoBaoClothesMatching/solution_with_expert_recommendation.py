b81342ac77d2d0b7c088076a172e2251bf56ed2b
yao <myspiritcrazy@gmail.com>
2018-02-28 21:46:02 -0800

    refactor code and documentation for Taobao Clothes Matching System (pythonic)

--- TaoBaoClothesMatching/solution_with_expert_recommendation.py
+++ TaoBaoClothesMatching/solution_with_expert_recommendation.py
@@ -16,7 +16,7 @@ class ClotheMatchingSystem:
 
         check item id is in clothes list or not
         """
-        clothes = clothe_list.split(",")
+        clothes = clothe_list.split(',')
         for clothe_id in clothes:
             if item_id == int(clothe_id):
                 return True
@@ -29,20 +29,20 @@ class ClotheMatchingSystem:
 
         get match list with the given item id
         """
-        with open("./data/dim_fashion_matchsets.txt") as input_file:
+        with open('./data/dim_fashion_matchsets.txt') as input_file:
             for line in input_file:
-                tmp_line_arr_1 = line.split(" ")
+                tmp_line_arr_1 = line.split(' ')
                 match_list = tmp_line_arr_1[1]
-                tmp_line_arr_2 = match_list.split(";")
+                tmp_line_arr_2 = match_list.split(';')
                 target_index = -1
-                result_list = ""
+                result_list = ''
                 for idx, clothes in enumerate(tmp_line_arr_2):
                     if self.is_in_list(item_id, clothes):
                         target_index = idx
                     else:
-                        result_list = (result_list + clothes + ";")
+                        result_list = (result_list + clothes + ';')
                 if target_index == -1:
-                    return ""
+                    return ''
                 else:
                     return result_list[:-1]
 
@@ -52,7 +52,7 @@ class ClotheMatchingSystem:
 
         test method to get matching list with item ids
         """
-        with open("./data/test_items.txt") as input_file:
+        with open('./data/test_items.txt') as input_file:
             for line in input_file:
                 item_id = int(line)
                 match_list = self.get_match_list(item_id)
