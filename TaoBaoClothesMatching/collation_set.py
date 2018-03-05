da99a9ac61c7ff03035ae69ba7cdd740352dd582
yao <myspiritcrazy@gmail.com>
2018-03-04 23:50:32 -0800

    refactor code for Taobao clothing match system (collation set based on expert data

---
+++ TaoBaoClothesMatching/collation_set.py
@@ -0,0 +1,72 @@
+""" Collation set """
+
+import constant
+import itertools
+
+
+class CollationSet(object):
+    """Collation set.
+
+       Field:
+          One example:
+              [[1,2], [3], [4,5,6]]
+    """
+
+    def __init__(self, _collation_set):
+        self._collation_set = _collation_set
+
+    def to_matched_item_pairs(self):
+        """ Convert collation set into a list of matched item pairs.
+
+            Return:
+                A list of item pairs. For example [[1,2], [3], [4,5,6]] is collation set,
+                outputs of matched item pairs are
+                [[1, 3], [2, 3], [1, 4], [1, 5], [1, 6], [3, 4], [3, 5], [3, 6]]
+        """
+        # TODO: finish the codes here
+        match_item_pairs = []
+        for idx, match_item_id_arr in enumerate(self._collation_set):
+            for match_item_id in match_item_id_arr:
+                match_item_pairs.append(self.get_match_items(idx, match_item_id, self._collation_set))
+        return match_item_pairs
+
+        # with open(constant.COLLATION_SET_FILE) as input_file:
+        #     for line in input_file:
+        #         line_arr = line.split(' ')
+        #         match_list = line_arr[1]
+        #         match_list_arr = match_list.split(';')
+        #         for idx, match_item_ids in enumerate(match_list_arr):
+        #             match_item_id_arr = match_item_ids.split(',')
+        #             for match_item_id in match_item_id_arr:
+        #                 matched_item_pairs.append(self.get_match_items(idx, match_item_id, match_list_arr))
+        # return match_item_pairs
+
+    def get_match_items(self, index, item_id, match_list_arr):
+        """
+        :param index:
+        :param match_list_arr:
+        :return:
+
+        get match items based on expert recommendation data
+        """
+        item_id = int(item_id)
+        match_item_pairs = []
+        for idx, match_item_ids in enumerate(match_list_arr):
+            if idx == index:
+                continue
+            else:
+                for match_item_id in match_item_ids:
+                    match_item_id = int(match_item_id)
+                    match_item_pairs.append([min(item_id, match_item_id), max(item_id, match_item_id)])
+        return match_item_pairs
+
+if __name__ == '__main__':
+    import util
+
+    print 'Bingo'
+    collation_list = [['1463018', '230955'],
+                      ['1596334', '1704853'],
+                      ['2226122', '284814', '36278', '480281']]
+    match_set = CollationSet(collation_list)
+    matched_item_pairs = match_set.to_matched_item_pairs()
+    print matched_item_pairs
