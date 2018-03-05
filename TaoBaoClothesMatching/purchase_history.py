126f1f5ae03e78bb01a39a9809ed250cd1ace88a
yao <myspiritcrazy@gmail.com>
2018-03-05 00:11:42 -0800

    refactor code for Taobao clothing match system (purchase history)

---
+++ TaoBaoClothesMatching/purchase_history.py
@@ -0,0 +1,67 @@
+""" Purchase history class. """
+
+import itertools
+
+class PurchaseHistory(object):
+    def __init__(self, _user_id, _items):
+        self._user_id = _user_id
+        self._items = _items
+
+    def get_user_id(self):
+        return self._user_id
+
+    def get_purchased_items(self):
+        return self._items
+
+    def add_one_item(self, item_id):
+        self._items.append(item_id)
+
+    def to_item_pairs(self):
+        """ Convert items into a list of co-purchased item pairs.
+
+            Return:
+                A list of co-purchased item pairs. For example [1, 2, 3] is user's purchased items,
+                outputs of co-purchased item pairs are
+                [[1, 2], [1, 3], [2, 3]]
+        """
+        # TODO: finish the codes here
+        item_pairs = []
+        item_set = set()
+        for idx, item_id in enumerate(self._items):
+            item_pair = self.get_item_pairs(idx, item_id, item_set)
+            if item_pair:
+                item_pairs.append(item_pair)
+            else:
+                continue
+        return list(item_pairs)
+
+    def get_item_pairs(self, index, cur_item_id, item_set):
+        """
+        :param index:
+        :param cur_item_id:
+        :param item_set:
+        :return:
+
+        get match items based on expert recommendation data
+        """
+        cur_item_id = int(cur_item_id)
+        item_pairs = []
+        for idx, item_id in enumerate(self._items):
+            if idx == index:
+                continue
+            else:
+                item_id = int(item_id)
+                item_pair = [min(item_id, cur_item_id), max(item_id, cur_item_id)]
+                item_pair_tuple = tuple(item_pair)
+                if item_pair_tuple in item_set:
+                    continue
+                else:
+                    item_set.add(item_pair_tuple)
+                    item_pairs.append(item_pair)
+        return item_pairs
+
+
+if __name__=='__main__':
+    print 'Bingo'
+    purchased_history = PurchaseHistory(_user_id='0001', _items=['1', '2', '3'])
+    print purchased_history.to_item_pairs()
