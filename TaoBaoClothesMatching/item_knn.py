a10cf7fca3211dddd1f3ae735684f032ef9385c8
yao <myspiritcrazy@gmail.com>
2018-03-17 00:38:25 -0700

    add function to learn pair relationship model for category and item

--- TaoBaoClothesMatching/item_knn.py
+++ TaoBaoClothesMatching/item_knn.py
@@ -133,3 +133,79 @@ class ItemKNN(object):
                 model_file.write(item_pair[0] + ' ' + item_pair[1] + ' ' + str(probability) + '\n')
 
         return item_pair_prob_dic
+
+    @staticmethod
+    def learn_pair_relationship_model(pairs, neigh_size=20, model_file_path=''):
+        """ Learn category or item matching relationship from matching set or purchased history,
+            and store them into a local file.
+
+            Args:
+                pairs: a list of category pairs obtained from matching set or
+                       purchased item pairs obtained from item purchased history.
+                neigh_size: neighborhood size, for each source category/item, we only need to keep its more relevant top
+                            neigh_size categories/items.
+                model_file_path: local path to write model file
+
+            Return:
+                a table with three columns like
+                source category            matched category            probability
+                1001                       1002                         0.2
+                1001                       1005                         0.5
+                1001                       1100                         0.3
+                ...                        ...                          ...
+
+                or
+
+                source item              purchased item               probability
+                1                         2                           0.15
+                1                         8                           0.6
+                1                         10                          0.15
+                1                         20                          0.1
+
+                The above table is expected to be stored into a local file.
+        """
+        pair_probability_dic = {}
+        pair_count_dic = {}
+        pair_set_dic = {}
+        for pair in pairs:
+            if pair in pair_count_dic:
+                pair_count_dic[pair] += 1
+            else:
+                pair_count_dic[pair] = 1
+            pair_element_one = pair[0]
+            pair_element_two = pair[1]
+            if pair_element_one in pair_set_dic:
+                pair_set = pair_set_dic[pair_element_one]
+                if pair_element_two not in pair_set:
+                    pair_set.add(pair_element_two)
+                else:
+                    continue
+            else:
+                pair_set = set()
+                pair_set.add(pair_element_two)
+                pair_set_dic[pair_element_one] = pair_set
+
+        pair_count = 0
+        for element_id, pair_set in pair_set_dic.items():
+            if len(pair_set) <= neigh_size:
+                for match_element_id in pair_set:
+                    pair = (element_id, match_element_id)
+                    pair_count += pair_count_dic[pair]
+                    pair_probability_dic[pair] = 0
+            else:
+                tmp_pair_dic = {}
+                for match_element_id in pair_set:
+                    pair = (element_id, match_element_id)
+                    tmp_pair_dic[pair] = pair_count_dic[pair]
+                tmp_pair_counter = collections.Counter(tmp_pair_dic)
+                for pair, pair_count in tmp_pair_counter.most_common(neigh_size):
+                    pair_count += pair_count_dic[pair]
+                    pair_probability_dic[pair] = 0
+
+        with open(model_file_path, 'a') as model_file:
+            for pair in pair_probability_dic:
+                probability = 1.0 * pair_count_dic[pair] / pair_count
+                pair_probability_dic[pair] = probability
+                model_file.write(pair[0] + ' ' + pair[1] + ' ' + str(probability) + '\n')
+
+        return pair_probability_dic
