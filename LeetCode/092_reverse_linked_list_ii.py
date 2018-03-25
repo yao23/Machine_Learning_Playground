dc6769f385155e3b642881ae4035f8d44dc88703
Yao <myspiritcrazy@gmail.com>
2018-03-25 09:12:34 -0700

    refactor code for  Reverse Linked List II

--- LeetCode/092_reverse_linked_list_ii.py
+++ LeetCode/092_reverse_linked_list_ii.py
@@ -6,20 +6,21 @@ class ListNode(object):
 
 
 class Solution(object):
-    # The idea is simple and intuitive: find linkedlist [m, n], reverse it,
-    # then connect m with n+1, connect n with m-1
-    # @param head, a ListNode
-    # @param m, an integer
-    # @param n, an integer
-    # @return a ListNode
-    # input: 1->2->3->4->5, m = 2, n = 4, return 1->4->3->2->5
-    # beats 43.68%
     def reverseBetween(self, head, m, n):
         """
         :type head: ListNode
         :type m: int
         :type n: int
         :rtype: ListNode
+
+        # The idea is simple and intuitive: find linkedlist [m, n], reverse it,
+        # then connect m with n+1, connect n with m-1
+        # @param head, a ListNode
+        # @param m, an integer
+        # @param n, an integer
+        # @return a ListNode
+        # input: 1->2->3->4->5, m = 2, n = 4, return 1->4->3->2->5
+        # beats 43.68%
         """
         if m == n:
             return head
