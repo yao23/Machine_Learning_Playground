4cd0747e7fcc6f5fb90928af34edd259026a6a41
yao <myspiritcrazy@gmail.com>
2018-03-28 21:29:57 -0700

    refactor code for Unique Binary Search Trees

--- LeetCode/096_unique_binary_search_trees.py
+++ LeetCode/096_unique_binary_search_trees.py
@@ -2,21 +2,23 @@ import math
 
 
 class Solution(object):
-    # Catalan Number  (2n)!/((n+1)!*n!)
-    # beats 78.51%
     def numTrees(self, n):
         """
         :type n: int
         :rtype: int
+
+        Catalan Number  (2n)!/((n+1)!*n!)
+        beats 78.51%
         """
         return math.factorial(2 * n) / (math.factorial(n) * math.factorial(n + 1))
 
-    # DP
-    # beats 32.81%
     def numTrees1(self, n):
         """
         :type n: int
         :rtype: int
+
+        DP
+        beats 32.81%
         """
         res = [0] * (n + 1)
         res[0] = 1
