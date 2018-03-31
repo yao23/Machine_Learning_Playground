37b368aed487ebd37af5f659b857b7b9f73fb127
yao <myspiritcrazy@gmail.com>
2018-03-30 21:20:30 -0700

    refactor code for Validate binary search tree

--- LeetCode/098_validate_binary_search_tree.py
+++ LeetCode/098_validate_binary_search_tree.py
@@ -5,16 +5,18 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution(object):
-    # beats 79.81%
     def isValidBST(self, root, lessThan = float('inf'), largerThan = float('-inf')):
         """
         :type root: TreeNode
         :rtype: bool
+
+        beats 79.81%
         """
         if not root:
             return True
         if root.val <= largerThan or root.val >= lessThan:
             return False
         return self.isValidBST(root.left, min(lessThan, root.val), largerThan) and \
-               self.isValidBST(root.right, lessThan, max(root.val, largerThan))
+            self.isValidBST(root.right, lessThan, max(root.val, largerThan))
