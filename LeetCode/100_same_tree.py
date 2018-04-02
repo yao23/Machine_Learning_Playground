f6d1053936f2616f813ac1cea7f6f3f2f8043b0c
yao <myspiritcrazy@gmail.com>
2018-04-01 17:20:41 -0700

    refactor code for Same Tree

--- LeetCode/100_same_tree.py
+++ LeetCode/100_same_tree.py
@@ -5,24 +5,27 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution(object):
-    # beats 25.98%
     def isSameTree(self, p, q):
         """
         :type p: TreeNode
         :type q: TreeNode
         :rtype: bool
+
+        beats 25.98%
         """
         if p and q:
             return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         return p is q
 
-    # beats 47.51%
     def isSameTree1(self, p, q):
         """
         :type p: TreeNode
         :type q: TreeNode
         :rtype: bool
+
+        beats 47.51%
         """
 
         def t(n):
@@ -30,11 +33,12 @@ class Solution(object):
 
         return t(p) == t(q)
 
-    # beats 32.56%
     def isSameTree2(self, p, q):
         """
         :type p: TreeNode
         :type q: TreeNode
         :rtype: bool
+
+        beats 32.56%
         """
         return p and q and p.val == q.val and all(map(self.isSameTree, (p.left, p.right), (q.left, q.right))) or p is q
