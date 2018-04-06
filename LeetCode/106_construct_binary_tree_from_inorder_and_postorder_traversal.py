e5d5f24634374db4ab1342942d74e5edad7f889b
yao <myspiritcrazy@gmail.com>
2018-04-06 15:51:34 -0700

    refactor code for Construct Binary Tree from Postorder and Inorder traversal

--- LeetCode/106_construct_binary_tree_from_inorder_and_postorder_traversal.py
+++ LeetCode/106_construct_binary_tree_from_inorder_and_postorder_traversal.py
@@ -1,28 +1,30 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution(object):
-    # @param inorder, a list of integers
-    # @param postorder, a list of integers
-    # @return a tree node
-    # beats 77.50%
     def buildTree(self, inorder, postorder):
         """
         :type inorder: List[int]
         :type postorder: List[int]
         :rtype: TreeNode
+
+        @param inorder, a list of integers
+        @param postorder, a list of integers
+        @return a tree node
+        beats 77.50%
         """
         if not inorder or not postorder:
             return None
 
         root = TreeNode(postorder.pop())
-        inorderIndex = inorder.index(root.val)
+        inorder_index = inorder.index(root.val)
 
-        root.right = self.buildTree(inorder[inorderIndex + 1:], postorder)
-        root.left = self.buildTree(inorder[:inorderIndex], postorder)
+        root.right = self.buildTree(inorder[inorder_index + 1:], postorder)
+        root.left = self.buildTree(inorder[:inorder_index], postorder)
 
         return root
