d0b3bcba11f8eee8672348c6b20bc16ecf4fe629
yao <myspiritcrazy@gmail.com>
2018-04-08 20:18:27 -0700

    refactor code for Convert Sorted Array to Binary Search Tree

--- LeetCode/108_convert_sorted_array_to_binary_search_tree.py
+++ LeetCode/108_convert_sorted_array_to_binary_search_tree.py
@@ -1,18 +1,20 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
 
 class Solution(object):
-    # @param num, a list of integers
-    # @return a tree node
-    # beats 56.15%
     def sortedArrayToBST(self, nums):
         """
         :type nums: List[int]
         :rtype: TreeNode
+
+        @param nums, a list of integers
+        @return a tree node
+        beats 56.15%
         """
         if not nums:
             return None
