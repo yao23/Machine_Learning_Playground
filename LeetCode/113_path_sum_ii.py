6720a30f659a95d0fbb844ded6e5f0ec5e57ff11
yao <myspiritcrazy@gmail.com>
2018-04-13 20:54:26 -0700

    refactor code for Path Sum II

--- LeetCode/113_path_sum_ii.py
+++ LeetCode/113_path_sum_ii.py
@@ -5,13 +5,15 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution(object):
-    # beats 39.96%
     def pathSum(self, root, sum):
         """
         :type root: TreeNode
         :type sum: int
         :rtype: List[List[int]]
+
+        beats 39.96%
         """
         if not root:
             return []
