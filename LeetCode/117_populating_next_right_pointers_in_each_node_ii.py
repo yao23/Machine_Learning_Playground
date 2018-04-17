4841a128d33344dbe05605344b3182aac7bee16d
yao <myspiritcrazy@gmail.com>
2018-04-16 20:46:10 -0700

    refactor code for Populating Next Pointers in Each Node II

--- LeetCode/117_populating_next_right_pointers_in_each_node_ii.py
+++ LeetCode/117_populating_next_right_pointers_in_each_node_ii.py
@@ -1,16 +1,22 @@
 # Definition for binary tree with next pointer.
-# class TreeLinkNode:
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-#         self.next = None
+class TreeLinkNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+        self.next = None
+
 
 class Solution:
-    # @param root, a tree link node
-    # @return nothing
-    # beats 30.91%
     def connect(self, root):
+        """
+        :param root:
+        :return:
+
+        @param root, a tree link node
+        @return nothing
+        beats 30.91%
+        """
         tail = dummy = TreeLinkNode(0)
         while root:
             tail.next = root.left
