ae52aaf0193222ff58354a6e6da9cee8e66f0e8c
yao <myspiritcrazy@gmail.com>
2018-04-17 20:40:35 -0700

    refactor code for Pascals Triangle

--- LeetCode/118_pascals_triangle.py
+++ LeetCode/118_pascals_triangle.py
@@ -1,9 +1,10 @@
 class Solution(object):
-    # beats 20.39%
     def generate(self, numRows):
         """
         :type numRows: int
         :rtype: List[List[int]]
+
+        beats 20.39%
         """
         res = [[1]]
         for i in range(1, numRows):
