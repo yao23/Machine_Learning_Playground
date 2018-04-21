27442633ca5b1786f0def341d4b83d14bd847df9
yao <myspiritcrazy@gmail.com>
2018-04-20 18:32:39 -0700

    refactor code for Binary Tree Maximum Path Sum

--- LeetCode/124_binary_tree_maximum_path_sum.py
+++ LeetCode/124_binary_tree_maximum_path_sum.py
@@ -1,11 +1,12 @@
-# Definition for a binary tree node.
-# class TreeNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.left = None
-#         self.right = None
-
 import sys
+
+
+# Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
 
 
 class Solution(object):
