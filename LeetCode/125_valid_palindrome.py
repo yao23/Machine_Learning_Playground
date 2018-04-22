e24e8dd52c31246e4d32d0970c55726ba8a4ff4b
yao <myspiritcrazy@gmail.com>
2018-04-21 21:18:41 -0700

    refactor code for Valid Palindrome

--- LeetCode/125_valid_palindrome.py
+++ LeetCode/125_valid_palindrome.py
@@ -1,17 +1,18 @@
 class Solution(object):
-    # beats 40.95%
     def isPalindrome(self, s):
         """
         :type s: str
         :rtype: bool
+
+        beats 40.95%
         """
-        l, r = 0, len(s)-1
-        while l < r:
-            while l < r and not s[l].isalnum():
-                l += 1
-            while l <r and not s[r].isalnum():
-                r -= 1
-            if s[l].lower() != s[r].lower():
+        left, right = 0, len(s)-1
+        while left < right:
+            while left < right and not s[left].isalnum():
+                left += 1
+            while left < right and not s[right].isalnum():
+                right -= 1
+            if s[left].lower() != s[right].lower():
                 return False
-            l +=1; r -= 1
+            left += 1; right -= 1
         return True
