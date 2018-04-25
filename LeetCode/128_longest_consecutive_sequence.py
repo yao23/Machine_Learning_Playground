5718d91a0b3032f77387b72a6d2e0d4c5e5a4c0e
yao <myspiritcrazy@gmail.com>
2018-04-24 20:02:39 -0700

    refactor code for Longest Consecutive Sequence

--- LeetCode/128_longest_consecutive_sequence.py
+++ LeetCode/128_longest_consecutive_sequence.py
@@ -1,27 +1,28 @@
 class Solution(object):
-    # @param num, a list of integer
-    # @return an integer
-    # beats 77.49%
     def longestConsecutive(self, nums):
         """
         :type nums: List[int]
         :rtype: int
+
+        @param num, a list of integer
+        @return an integer
+        beats 77.49%
         """
-        nums=set(nums)
-        maxLen=0
+        nums = set(nums)
+        max_len = 0
         while nums:
-            n=nums.pop()
-            i=n+1
-            l1=0
-            l2=0
+            n = nums.pop()
+            i = n+1
+            l1 = 0
+            l2 = 0
             while i in nums:
                 nums.remove(i)
-                i+=1
-                l1+=1
-            i=n-1
+                i += 1
+                l1 += 1
+            i = n - 1
             while i in nums:
                 nums.remove(i)
-                i-=1
-                l2+=1
-            maxLen=max(maxLen,l1+l2+1)
-        return maxLen
+                i -= 1
+                l2 += 1
+            max_len = max(max_len, l1 + l2 + 1)
+        return max_len
