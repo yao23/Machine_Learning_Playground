32366ef3bd7739de6b26d86e507bf38f8e21c3e7
yao <myspiritcrazy@gmail.com>
2018-05-01 20:01:08 -0700

    refactor code for Single Number II

--- LeetCode/137_single_number_ii.py
+++ LeetCode/137_single_number_ii.py
@@ -1,13 +1,14 @@
 class Solution(object):
-    # @param {integer[]} nums
-    # @return {integer}
-    # beats 84.85%
     def singleNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
+
+        @param {integer[]} nums
+        @return {integer}
+        beats 84.85%
         """
-        a= set(nums)
-        a = sum(a)*3 -sum(nums)
+        a = set(nums)
+        a = sum(a) * 3 - sum(nums)
         a = a/2
         return a
