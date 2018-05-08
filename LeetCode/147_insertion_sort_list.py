a79180d348f9b1e30505be5f85d97c75f1281fd3
yao <myspiritcrazy@gmail.com>
2018-05-07 22:42:41 -0700

    refactor code for Insertion Sort List

--- LeetCode/147_insertion_sort_list.py
+++ LeetCode/147_insertion_sort_list.py
@@ -1,15 +1,17 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution(object):
-    # beats 91.67%
     def insertionSortList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
+
+        beats 91.67%
         """
         p = dummy = ListNode(0)
         cur = dummy.next = head
