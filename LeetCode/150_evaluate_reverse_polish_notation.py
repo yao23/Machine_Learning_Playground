873862bd2cc0d81e6908269b34c9a8862a7cc791
yao <myspiritcrazy@gmail.com>
2018-05-09 20:36:25 -0700

    refactor code for Evaluate Reverse Polish Notation

--- LeetCode/150_evaluate_reverse_polish_notation.py
+++ LeetCode/150_evaluate_reverse_polish_notation.py
@@ -1,28 +1,29 @@
 class Solution(object):
-    # beats 56.21%
     def evalRPN(self, tokens):
         """
         :type tokens: List[str]
         :rtype: int
+
+        beats 56.21%
         """
         stack = []
-        for t in tokens:
-            if t not in ["+", "-", "*", "/"]:
-                stack.append(int(t))
+        for token in tokens:
+            if token not in ["+", "-", "*", "/"]:
+                stack.append(int(token))
             else:
-                r, l = stack.pop(), stack.pop()
-                if t == "+":
-                    stack.append(l+r)
-                elif t == "-":
-                    stack.append(l-r)
-                elif t == "*":
-                    stack.append(l*r)
+                right, left = stack.pop(), stack.pop()
+                if token == "+":
+                    stack.append(left + right)
+                elif token == "-":
+                    stack.append(left - right)
+                elif token == "*":
+                    stack.append(left * right)
                 else:
                     # here take care of the case like "1/-22",
                     # in Python 2.x, it returns -1, while in
                     # Leetcode it should return 0
-                    if l*r < 0 and l % r != 0:
-                        stack.append(l/r+1)
+                    if left * right < 0 and left % right != 0:
+                        stack.append(left / right + 1)
                     else:
-                        stack.append(l/r)
+                        stack.append(left / right)
         return stack.pop()
