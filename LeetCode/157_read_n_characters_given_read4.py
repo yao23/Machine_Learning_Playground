0217cdc2417c83812c5e5be806efc7dbd912011d
yao <myspiritcrazy@gmail.com>
2018-05-14 20:51:13 -0700

    refactor code for Read n Characters Given Read4

--- LeetCode/157_read_n_characters_given_read4.py
+++ LeetCode/157_read_n_characters_given_read4.py
@@ -1,22 +1,30 @@
+import math
+
+
 # The read4 API is already defined for you.
-# @param buf, a list of characters
-# @return an integer
-# def read4(buf):
+def read4(buf):
+    """
+    :param buf: a list of characters
+    :return: an integer
+    """
+    pass
+
 
 class Solution(object):
-    # beats 98.02%
     def read(self, buf, n):
         """
         :type buf: Destination buffer (List[str])
         :type n: Maximum number of characters to read (int)
         :rtype: The number of characters read (int)
+
+        beats 98.02%
         """
         idx = 0
         while True:
             buf4 = [""]*4
-            curr = min(read4(buf4),n-idx)  # curr is the number of chars that reads
+            curr = math.min(read4(buf4), n - idx)  # curr is the number of chars that reads
             for i in xrange(curr):
                 buf[idx] = buf4[i]
-                idx+=1
-            if curr!=4 or idx==n:  # return if it reaches the end of file or reaches n
+                idx += 1
+            if curr != 4 or idx == n:  # return if it reaches the end of file or reaches n
                 return idx
