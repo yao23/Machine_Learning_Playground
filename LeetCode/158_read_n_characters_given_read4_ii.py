a700e090dc334df6e95d52a4104c610b20fa366d
yao <myspiritcrazy@gmail.com>
2018-05-15 20:40:48 -0700

    refactor code for Read n Characters Given Read4 II

--- LeetCode/158_read_n_characters_given_read4_ii.py
+++ LeetCode/158_read_n_characters_given_read4_ii.py
@@ -1,21 +1,23 @@
 # The read4 API is already defined for you.
-# @param buf, a list of characters
-# @return an integer
-# def read4(buf):
+def read4(buf):
+    """
+    :param buf: a list of characters
+    :return: an integer
+    """
+    pass
+
 
 class Solution(object):
-    # @param buf, Destination buffer (a list of characters)
-    # @param n,   Maximum number of characters to read (an integer)
-    # @return     The number of characters read (an integer)
     def __init__(self):
         self.queue = []
 
-    # beats 56.25%
     def read(self, buf, n):
         """
         :type buf: Destination buffer (List[str])
         :type n: Maximum number of characters to read (int)
         :rtype: The number of characters read (int)
+
+        beats 56.25%
         """
         tmpN = n
         ans = 0
@@ -25,7 +27,7 @@ class Solution(object):
             self.queue += tmpBuf # same as self.queue.extend(tmpBuf)
             read_len = min(len(self.queue), n-ans)
             for i in xrange(read_len):
-                buf[ans+i] = self.queue.pop(0) # from the first element
+                buf[ans+i] = self.queue.pop(0)  # from the first element
 
             tmpN -= read_len
             ans += read_len
