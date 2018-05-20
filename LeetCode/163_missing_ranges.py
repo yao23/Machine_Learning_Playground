d5fe7298ce57168f0394fafe6d9be425c4779ca0
yao <myspiritcrazy@gmail.com>
2018-05-19 19:58:23 -0700

    refactor code for Missing Range

--- LeetCode/163_missing_ranges.py
+++ LeetCode/163_missing_ranges.py
@@ -1,19 +1,20 @@
 class Solution(object):
-    # beats 27.01%
     def findMissingRanges(self, nums, lower, upper):
         """
         :type nums: List[int]
         :type lower: int
         :type upper: int
         :rtype: List[str]
+
+        beats 27.01%
         """
         result = []
         nums.append(upper+1)
         pre = lower - 1
         for i in nums:
-            if (i == pre + 2):
+            if i == pre + 2:
                 result.append(str(i-1))
-            elif (i > pre + 2):
+            elif i > pre + 2:
                 result.append(str(pre + 1) + "->" + str(i -1))
             pre = i
         return result
