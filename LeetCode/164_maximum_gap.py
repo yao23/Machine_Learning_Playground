57ada7aae439ed402900c4c82d0d9c8a528f3c76
yao <myspiritcrazy@gmail.com>
2018-05-20 22:11:07 -0700

    refactor code for Maximum Gap

--- LeetCode/164_maximum_gap.py
+++ LeetCode/164_maximum_gap.py
@@ -1,14 +1,19 @@
 class Solution(object):
-    # @param nums, a list of integer
-    # @return an integer
     def maximumGap(self, nums):
+        """
+        :param nums:
+        :return:
+
+        @param nums, a list of integer
+        @return an integer
+        """
         if len(nums) < 2 or max(nums) - min(nums) == 0:  # in case bsize == 0
             return 0
-        maxn, minn, lenn = max(nums), min(nums), len(nums)
-        bsize = (maxn - minn + 1.0) / lenn  # could have integer issue on OJ
-        buckets = [[2 ** 31 - 1, -1] for _ in range(lenn + 1)]
+        max_n, min_n, len_n = max(nums), min(nums), len(nums)
+        bsize = (max_n - min_n + 1.0) / len_n  # could have integer issue on OJ
+        buckets = [[2 ** 31 - 1, -1] for _ in range(len_n + 1)]
         for i in nums:
-            place = int((i - minn) // bsize)
+            place = int((i - min_n) // bsize)
             buckets[place][0] = min(i, buckets[place][0])
             buckets[place][1] = max(i, buckets[place][1])
         res, prev = 0, buckets[0][0]
