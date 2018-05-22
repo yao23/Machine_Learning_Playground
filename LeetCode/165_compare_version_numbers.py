d3ca5bd5ce4f3abe436a2154c76c3cfbeecd9c8b
yao <myspiritcrazy@gmail.com>
2018-05-21 21:15:19 -0700

    refactor code for Compare Version Number

--- LeetCode/165_compare_version_numbers.py
+++ LeetCode/165_compare_version_numbers.py
@@ -1,12 +1,13 @@
 class Solution(object):
-    # beats 91.38%
     def compareVersion(self, version1, version2):
         """
         :type version1: str
         :type version2: str
         :rtype: int
+
+        beats 91.38%
         """
         main1, _, rest1 = ('0' + version1).partition('.')
         main2, _, rest2 = ('0' + version2).partition('.')
         return cmp(int(main1), int(main2)) or \
-               len(rest1+rest2) and self.compareVersion(rest1, rest2)
+               len(rest1 + rest2) and self.compareVersion(rest1, rest2)
