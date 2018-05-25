5d1431b6f25708bc60782306beea140bdeeec752
yao <myspiritcrazy@gmail.com>
2018-05-24 19:58:22 -0700

    refactor code for Excel Sheet Column Title

--- LeetCode/168_excel_sheet_column_title.py
+++ LeetCode/168_excel_sheet_column_title.py
@@ -1,14 +1,15 @@
 class Solution(object):
-    # beats 46.90%
     def convertToTitle(self, num):
         """
         :type n: int
         :rtype: str
+
+        beats 46.90%
         """
         capitals = [chr(x) for x in range(ord('A'), ord('Z')+1)]
         result = []
         while num > 0:
-            result.append(capitals[(num-1)%26])
+            result.append(capitals[(num-1) % 26])
             num = (num-1) // 26
         result.reverse()
         return ''.join(result)
