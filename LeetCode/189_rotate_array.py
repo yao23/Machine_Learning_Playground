aae606fc92fa1e6b202aeae8264cb49f0df14114
yao <myspiritcrazy@gmail.com>
2018-05-29 20:13:38 -0700

    refactor code for Rotate Array

--- LeetCode/189_rotate_array.py
+++ LeetCode/189_rotate_array.py
@@ -4,8 +4,9 @@ class Solution(object):
         :type nums: List[int]
         :type k: int
         :rtype: void Do not return anything, modify nums in-place instead.
+
+        beats 97.46%
         """
-        # beats 97.46%
         n = len(nums)
         k = k % n
         nums[:] = nums[n-k:] + nums[:n-k]
