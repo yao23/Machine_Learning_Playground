9705829bb10c7608412ad28041c0aaefd2251b86
yao <myspiritcrazy@gmail.com>
2018-05-30 19:49:25 -0700

    refactor code for Reverse Bits

--- LeetCode/190_reverse_bits.py
+++ LeetCode/190_reverse_bits.py
@@ -1,8 +1,13 @@
 class Solution:
-    # @param n, an integer
-    # @return an integer
-    # beats 86.13%
     def reverseBits(self, n):
-        oribin='{0:032b}'.format(n)
-        reversebin=oribin[::-1]
-        return int(reversebin,2)
+        """
+        :param n:
+        :return:
+        @param n, an integer
+        @return an integer
+
+        beats 86.13%
+        """
+        ori_bin = '{0:032b}'.format(n)
+        reverse_bin = ori_bin[::-1]
+        return int(reverse_bin, 2)
