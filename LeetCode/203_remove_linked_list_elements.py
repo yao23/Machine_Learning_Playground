6400f900c762fe4ac8b83a089998ebb6afbdba96
yao <myspiritcrazy@gmail.com>
2018-06-03 17:26:41 -0700

    refactor code for Remove LinkedIn List Elements

--- LeetCode/203_remove_linked_list_elements.py
+++ LeetCode/203_remove_linked_list_elements.py
@@ -1,28 +1,31 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution(object):
-    # @param {ListNode} head
-    # @param {integer} val
-    # @return {ListNode}
     def removeElements(self, head, val):
         """
         :type head: ListNode
         :type val: int
         :rtype: ListNode
+
+        @param {ListNode} head
+        @param {integer} val
+        @return {ListNode}
+
+        beats 62.13%
         """
-        #beats 62.13%
         dummy = ListNode(-1)
         dummy.next = head
-        next = dummy
+        next_node = dummy
 
-        while next != None and next.next != None:
-            if next.next.val == val:
-                next.next = next.next.next
+        while next_node is not None and next_node.next is not None:
+            if next_node.next.val == val:
+                next_node.next = next_node.next.next
             else:
-                next = next.next
+                next_node = next_node.next
 
         return dummy.next
