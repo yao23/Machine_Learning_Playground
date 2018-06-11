bf3afeceda29fcd32baeaf7945ac5dfe4c4fd70c
yao <myspiritcrazy@gmail.com>
2018-06-10 20:09:55 -0700

    refactor code for Shortest Palindrome

--- LeetCode/214_shortest_palindrome.py
+++ LeetCode/214_shortest_palindrome.py
@@ -1,9 +1,10 @@
 class Solution(object):
-    # beats 20.68%
     def shortestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
+
+        beats 20.68%
         """
         r = s[::-1]
         for i in range(len(s) + 1):
