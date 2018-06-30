d719b4d9da9351298714fc2077b301a758472482
yao <myspiritcrazy@gmail.com>
2018-06-29 18:53:37 -0700

    refactor code for Search a 2D Matrix II

--- LeetCode/240_search_a_2d_matrix_ii.py
+++ LeetCode/240_search_a_2d_matrix_ii.py
@@ -1,18 +1,19 @@
 class Solution(object):
-    # beats 44.95%
     def searchMatrix(self, mat, x):
         """
         :type matrix: List[List[int]]
         :type target: int
         :rtype: bool
+
+        beats 44.95%
         """
         if mat:
             m, n, r, c = len(mat), len(mat[0]), 0,  len(mat[0]) - 1
-            while r<m and c>=0:
-                if mat[r][c]==x:
+            while r < m and c >= 0:
+                if mat[r][c] == x:
                     return True
-                if mat[r][c]<x:
-                    r+=1
+                if mat[r][c] < x:
+                    r += 1
                 else:
-                    c-=1
+                    c -= 1
         return False
