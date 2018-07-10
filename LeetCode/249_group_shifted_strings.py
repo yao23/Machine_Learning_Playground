c587788b9a8d4d477f692be13e9193c113ff8c69
yao <myspiritcrazy@gmail.com>
2018-07-09 20:57:39 -0700

    refactor code for Group Shifted Strings

--- LeetCode/249_group_shifted_strings.py
+++ LeetCode/249_group_shifted_strings.py
@@ -1,9 +1,13 @@
+import itertools
+
+
 class Solution(object):
-    # beats 46.08%
     def groupStrings(self, strings):
         """
         :type strings: List[str]
         :rtype: List[List[str]]
+
+        beats 46.08%
         """
         key = lambda s: [(ord(c) - ord(s[0])) % 26 for c in s]
         return [list(g) for _, g in itertools.groupby(sorted(strings, key=key), key)]
