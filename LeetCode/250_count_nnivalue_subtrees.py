16b4250be92cd3bd0c21f1d7c23d94501b3a0c2c
yao <myspiritcrazy@gmail.com>
2018-07-10 23:15:04 -0700

    refactor code for Count Nnivalue Subtrees

--- LeetCode/250_count_nnivalue_subtrees.py
+++ LeetCode/250_count_nnivalue_subtrees.py
@@ -5,22 +5,29 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution(object):
-    # beats 86.26%
     def countUnivalSubtrees(self, root):
         """
         :type root: TreeNode
         :rtype: int
+
+        beats 86.26%
         """
         self.count = 0
         self.checkUni(root)
         return self.count
 
-    # bottom-up, first check the leaf nodes and count them,
-    # then go up, if both children are "True" and root.val is
-    # equal to both children's values if exist, then root node
-    # is uniValue suntree node.
     def checkUni(self, root):
+        """
+        :param root:
+        :return:
+
+        bottom-up, first check the leaf nodes and count them,
+        then go up, if both children are "True" and root.val is
+        equal to both children's values if exist, then root node
+        is uniValue suntree node.
+        """
         if not root:
             return True
         l, r = self.checkUni(root.left), self.checkUni(root.right)
