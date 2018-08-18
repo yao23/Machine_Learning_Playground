39639bc1e7db8b3333af84a654398cca08f422b2
yao <myspiritcrazy@gmail.com>
2018-08-17 23:06:23 -0700

    refactor code for Palindrome Permutation

--- LeetCode/266_palindrome_permutation.py
+++ LeetCode/266_palindrome_permutation.py
@@ -1,8 +1,12 @@
+import collections
+
+
 class Solution(object):
-    # beats 49.02%
     def canPermutePalindrome(self, s):
         """
         :type s: str
         :rtype: bool
+
+        beats 49.02%
         """
         return sum(v % 2 for v in collections.Counter(s).values()) < 2
