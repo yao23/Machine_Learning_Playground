d690d4ab89e8df75afe1ba0035c27c079bf7dff7
yao <myspiritcrazy@gmail.com>
2018-08-25 09:24:48 -0700

    refactor code for h-Index II

--- LeetCode/275_h-Index_ii.py
+++ LeetCode/275_h-Index_ii.py
@@ -1,16 +1,17 @@
 class Solution(object):
-    # beats 17.02%
     def hIndex(self, citations):
         """
         :type citations: List[int]
         :rtype: int
+
+        beats 17.02%
         """
         n = len(citations)
-        l, r = 0, n - 1
-        while l <= r:
-            mid = (l + r) / 2
+        left, right = 0, n - 1
+        while left <= right:
+            mid = (left + right) / 2
             if citations[mid] >= n - mid:
-                r = mid - 1
+                right = mid - 1
             else:
-                l = mid + 1
-        return n - l
+                left = mid + 1
+        return n - left
