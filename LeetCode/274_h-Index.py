86006248caaf2e378d2adfeb75137c5d12a39c59
yao <myspiritcrazy@gmail.com>
2018-08-24 19:55:49 -0700

    refactor code for h-Index

--- LeetCode/274_h-Index.py
+++ LeetCode/274_h-Index.py
@@ -1,22 +1,23 @@
 class Solution(object):
-    # beats 57.86%
     def hIndex(self, citations):
         """
         :type citations: List[int]
         :rtype: int
+
+        beats 57.86%
         """
         n = len(citations)
-        citeCount = [0] * (n + 1)
+        cite_count = [0] * (n + 1)
         for c in citations:
             if c >= n:
-                citeCount[n] += 1
+                cite_count[n] += 1
             else:
-                citeCount[c] += 1
+                cite_count[c] += 1
 
         i = n - 1
         while i >= 0:
-            citeCount[i] += citeCount[i + 1]
-            if citeCount[i + 1] >= i + 1:
+            cite_count[i] += cite_count[i + 1]
+            if cite_count[i + 1] >= i + 1:
                 return i + 1
             i -= 1
         return 0
