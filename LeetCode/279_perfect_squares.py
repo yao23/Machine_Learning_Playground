5d3cd64d3a3f858a216e227c2b19c634ec3459cd
yao <myspiritcrazy@gmail.com>
2018-08-28 18:23:38 -0700

    refactor code for Perfect Squares

--- LeetCode/279_perfect_squares.py
+++ LeetCode/279_perfect_squares.py
@@ -1,29 +1,30 @@
 class Solution(object):
-    # beats 89.17%
     def numSquares(self, n):
         """
         :type n: int
         :rtype: int
+
+        beats 89.17%
         """
         if n < 2:
             return n
         lst = []
         i = 1
         while i * i <= n:
-            lst.append( i * i )
+            lst.append(i * i)
             i += 1
         cnt = 0
-        toCheck = {n}
-        while toCheck:
+        to_check = {n}
+        while to_check:
             cnt += 1
             temp = set()
-            for x in toCheck:
+            for x in to_check:
                 for y in lst:
                     if x == y:
                         return cnt
                     if x < y:
                         break
-                    temp.add(x-y)
-            toCheck = temp
+                    temp.add(x - y)
+            to_check = temp
 
         return cnt
