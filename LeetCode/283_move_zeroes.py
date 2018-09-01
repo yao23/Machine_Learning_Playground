c64a90f766c6e99f159f94c3305c01f5e00ef424
yao <myspiritcrazy@gmail.com>
2018-09-01 09:34:41 -0700

    refactor code for Move Zeroes

--- LeetCode/283_move_zeroes.py
+++ LeetCode/283_move_zeroes.py
@@ -1,10 +1,11 @@
 class Solution(object):
-    # in-place
-    # beats 90.50%
     def moveZeroes(self, nums):
         """
         :type nums: List[int]
         :rtype: void Do not return anything, modify nums in-place instead.
+
+        in-place
+        beats 90.50%
         """
         zero = 0  # records the position of "0"
         for i in xrange(len(nums)):
