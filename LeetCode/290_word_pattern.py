4b5b957e76060f7361e9ac034ff7d6a75af49e0c
yao <myspiritcrazy@gmail.com>
2018-09-05 20:05:29 -0700

    refactor code for Word Pattern

--- LeetCode/290_word_pattern.py
+++ LeetCode/290_word_pattern.py
@@ -1,10 +1,11 @@
 class Solution(object):
-    # beats 83.67%
     def wordPattern(self, pattern, str):
         """
         :type pattern: str
         :type str: str
         :rtype: bool
+
+        beats 83.67%
         """
         f = lambda s: map({}.setdefault, s, range(len(s)))
         return f(pattern) == f(str.split())
