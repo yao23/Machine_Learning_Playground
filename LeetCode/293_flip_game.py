f1752bfa408866fc799a66e5ff4acac34ed4bd1f
yao <myspiritcrazy@gmail.com>
2018-09-08 19:46:01 -0700

    refactor code for Flip Game

--- LeetCode/293_flip_game.py
+++ LeetCode/293_flip_game.py
@@ -1,8 +1,9 @@
 class Solution(object):
-    # beats 89.85%
     def generatePossibleNextMoves(self, s):
         """
         :type s: str
         :rtype: List[str]
+
+        beats 89.85%
         """
         return [s[:i] + "--" + s[i + 2:] for i in range(len(s) - 1) if s[i] == s[i + 1] == "+"]
