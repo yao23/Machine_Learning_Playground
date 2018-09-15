22205c26c2ea522dd8cffb4af1d9c030bc280b72
yao <myspiritcrazy@gmail.com>
2018-09-14 23:40:26 -0700

    refactor code for Bulls and Cows

--- LeetCode/299_bulls_and_cows.py
+++ LeetCode/299_bulls_and_cows.py
@@ -1,12 +1,14 @@
 from collections import Counter
 
+
 class Solution(object):
-    # beats 60.13%
     def getHint(self, secret, guess):
         """
         :type secret: str
         :type guess: str
         :rtype: str
+
+        beats 60.13%
         """
         s, g = Counter(secret), Counter(guess)
         a = sum(i == j for i, j in zip(secret, guess))
