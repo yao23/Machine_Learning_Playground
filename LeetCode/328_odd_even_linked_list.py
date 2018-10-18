4ee37f6f8b45ba95a88892e5b28c5679663c51d7
yao <myspiritcrazy@gmail.com>
2018-10-17 22:00:51 -0700

    refactor code for Odd Even Linked List

--- LeetCode/328_odd_even_linked_list.py
+++ LeetCode/328_odd_even_linked_list.py
@@ -1,15 +1,17 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
 
 class Solution(object):
-    # beats 58.27%
     def oddEvenList(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
+
+        beats 58.27%
         """
         dummy1 = odd = ListNode(0)
         dummy2 = even = ListNode(0)
