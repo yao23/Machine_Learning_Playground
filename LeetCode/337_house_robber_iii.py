60a81263d6b5f808c970b5e60beaef4ea4b2644f
yao <myspiritcrazy@gmail.com>
2018-10-29 21:22:51 -0700

    refactor code for House Robber III

--- LeetCode/337_house_robber_iii.py
+++ LeetCode/337_house_robber_iii.py
@@ -5,18 +5,20 @@
 #         self.left = None
 #         self.right = None
 
+
 class Solution(object):
-    # beats 17.80%
     def rob(self, root):
         """
         :type root: TreeNode
         :rtype: int
+
+        beats 17.80%
         """
         return self.robDFS(root)[1]
 
     def robDFS(self,node):
         if node is None:
-            return (0,0)
+            return (0, 0)
         l = self.robDFS(node.left)
         r = self.robDFS(node.right)
-        return (l[1] + r[1], max(l[1] + r[1], l[0] + r[0] + node.val))
+        return l[1] + r[1], max(l[1] + r[1], l[0] + r[0] + node.val)
