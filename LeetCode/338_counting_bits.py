10dd7e764852d3ac5ea49ea9c3117f0522ee342f
yao <myspiritcrazy@gmail.com>
2018-10-30 21:33:58 -0700

    refactor code for Counting Bits

--- LeetCode/338_counting_bits.py
+++ LeetCode/338_counting_bits.py
@@ -1,14 +1,14 @@
 class Solution(object):
-    # beats 94.38%
     def countBits(self, num):
         """
         :type num: int
         :rtype: List[int]
-        """
-        iniArr = [0]
-        if num > 0:
-            amountToAdd = 1
-            while len(iniArr) < num + 1:
-                iniArr.extend([x + 1 for x in iniArr])
 
-        return iniArr[0:num + 1]
+        beats 94.38%
+        """
+        ini_arr = [0]
+        if num > 0:
+            while len(ini_arr) < num + 1:
+                ini_arr.extend([x + 1 for x in ini_arr])
+
+        return ini_arr[0:num + 1]
