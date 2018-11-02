4f58c7a75756123fe93c365b93a736dacd88cd82
yao <myspiritcrazy@gmail.com>
2018-11-02 13:52:42 -0700

    refactor code for Integer Break

--- LeetCode/343_integer_break.py
+++ LeetCode/343_integer_break.py
@@ -1,9 +1,10 @@
 class Solution(object):
-    # beats 62.35%
     def integerBreak(self, n):
         """
         :type n: int
         :rtype: int
+
+        beats 62.35%
         """
         if n == 2:
             return 1
@@ -11,8 +12,8 @@ class Solution(object):
             return 2
         list_3 = [3] * (n/3) # generate a list of 3
         mod_3 = n%3
-        if mod_3 == 1: # if a 1 is left, then add it to the first element to get a 4
+        if mod_3 == 1:  # if a 1 is left, then add it to the first element to get a 4
             list_3[0] += 1
-        if mod_3 == 2: # if a 2 is left, then put it into the list
+        if mod_3 == 2:  # if a 2 is left, then put it into the list
             list_3.append(2)
         return reduce(lambda a, b: a*b, list_3)
