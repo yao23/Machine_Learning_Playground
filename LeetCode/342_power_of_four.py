38cd4edf2c67423fa3d2054dd219bffe6bf1f483
yao <myspiritcrazy@gmail.com>
2018-11-01 20:35:23 -0700

    refactor code for Power of Four

--- LeetCode/342_power_of_four.py
+++ LeetCode/342_power_of_four.py
@@ -1,8 +1,9 @@
 class Solution(object):
-    # beats 89.32%
     def isPowerOfFour(self, num):
         """
         :type num: int
         :rtype: bool
+
+        beats 89.32%
         """
-        return num != 0 and num &(num-1) == 0 and num & 1431655765== num
+        return num != 0 and num & (num-1) == 0 and num & 1431655765 == num
