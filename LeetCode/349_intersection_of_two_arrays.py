8d504c3e6c6b5f930820695c1b23b299332bf5f6
yao <myspiritcrazy@gmail.com>
2018-11-07 22:22:25 -0800

    refactor code for Intersection of Two Arrays

--- LeetCode/349_intersection_of_two_arrays.py
+++ LeetCode/349_intersection_of_two_arrays.py
@@ -1,11 +1,12 @@
 class Solution(object):
-    # beats 68.70%
     def intersection(self, nums1, nums2):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :rtype: List[int]
+
+        beats 68.70%
         """
-        nums1=set(nums1)
-        nums2=set(nums2)
-        return list(nums1&nums2)
+        nums1 = set(nums1)
+        nums2 = set(nums2)
+        return list(nums1 & nums2)
