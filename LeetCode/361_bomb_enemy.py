20927e5b4fa95d96b46ecc5661819e716126b832
yao <myspiritcrazy@gmail.com>
2018-11-18 20:47:46 -0800

    refactor code for Bomb Enemy

--- LeetCode/361_bomb_enemy.py
+++ LeetCode/361_bomb_enemy.py
@@ -1,18 +1,19 @@
 class Solution(object):
-    # beats 98.70%
     def maxKilledEnemies(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
+
+        beats 98.70%
         """
         def hits(grid):
             return [[h
                      for block in ''.join(row).split('W')
                      for h in [block.count('E')] * len(block) + [0]]
                     for row in grid]
-        rowhits = hits(grid)
-        colhits = zip(*hits(zip(*grid)))
+        row_hits = hits(grid)
+        col_hits = zip(*hits(zip(*grid)))
         return max([rh + ch
-                    for row in zip(grid, rowhits, colhits)
+                    for row in zip(grid, row_hits, col_hits)
                     for cell, rh, ch in zip(*row)
                     if cell == '0'] or [0])
