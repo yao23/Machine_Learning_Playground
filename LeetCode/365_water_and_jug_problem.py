0b01a1893faebdbc08e4a09b092f11e78fb8602f
yaoli <myspiritcrazy@gmail.com>
2018-11-22 20:31:47 -0800

    Refactor code for Water and Jug Problem

--- LeetCode/365_water_and_jug_problem.py
+++ LeetCode/365_water_and_jug_problem.py
@@ -1,11 +1,12 @@
 class Solution(object):
-    # beats 16.42%
     def canMeasureWater(self, x, y, z):
         """
         :type x: int
         :type y: int
         :type z: int
         :rtype: bool
+
+        beats 16.42%
         """
         from fractions import gcd
         return z == 0 or x + y >= z and z % gcd(x, y) == 0
