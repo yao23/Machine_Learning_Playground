4f25331550252b065bb8f5431bd44efc143118aa
yaoli <myspiritcrazy@gmail.com>
2018-11-28 23:08:54 -0800

    Refactor code for Super Pow

--- LeetCode/372_super_pow.py
+++ LeetCode/372_super_pow.py
@@ -1,9 +1,10 @@
 class Solution(object):
-    # beats 84.25%
     def superPow(self, a, b):
         """
         :type a: int
         :type b: List[int]
         :rtype: int
+
+        beats 84.25%
         """
         return 0 if a % 1337 == 0 else pow(a, reduce(lambda x, y: (x * 10 + y) % 1140, b) + 1140, 1337)
