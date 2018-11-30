098e0faac1114966b8c83287708be4babd65d774
yaoli <myspiritcrazy@gmail.com>
2018-11-29 22:47:50 -0800

    Refactor code for Find K Pairs With Smallest Sums

--- LeetCode/373_find_k_pairs_with_smallest_sums.py
+++ LeetCode/373_find_k_pairs_with_smallest_sums.py
@@ -1,14 +1,19 @@
 # https://discuss.leetcode.com/topic/50450/slow-1-liner-to-fast-solutions/2
+import heapq
+
+
 class Solution(object):
-    # beats 83.63%
     def kSmallestPairs(self, nums1, nums2, k):
         """
         :type nums1: List[int]
         :type nums2: List[int]
         :type k: int
         :rtype: List[List[int]]
+
+        beats 83.63%
         """
         queue = []
+
         def push(i, j):
             if i < len(nums1) and j < len(nums2):
                 heapq.heappush(queue, [nums1[i] + nums2[j], i, j])
