b16de9ccda198b89f88789a2db23e3919b5414c7
yaoli <myspiritcrazy@gmail.com>
2018-12-02 21:04:48 -0800

    Refactor code for Wiggle Subsequence

--- LeetCode/376_wiggle_subsequence.py
+++ LeetCode/376_wiggle_subsequence.py
@@ -1,18 +1,25 @@
 from itertools import groupby
 
+
 class Solution(object):
-    # beats 44.44%
     def wiggleMaxLength(self, nums):
         """
         :type nums: List[int]
         :rtype: int
+
+        beats 44.44%
         """
         norep = [num for num, _ in groupby(nums)]
         triples = zip(norep, norep[1:], norep[2:])
         return sum((b>a) == (b>c) for a, b, c in triples) + len(norep[:2])
 
-    # beats 97.22%
     def wiggleMaxLength1(self, nums):
+        """
+        :param nums:
+        :return:
+
+        beats 97.22%
+        """
         norep = [num for num, _ in groupby(nums)]
         if len(norep) < 2: return len(norep)
         triples = zip(norep, norep[1:], norep[2:])
