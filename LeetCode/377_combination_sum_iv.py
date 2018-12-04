142b17d9ad35d0d190b2ecd3eaaf8c3be45a44ac
yaoli <myspiritcrazy@gmail.com>
2018-12-03 22:48:51 -0800

    Refactor code for Combination Sum IV

--- LeetCode/377_combination_sum_iv.py
+++ LeetCode/377_combination_sum_iv.py
@@ -1,15 +1,19 @@
 class Solution(object):
-    # beats 87.90%
     def combinationSum4(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
         :rtype: int
+
+        beats 87.90%
         """
-        nums, combs = sorted(nums), [1] + [0] * (target)
+        nums, combs = sorted(nums), [1] + [0] * target
         for i in range(target + 1):
             for num in nums:
-                if num  > i: break
-                if num == i: combs[i] += 1
-                if num  < i: combs[i] += combs[i - num]
+                if num > i:
+                    break
+                if num == i:
+                    combs[i] += 1
+                if num < i:
+                    combs[i] += combs[i - num]
         return combs[target]
