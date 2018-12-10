04e11f92dd15992650ce288b37f9e9a58878d84f
yaoli <myspiritcrazy@gmail.com>
2018-12-09 18:25:34 -0800

    Refactor code for First Unique Character in A String

--- LeetCode/387_first_unique_character_in_a_string.py
+++ LeetCode/387_first_unique_character_in_a_string.py
@@ -1,9 +1,10 @@
 class Solution(object):
-    # beats 99.69%
     def firstUniqChar(self, s):
         """
         :type s: str
         :rtype: int
+
+        beats 99.69%
         """
         letters='abcdefghijklmnopqrstuvwxyz'
         index=[s.index(l) for l in letters if s.count(l) == 1]
