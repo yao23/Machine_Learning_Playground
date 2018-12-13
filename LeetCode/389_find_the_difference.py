93dca7b54aa0bd82c9e471e20d9ba0b11d91f751
yaoli <myspiritcrazy@gmail.com>
2018-12-12 20:43:29 -0800

    Refactor code for Find The Difference

--- LeetCode/389_find_the_difference.py
+++ LeetCode/389_find_the_difference.py
@@ -13,12 +13,13 @@ class Solution(object):
         s, t = sorted(s), sorted(t)
         return t[-1] if s == t[:-1] else [x[1] for x in zip(s, t) if x[0] != x[1]][0]
 
-    # beats 96.00%
     def findTheDifference1(self, s, t):
         """
         :type s: str
         :type t: str
         :rtype: str
+
+        beats 96.00%
         """
         return chr(reduce(operator.xor, map(ord, s + t)))
 
