b4b3e527b129a30c2120380c84cbab3ba782ffb6
yaoli <myspiritcrazy@gmail.com>
2018-12-16 12:29:10 -0800

    Refactor code for Elimination Game

--- LeetCode/390_elimination_game.py
+++ LeetCode/390_elimination_game.py
@@ -1,9 +1,10 @@
 class Solution(object):
-    # beats 45.07%
     def lastRemaining(self, n):
         """
         :type n: int
         :rtype: int
+
+        beats 45.07%
         """
         start, size, inv = 1, 1, 1
         while n > 1: start, size, inv, n = start + inv * size + 2 * (n // 2 - 1) * inv * size,\
