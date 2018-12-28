994e986ad8bdaef22a618f68a85f9eedf9601983
yaoli <myspiritcrazy@gmail.com>
2018-12-28 09:18:03 -0800

    Refactor code for Is Subsequence

--- LeetCode/392_is_subsequence.py
+++ LeetCode/392_is_subsequence.py
@@ -1,10 +1,12 @@
 class Solution(object):
-    # beats 86.80%
     def isSubsequence(self, s, t):
         """
         :type s: str
         :type t: str
         :rtype: bool
+
+            # beats 86.80%
+
         """
         t = iter(t)
         return all(c in t for c in s)
