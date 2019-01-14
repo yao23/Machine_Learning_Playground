345d48d6bc7189e2d38c25f18767128010f42142
yaoli <myspiritcrazy@gmail.com>
2019-01-13 16:55:42 -0800

    Refactor code for Network Delay Time

---
+++ LeetCode/743_network_delay_time.py
@@ -0,0 +1,28 @@
+from heapq import *
+
+INF = 1 << 60
+
+
+class Solution:
+    def networkDelayTime(self, times, N, K):
+        """
+        :type times: List[List[int]]
+        :type N: int
+        :type K: int
+        :rtype: int
+        """
+        delay, current_queue = [INF] * (N + 1), [(0, K)]
+        delay[K] = 0
+        neighbor_list = [[] for _ in range(N + 1)]
+        for t in times:
+            neighbor_list[t[0]] += [(t[2], t[1])]
+        while current_queue:
+            current_node = heappop(current_queue)[1]
+            for neighbor_cost in neighbor_list[current_node]:
+                cost = neighbor_cost[0]
+                neighbor = neighbor_cost[1]
+                neighbor_delay = delay[current_node] + cost
+                if neighbor_delay < delay[neighbor]:
+                    delay[neighbor] = neighbor_delay
+                    heappush(current_queue, (delay[neighbor], neighbor))
+        return -(INF in delay[1:]) or max(delay[1:])
