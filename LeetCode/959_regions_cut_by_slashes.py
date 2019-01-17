b94c1efa99915e97b3b261890f7c9d314b5d3dd9
yaoli <myspiritcrazy@gmail.com>
2019-01-16 21:50:21 -0800

    Refactor code for Regions Cut By Slashes

--- LeetCode/959_regions_cut_by_slashes.py
+++ LeetCode/959_regions_cut_by_slashes.py
@@ -54,42 +54,42 @@ class Solution:
 
         beats 68.18%
         """
-        def dfs(i, j, k):
-            if 0 <= i < n > j >= 0 and not matrix[i][j][k]:
-                if grid[i][j] == "*":
-                    if k <= 1:
-                        matrix[i][j][0] = matrix[i][j][1] = cnt
-                        dfs(i - 1, j, 2)
-                        dfs(i, j + 1, 3)
+        def dfs(x, y, z):
+            if 0 <= x < n > y >= 0 and not matrix[x][y][z]:
+                if grid[x][y] == "*":
+                    if z <= 1:
+                        matrix[x][y][0] = matrix[x][y][1] = count
+                        dfs(x - 1, y, 2)
+                        dfs(x, y + 1, 3)
                     else:
-                        matrix[i][j][2] = matrix[i][j][3] = cnt
-                        dfs(i + 1, j, 0)
-                        dfs(i, j - 1, 1)
-                elif grid[i][j] == "/":
-                    if 1 <= k <= 2:
-                        matrix[i][j][1] = matrix[i][j][2] = cnt
-                        dfs(i, j + 1, 3)
-                        dfs(i + 1, j, 0)
+                        matrix[x][y][2] = matrix[x][y][3] = count
+                        dfs(x + 1, y, 0)
+                        dfs(x, y - 1, 1)
+                elif grid[x][y] == "/":
+                    if 1 <= z <= 2:
+                        matrix[x][y][1] = matrix[x][y][2] = count
+                        dfs(x, y + 1, 3)
+                        dfs(x + 1, y, 0)
                     else:
-                        matrix[i][j][0] = matrix[i][j][3] = cnt
-                        dfs(i - 1, j, 2)
-                        dfs(i, j - 1, 1)
+                        matrix[x][y][0] = matrix[x][y][3] = count
+                        dfs(x - 1, y, 2)
+                        dfs(x, y - 1, 1)
                 else:
-                    matrix[i][j][0] = matrix[i][j][1] = matrix[i][j][2] = matrix[i][j][3] = cnt
-                    dfs(i - 1, j, 2)
-                    dfs(i, j + 1, 3)
-                    dfs(i + 1, j, 0)
-                    dfs(i, j - 1, 1)
+                    matrix[x][y][0] = matrix[x][y][1] = matrix[x][y][2] = matrix[x][y][3] = count
+                    dfs(x - 1, y, 2)
+                    dfs(x, y + 1, 3)
+                    dfs(x + 1, y, 0)
+                    dfs(x, y - 1, 1)
 
         grid, n = [row.replace("\\", "*") for row in grid], len(grid)
-        matrix, cnt = [[[0, 0, 0, 0] for j in range(n)] for i in range(n)], 0
+        matrix, count = [[[0, 0, 0, 0] for _ in range(n)] for _ in range(n)], 0
         for i in range(n):
             for j in range(n):
                 for k in range(4):
                     if not matrix[i][j][k]:
-                        cnt += 1
+                        count += 1
                         dfs(i, j, k)
-        return cnt
+        return count
 
     def regionsBySlashes2(self, grid):
         """
