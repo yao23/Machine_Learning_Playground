d3bcd671ff466da5d8824b7808af9baa7faf19da
yaoli <myspiritcrazy@gmail.com>
2019-01-21 20:27:54 -0800

    Refactor code for Reverse Integer

--- LeetCode/007_reverse_integer.py
+++ LeetCode/007_reverse_integer.py
@@ -1,9 +1,10 @@
 class Solution(object):
-    # beats 54.38%
     def reverse(self, x):
         """
         :type x: int
         :rtype: int
+
+        beats 54.38%
         """
         s = cmp(x, 0)
         r = int(`s*x`[::-1])
