6610d809139f2cdba07fcfc0cea8932f1a7d611e
yaoli <myspiritcrazy@gmail.com>
2019-02-25 23:01:45 -0800

    Refactor code for Merge Intervals

--- LeetCode/056_merge_intervals.py
+++ LeetCode/056_merge_intervals.py
@@ -1,8 +1,8 @@
 # Definition for an interval.
-# class Interval(object):
-#     def __init__(self, s=0, e=0):
-#         self.start = s
-#         self.end = e
+class Interval(object):
+    def __init__(self, s=0, e=0):
+        self.start = s
+        self.end = e
 
 
 class Solution(object):
