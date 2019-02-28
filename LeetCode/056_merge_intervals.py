09ba52b3f44625842b40c4089366118303b69b61
yaoli <myspiritcrazy@gmail.com>
2019-02-27 23:29:50 -0800

    Refactor code for Merge Intervals

--- LeetCode/056_merge_intervals.py
+++ LeetCode/056_merge_intervals.py
@@ -19,7 +19,7 @@ class Solution(object):
         beats 52.65%
         """
         out = []
-        for i in sorted(intervals, key=lambda i: i.start):
+        for i in sorted(intervals, key=lambda interval: interval.start):
             if out and i.start <= out[-1].end:
                 out[-1].end = max(out[-1].end, i.end)
             else:
