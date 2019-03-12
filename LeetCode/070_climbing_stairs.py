172ce498911b9a984464eb01a3000313b8705476
yaoli <myspiritcrazy@gmail.com>
2019-03-11 19:25:13 -0700

    Refactor code for Climbing Stairs

--- LeetCode/070_climbing_stairs.py
+++ LeetCode/070_climbing_stairs.py
@@ -28,7 +28,7 @@ class Solution(object):
             return 1
         res = [0 for i in xrange(n)]
         res[0], res[1] = 1, 2
-        for i in xrange(2, n):
+        for i in range(2, n):
             res[i] = res[i - 1] + res[i - 2]
         return res[-1]
 
