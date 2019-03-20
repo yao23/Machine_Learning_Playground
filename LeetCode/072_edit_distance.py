4523645a2db9c72a5d3ecc9e62edb27c61fa02fb
yaoli <myspiritcrazy@gmail.com>
2019-03-19 20:48:01 -0700

    Refactor code for Edit Distance

--- LeetCode/072_edit_distance.py
+++ LeetCode/072_edit_distance.py
@@ -14,7 +14,7 @@ class Solution(object):
             dp[i][0] = i
         for j in range(l2):
             dp[0][j] = j
-        for i in xrange(1, l1):
+        for i in range(1, l1):
             for j in xrange(1, l2):
                 dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + (word1[i - 1] != word2[j - 1]))
         return dp[-1][-1]
