b124284a6ae195a4f65293f586c6f9d9aadd5da5
yaoli <myspiritcrazy@gmail.com>
2019-03-28 18:50:55 -0700

    Refactor code for Subsets

--- LeetCode/078_subsets.py
+++ LeetCode/078_subsets.py
@@ -40,7 +40,7 @@ class Solution(object):
         nums.sort()
         for i in range(1 << len(nums)):
             tmp = []
-            for j in xrange(len(nums)):
+            for j in range(len(nums)):
                 if i & 1 << j:  # if i >> j & 1:
                     tmp.append(nums[j])
             res.append(tmp)
