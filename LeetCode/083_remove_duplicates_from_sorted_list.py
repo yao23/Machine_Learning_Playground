fd275fdfbb37d930c25009470263197e69adf170
yaoli <myspiritcrazy@gmail.com>
2019-03-29 19:31:47 -0700

    Refactor code for Remove duplicates from sorted list

--- LeetCode/083_remove_duplicates_from_sorted_list.py
+++ LeetCode/083_remove_duplicates_from_sorted_list.py
@@ -1,8 +1,8 @@
 # Definition for singly-linked list.
-# class ListNode(object):
-#     def __init__(self, x):
-#         self.val = x
-#         self.next = None
+class ListNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.next = None
 
 
 class Solution(object):
