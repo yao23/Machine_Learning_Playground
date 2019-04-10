1510fe03fdc91e26354b43943d459e3a5df761d6
yaoli <myspiritcrazy@gmail.com>
2019-04-09 19:48:43 -0700

    Refactor code for Decode Ways

--- LeetCode/091_decode_ways.py
+++ LeetCode/091_decode_ways.py
@@ -4,17 +4,20 @@ class Solution(object):
         :type s: str
         :rtype: int
 
-        w tells the number of ways
-        v tells the previous number of ways
-        d is the current digit
-        p is the previous digit
+        num_ways tells the number of ways
+        prev_num_ways tells the previous number of ways
+        cur_digit is the current digit
+        pre_digit is the previous digit
 
         beats 84.18%
         """
-        v, w, p = 0, int(s > ''), ''
-        for d in s:
-            v, w, p = w, (d > '0') * w + (9 < int(p + d) < 27) * v, d
-        return w
+        prev_num_ways, num_ways, pre_digit = 0, int(s > ''), ''
+        for cur_digit in s:
+            p_num_ways = prev_num_ways
+            prev_num_ways = num_ways
+            num_ways = (cur_digit > '0') * num_ways + (9 < int(pre_digit + cur_digit) < 27) * p_num_ways
+            pre_digit = cur_digit
+        return num_ways
 
     def numDecodings1(self, s):
         """
