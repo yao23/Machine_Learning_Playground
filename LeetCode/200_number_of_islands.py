0b4edc1f192c2814a79ad57629eb32591c283a36
Yao Li <myspiritcrazy@gmail.com>
2024-03-05 10:55:10 -0800

    Update 200_number_of_islands.py

--- LeetCode/200_number_of_islands.py
+++ LeetCode/200_number_of_islands.py
@@ -15,3 +15,38 @@ class Solution(object):
                 return 1
             return 0
         return sum(sink(i, j) for i in range(len(grid)) for j in range(len(grid[i])))
+
+    def numIslandsV1(self, grid: List[List[str]]) -> int:
+        """
+        :type grid: List[List[str]]
+        :rtype: int
+
+        Graph traversal (DFS) and update visited node
+
+        beats 67.93%
+        """
+        row = len(grid)
+        col = len(grid[0])
+        visited = set()
+
+        def dfs(x, y):
+            if grid[x][y] == "0" or (x * col + y) in visited:
+                return
+            
+            visited.add(x * col + y)
+            if x + 1 < row:
+                dfs(x + 1, y)
+            if y + 1 < col:
+                dfs(x, y + 1)
+            if x - 1 >= 0:
+                dfs(x - 1, y)
+            if y - 1 >= 0:
+                dfs(x, y - 1)
+
+        cnt = 0
+        for x in range(row):
+            for y in range(col):
+                if grid[x][y] == "1" and (x * col + y) not in visited:
+                    cnt += 1
+                    dfs(x, y)
+        return cnt
