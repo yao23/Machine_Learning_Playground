fda20926613684b8a940367d0dfb66c7e6079b0c
Yao Li <myspiritcrazy@gmail.com>
2024-03-09 09:43:18 -0800

    Update 079_word_search.py

--- LeetCode/079_word_search.py
+++ LeetCode/079_word_search.py
@@ -1,5 +1,33 @@
 class Solution(object):
-    def exist(self, board, word):
+    def exist(self, board: List[List[str]], word: str) -> bool:
+        """
+        :type board: List[List[str]]
+        :type word: str
+        :rtype: bool
+
+        beats 58.20%
+        """
+        row = len(board)
+        col = len(board[0])
+        visit = set()
+
+        def dfs(r, c , idx):
+            if idx == len(word):
+                return True
+            if r < 0 or c < 0 or r == row or c == col or (r, c) in visit or board[r][c] != word[idx]:
+                return False
+            visit.add((r, c))
+            res = dfs(r + 1, c, idx + 1) or dfs(r - 1, c, idx + 1) or dfs(r, c + 1, idx + 1) or dfs(r, c - 1, idx + 1)
+            visit.remove((r, c))
+            return res
+        
+        for r in range(row):
+            for c in range(col):
+                if dfs(r, c, 0):
+                    return True
+        return False
+    
+    def existV0(self, board, word):
         """
         :type board: List[List[str]]
         :type word: str
