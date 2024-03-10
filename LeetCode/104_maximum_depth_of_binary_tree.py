d1ec4c599bc60c8e89cef5a9fffce0ac604ad8c4
Yao Li <myspiritcrazy@gmail.com>
2024-03-10 16:02:03 -0700

    Update 104_maximum_depth_of_binary_tree.py

--- LeetCode/104_maximum_depth_of_binary_tree.py
+++ LeetCode/104_maximum_depth_of_binary_tree.py
@@ -9,6 +9,31 @@ class TreeNode(object):
 
 
 class Solution(object):
+    def maxDepth(self, root: Optional[TreeNode]) -> int:
+        """
+        :type root: TreeNode
+        :rtype: int
+
+        beats 90.44%
+        """
+        res = 0
+
+        def dfs(node, depth):
+            nonlocal res
+            if not node:
+                return
+
+            depth += 1
+            if not node.left and not node.right:
+                if depth > res:
+                    res = depth
+
+            dfs(node.left, depth)
+            dfs(node.right, depth)
+
+        dfs(root, 0)
+        return res
+
     def maxDepth(self, root):
         """
         :type root: TreeNode
@@ -17,3 +42,50 @@ class Solution(object):
         beats 10.60%
         """
         return 1 + max(map(self.maxDepth, (root.left, root.right))) if root else 0
+
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        """
+        https://github.com/neetcode-gh/leetcode/blob/main/python/0104-maximum-depth-of-binary-tree.py
+        """
+        if not root:
+            return 0
+
+        return 1 + max(self.maxDepth(root.left), self.maxDepth(root.right))
+
+
+# ITERATIVE DFS
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        stack = [[root, 1]]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+
+            if node:
+                res = max(res, depth)
+                stack.append([node.left, depth + 1])
+                stack.append([node.right, depth + 1])
+        return res
+
+
+# BFS
+class Solution:
+    def maxDepth(self, root: TreeNode) -> int:
+        q = deque()
+        if root:
+            q.append(root)
+
+        level = 0
+
+        while q:
+
+            for i in range(len(q)):
+                node = q.popleft()
+                if node.left:
+                    q.append(node.left)
+                if node.right:
+                    q.append(node.right)
+            level += 1
+        return level
