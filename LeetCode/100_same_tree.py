cfa77dfc8bd67a65824c18b67e835988fe578111
Yao Li <myspiritcrazy@gmail.com>
2024-03-10 16:40:03 -0700

    Update 100_same_tree.py

--- LeetCode/100_same_tree.py
+++ LeetCode/100_same_tree.py
@@ -9,7 +9,21 @@ class TreeNode(object):
 
 
 class Solution(object):
-    def isSameTree(self, p, q):
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        """
+        Beats 97.55%
+        """
+        if p is None and q is None:
+            return True
+        elif p is not None and q is not None:
+            if p.val == q.val:
+                return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+            else:
+                return False
+        else:
+            return False
+            
+    def isSameTree0(self, p, q):
         """
         :type p: TreeNode
         :type q: TreeNode
@@ -21,6 +35,17 @@ class Solution(object):
             return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
         return p is q
 
+    def isSameTree2(self, p: TreeNode, q: TreeNode) -> bool:
+        """
+        https://github.com/neetcode-gh/leetcode/blob/main/python/0100-same-tree.py
+        """
+        if not p and not q:
+            return True
+        if p and q and p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
+            
     def isSameTree1(self, p, q):
         """
         :type p: TreeNode
