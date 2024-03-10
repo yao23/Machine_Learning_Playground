eeb44bb74a63dc728723fbb122bc6d012e86e258
Yao Li <myspiritcrazy@gmail.com>
2024-03-10 12:35:58 -0700

    Update 226_invert_binary_tree.py

--- LeetCode/226_invert_binary_tree.py
+++ LeetCode/226_invert_binary_tree.py
@@ -7,7 +7,43 @@
 
 
 class Solution(object):
-    def invertTree(self, root):
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+
+        beats 97.08%
+        """
+        if not root:
+            return root
+        
+        def dfs(node):
+            if not node:
+                return node
+            left = dfs(node.left)
+            right = dfs(node.right)
+            node.left = right
+            node.right = left
+            return node
+        
+        return dfs(root)
+
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        """
+        https://github.com/neetcode-gh/leetcode/blob/main/python/0226-invert-binary-tree.py        
+        """
+        if not root:
+            return None
+        
+        # swap the children
+        root.left, root.right = root.right, root.left
+        
+        # make 2 recursive calls
+        self.invertTree(root.left)
+        self.invertTree(root.right)
+        return root
+        
+    def invertTreeV0(self, root):
         """
         :type root: TreeNode
         :rtype: TreeNode
