8b4eea384417dbf315a418515318f4b48ff10967
Yao Li <myspiritcrazy@gmail.com>
2024-03-11 22:17:39 -0700

    Update 105_construct_binary_tree_from_preorder_and_inorder_traversal.py

--- LeetCode/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
+++ LeetCode/105_construct_binary_tree_from_preorder_and_inorder_traversal.py
@@ -18,7 +18,7 @@ class Solution(object):
         :type inorder: List[int]
         :rtype: TreeNode
 
-        beats 60.76%
+        beats 67.34%
         """
         if inorder:
             ind = inorder.index(preorder.pop(0))
@@ -26,3 +26,42 @@ class Solution(object):
             root.left = self.buildTree(preorder, inorder[0:ind])
             root.right = self.buildTree(preorder, inorder[ind+1:])
             return root
+
+    def buildTree0(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        """
+        beats 9.53%
+        """
+        if len(preorder) == 0:
+            return None
+        if len(preorder) == 1:
+            return TreeNode(preorder[0])
+        root = TreeNode(preorder[0])
+        idx = 0 
+        for i in range(len(inorder)):
+            if inorder[i] == preorder[0]:
+                idx = i
+                break
+        if idx == 0:
+            left = None
+            right = self.buildTree(preorder[1:], inorder[1:])
+        else:
+            left = self.buildTree(preorder[1:idx+1], inorder[:idx])
+            right = self.buildTree(preorder[idx+1:], inorder[idx+1:])
+        root.left = left
+        root.right = right
+        return root
+
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+        """
+        https://github.com/neetcode-gh/leetcode/blob/main/python/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+        
+        beats 37.43%
+        """
+        if not preorder or not inorder:
+            return None
+
+        root = TreeNode(preorder[0])
+        mid = inorder.index(preorder[0])
+        root.left = self.buildTree(preorder[1 : mid + 1], inorder[:mid])
+        root.right = self.buildTree(preorder[mid + 1 :], inorder[mid + 1 :])
+        return root
