d0b132adefd2790f73177100cfb9d3704fb9dd2f
Yao Li <myspiritcrazy@gmail.com>
2024-03-11 22:45:48 -0700

    Update 098_validate_binary_search_tree.py

--- LeetCode/098_validate_binary_search_tree.py
+++ LeetCode/098_validate_binary_search_tree.py
@@ -22,3 +22,34 @@ class Solution(object):
             return False
         return self.isValidBST(root.left, min(lessThan, root.val), largerThan) and \
             self.isValidBST(root.right, lessThan, max(root.val, largerThan))
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        """
+        https://github.com/neetcode-gh/leetcode/blob/main/python/0098-validate-binary-search-tree.py
+        
+        beats 41.76%
+        """
+        def valid(node, left, right):
+            if not node:
+                return True
+            if not (left < node.val < right):
+                return False
+
+            return valid(node.left, left, node.val) and valid(
+                node.right, node.val, right
+            )
+
+        return valid(root, float("-inf"), float("inf"))
+
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        """
+        beats 13.52%
+        """
+        def dfs(node, minVal, maxVal):
+            if not node:
+                return True
+            if node.val > minVal and node.val < maxVal:
+                return dfs(node.left, minVal, node.val) and dfs(node.right, node.val, maxVal)
+            else:
+                return False
+        return dfs(root, -sys.maxsize - 1, sys.maxsize)
