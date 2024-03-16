c5cbbb1d0c6e92cfeeabde752b74888c013b6ff3
Yao Li <myspiritcrazy@gmail.com>
2024-03-16 10:32:36 -0700

    Update 212_word_search_ii.py

--- LeetCode/212_word_search_ii.py
+++ LeetCode/212_word_search_ii.py
@@ -1,5 +1,57 @@
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+
+    def addWord(self, word):
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c] # moving to children
+        cur.isWord = True
+
 class Solution(object):
     def findWords(self, board, words):
+        """
+        :type board: List[List[str]]
+        :type words: List[str]
+        :rtype: List[str]
+
+        beats 31.72%
+
+        https://www.youtube.com/watch?v=asbcE9mZz_U&list=PLot-Xpze53ldBT_7QA8NVot219jFNr_GI&index=14
+        """
+        root = TrieNode()
+        for w in words:
+            root.addWord(w)
+
+        row, col = len(board), len(board[0])
+        res, visit = set(), set()
+
+        def dfs(r, c, node, word):
+            if r < 0 or c < 0 or r == row or c == col or (r,c) in visit or board[r][c] not in node.children:
+                return
+            visit.add((r,c))
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            if node.isWord:
+                res.add(word)
+                
+            dfs(r + 1, c, node, word)
+            dfs(r - 1, c, node, word)
+            dfs(r, c + 1, node, word)
+            dfs(r, c - 1, node, word)
+            visit.remove((r,c))
+
+        for r in range(row):
+            for c in range(col):
+                dfs(r, c, root, "")
+                
+        return list(res)
+    
+    
+    def findWords0(self, board, words):
         """
         :type board: List[List[str]]
         :type words: List[str]
