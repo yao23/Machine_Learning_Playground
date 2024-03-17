ecff58dc8a6a4a10d0f3fae2539c46fc103dd1bd
Yao Li <myspiritcrazy@gmail.com>
2024-03-17 11:12:07 -0700

    Update 231_power_of_two.py

--- LeetCode/231_power_of_two.py
+++ LeetCode/231_power_of_two.py
@@ -1,5 +1,16 @@
 class Solution(object):
-    def isPowerOfTwo(self, n):
+    def isPowerOfTwo(self, n: int) -> bool:
+        """
+        https://www.youtube.com/watch?v=H2bjttEV4Vc
+        
+        beats 50.42%
+        """
+        x = 1
+        while x < n:
+            x *= 2
+        return x == n
+        
+    def isPowerOfTwo1(self, n):
         """
         :type n: int
         :rtype: bool
@@ -7,3 +18,11 @@ class Solution(object):
         beats 9.48%
         """
         return n > 0 and not (n & n-1)
+
+    def isPowerOfTwo0(self, n: int) -> bool:
+        """
+        https://www.youtube.com/watch?v=H2bjttEV4Vc
+
+        beats 24.42%
+        """
+        return n > 0 and ((1 << 30) % n == 0)
