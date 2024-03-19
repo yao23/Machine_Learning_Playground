cc38dd5518239e7f3752c350c64932bc2ef5431c
Yao Li <myspiritcrazy@gmail.com>
2024-03-18 23:31:42 -0700

    Update 367_valid_perfect_square.py

--- LeetCode/367_valid_perfect_square.py
+++ LeetCode/367_valid_perfect_square.py
@@ -1,5 +1,35 @@
 class Solution(object):
-    def isPerfectSquare(self, num):
+    def isPerfectSquare(self, num: int) -> bool:
+        """
+        https://www.youtube.com/watch?v=Cg_wWPHJ2Sk&list=PLot-Xpze53leNZQd0iINpD-MAhMOMzWvO&index=3
+        
+        beats 96.54%
+        """
+        l, r = 1, num
+        while l <= r:
+            m = (l + r) // 2
+            sq = m * m
+            if sq > num:
+                r = m - 1
+            elif sq < num:
+                l = m + 1
+            else:
+                return True
+        return False
+    
+    def isPerfectSquare1(self, num: int) -> bool:
+        """
+        https://www.youtube.com/watch?v=Cg_wWPHJ2Sk&list=PLot-Xpze53leNZQd0iINpD-MAhMOMzWvO&index=3
+        
+        beats 5.48%
+        """
+        for i in range(1, num + 1):
+            if i * i == num:
+                return True
+            if i * i > num:
+                return False
+    
+    def isPerfectSquare0(self, num):
         """
         :type num: int
         :rtype: bool
