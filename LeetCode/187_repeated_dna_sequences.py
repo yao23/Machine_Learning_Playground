d9e599ef9b49337cd9c6491769390046c5ad3b48
Yao Li <myspiritcrazy@gmail.com>
2024-03-23 11:13:15 -0700

    Update 187_repeated_dna_sequences.py

--- LeetCode/187_repeated_dna_sequences.py
+++ LeetCode/187_repeated_dna_sequences.py
@@ -1,5 +1,20 @@
 class Solution(object):
-    def findRepeatedDnaSequences(self, s):
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        """
+        https://www.youtube.com/watch?v=FzTYfsmtOso&list=PLot-Xpze53leOBgcVsJBEGrHPd_7x_koV&index=11
+        
+        beats 80.74%
+        """
+        seen, res = set(), set()
+
+        for i in range(len(s) - 9): # need at least 10 characters
+            cur = s[i : i + 10]
+            if cur in seen: # seen before means repeated
+                res.add(cur)
+            seen.add(cur)
+        return list(res)
+        
+    def findRepeatedDnaSequencesV0(self, s):
         """
         :type s: str
         :rtype: List[str]
