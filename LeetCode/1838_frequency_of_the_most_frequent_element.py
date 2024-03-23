9d0ceba710d9ba2f7e8d37b93094d9202bae4a8f
Yao Li <myspiritcrazy@gmail.com>
2024-03-23 09:43:33 -0700

    Create 1838_frequency_of_the_most_frequent_element.py

---
+++ LeetCode/1838_frequency_of_the_most_frequent_element.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        """
+        https://www.youtube.com/watch?v=vgBrQ0NM5vE&list=PLot-Xpze53leOBgcVsJBEGrHPd_7x_koV&index=8
+    
+        beats 48.90%
+        """
+        nums.sort()
+        l, r = 0, 0
+        res, total = 0, 0
+
+        while r < len(nums):
+            total += nums[r]
+
+            while nums[r] * (r - l + 1) > total + k: # over allowed increment
+                total -= nums[l]
+                l += 1
+            
+            res = max(res, r - l + 1)
+            r += 1
+
+        return res
