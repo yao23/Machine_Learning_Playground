e44ff5cb54daabfdb34d157d9b619d41d7633262
Yao Li <myspiritcrazy@gmail.com>
2024-03-23 11:55:03 -0700

    Update 203_remove_linked_list_elements.py

--- LeetCode/203_remove_linked_list_elements.py
+++ LeetCode/203_remove_linked_list_elements.py
@@ -29,3 +29,42 @@ class Solution(object):
                 next_node = next_node.next
 
         return dummy.next
+
+    def removeElementsV1(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        """
+        https://www.youtube.com/watch?v=JI71sxtHTng&list=PLot-Xpze53leU0Ec0VkBhnf4npMRFiNcB&index=3
+        
+        beats 94.14%
+        """
+        node = ListNode(next=head)
+        pre, cur = node, head
+
+        while cur:
+            nxt = cur.next
+            if cur.val == val:
+                pre.next = nxt
+            else:
+                pre = cur
+            cur = nxt
+
+        return node.next
+        
+    def removeElementsV0(self, head: Optional[ListNode], val: int) -> Optional[ListNode]:
+        """
+        beats 75.62%
+        """
+        node = ListNode(0)
+        node.next = head
+        pre = node
+
+        while head:
+            while head and head.val == val:
+                nxt = head.next
+                head.next = None
+                head = nxt
+            pre.next = head
+            pre = head
+            if head:
+                head = head.next
+
+        return node.next
