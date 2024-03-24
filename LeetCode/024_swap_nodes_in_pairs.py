e7f38a154b06353ae4a6a5d6da6bf350a9c34dc4
Yao Li <myspiritcrazy@gmail.com>
2024-03-23 17:09:01 -0700

    Update 024_swap_nodes_in_pairs.py

--- LeetCode/024_swap_nodes_in_pairs.py
+++ LeetCode/024_swap_nodes_in_pairs.py
@@ -6,7 +6,32 @@
 
 
 class Solution(object):
-    def swapPairs(self, head):
+    def swapPairs(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        """
+        https://www.youtube.com/watch?v=o811TZLAWOo&list=PLot-Xpze53leU0Ec0VkBhnf4npMRFiNcB&index=9
+        
+        beats 73.14%
+        """
+        if not head or not head.next:
+            return head
+        dummy = ListNode(next=head.next)
+        pre = dummy
+        cur = head
+        while cur and cur.next:
+            nxt = cur.next
+            tmp = nxt.next
+
+            # swap first and second node
+            pre.next = nxt
+            nxt.next = cur
+            cur.next = tmp
+
+            pre = cur
+            cur = tmp
+
+        return dummy.next
+        
+    def swapPairsV0(self, head):
         """
         :type head: ListNode
         :rtype: ListNode
