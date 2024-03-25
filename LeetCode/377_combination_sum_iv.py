c760044b6dd00e53c5b64a10eb129b033e62da40
Yao Li <myspiritcrazy@gmail.com>
2024-03-24 17:00:26 -0700

    Update 377_combination_sum_iv.py

--- LeetCode/377_combination_sum_iv.py
+++ LeetCode/377_combination_sum_iv.py
@@ -1,5 +1,18 @@
 class Solution(object):
-    def combinationSum4(self, nums, target):
+    def combinationSum4(self, nums: List[int], target: int) -> int:
+        """
+        https://www.youtube.com/watch?v=dw2nMCxG0ik&list=PLot-Xpze53lcvx_tjrr_m2lgD2NsRHlNO&index=27
+        
+        beats 35.19%
+        """
+        dp = {0 : 1} # k - target, v - num of combinations
+        for total in range(1, target + 1):
+            dp[total] = 0
+            for n in nums: # dp[4] = dp[3] + dp[2] + dp[1]
+                dp[total] += dp.get(total - n, 0)
+        return dp[target]
+        
+    def combinationSum4V0(self, nums, target):
         """
         :type nums: List[int]
         :type target: int
