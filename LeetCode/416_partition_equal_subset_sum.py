5c562e2a55a4ca5b5fbbf6e216258c23b914e470
Yao Li <myspiritcrazy@gmail.com>
2024-03-24 23:56:36 -0700

    Update 416_partition_equal_subset_sum.py

--- LeetCode/416_partition_equal_subset_sum.py
+++ LeetCode/416_partition_equal_subset_sum.py
@@ -1,6 +1,32 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
         """
+        https://www.youtube.com/watch?v=IsvocB5BJhw&list=PLot-Xpze53lcvx_tjrr_m2lgD2NsRHlNO&index=31
+        
+        beats 88.81%
+        """
+        total = sum(nums)
+        if total % 2:
+            return False
+
+        dp = set()
+        dp.add(0)
+
+        target = total // 2
+
+        for i in range(len(nums) - 1, -1, -1):
+            nextDP = set()
+            for t in dp:
+                tmp = t + nums[i]
+                if tmp == target:
+                    return True
+                nextDP.add(tmp) # add cur num
+                nextDP.add(t) # no cur num
+            dp = nextDP
+        return True if target in dp else False
+
+    def canPartitionV0(self, nums: List[int]) -> bool:
+        """
         beats 10.54%
         """
         dp = {}
