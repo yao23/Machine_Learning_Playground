3fad9d957942d210344a5164e9f24f3aa2751100
Yao Li <myspiritcrazy@gmail.com>
2024-03-24 22:53:15 -0700

    Update 1911_maximum_alternating_subsequence_sum.py

--- LeetCode/1911_maximum_alternating_subsequence_sum.py
+++ LeetCode/1911_maximum_alternating_subsequence_sum.py
@@ -3,6 +3,25 @@ class Solution:
         """
         https://www.youtube.com/watch?v=4v42XOuU1XA&list=PLot-Xpze53lcvx_tjrr_m2lgD2NsRHlNO&index=31
         
+        DP
+
+        beats 60.05%
+        """
+        sumEven, sumOdd = 0, 0
+
+        for i in range(len(nums) - 1, -1, -1):
+            tmpEven = max(sumOdd + nums[i], sumEven)
+            tmpOdd = max(sumEven - nums[i], sumOdd)
+            sumEven, sumOdd = tmpEven, tmpOdd
+
+        return sumEven
+        
+    def maxAlternatingSumV0(self, nums: List[int]) -> int:
+        """
+        https://www.youtube.com/watch?v=4v42XOuU1XA&list=PLot-Xpze53lcvx_tjrr_m2lgD2NsRHlNO&index=31
+
+        dfs / backtracking with memorization
+        
         beats 7.77%
         """
         dp = {}
@@ -13,7 +32,7 @@ class Solution:
             if (i, even) in dp:
                 return dp[(i, even)]
             total = nums[i] if even else (-1 * nums[i])
-            dp[(i, even)] = max(total + dfs(i + 1, not even), dfs(i + 1, even))
+            dp[(i, even)] = max(total + dfs(i + 1, not even), dfs(i + 1, even)) # max of use nums[i] or not
             return dp[(i, even)]
 
         return dfs(0, True)
