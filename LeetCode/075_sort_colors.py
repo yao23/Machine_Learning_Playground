d9d2acfce858c20833f29d53e17c10e7d293f1b9
Yao Li <myspiritcrazy@gmail.com>
2024-04-04 22:56:13 -0700

    Update 075_sort_colors.py

--- LeetCode/075_sort_colors.py
+++ LeetCode/075_sort_colors.py
@@ -1,4 +1,29 @@
 class Solution(object):
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+
+        https://www.youtube.com/watch?v=4xbWSRZHqac
+
+        bucket sort
+
+        beats 46.30%
+        """
+        # Assuming arr only contains 0, 1 or 2
+        counts = [0, 0, 0]
+
+        # Count the quantity of each val in arr
+        for n in nums:
+            counts[n] += 1
+        
+        # Fill each bucket in the original array
+        i = 0
+        for n in range(len(counts)):
+            for j in range(counts[n]):
+                nums[i] = n
+                i += 1
+        return nums
+        
     def sortColorsV0(self, nums):
         """
         :type nums: List[int]
@@ -17,9 +42,11 @@ class Solution(object):
                 nums[i] = 0
                 i += 1
 
-    def sortColors(self, nums: List[int]) -> None:
+    def sortColorsV1(self, nums: List[int]) -> None:
         """
         Do not return anything, modify nums in-place instead.
+
+        quick select / sort
 
         https://www.youtube.com/watch?v=4xbWSRZHqac
 
