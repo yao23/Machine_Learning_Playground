2412bea5973796225e6d79846364d2baa9976325
Yao Li <myspiritcrazy@gmail.com>
2024-04-04 23:01:00 -0700

    Create 704_binary_search.py

---
+++ LeetCode/704_binary_search.py
@@ -0,0 +1,18 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        """
+        https://www.youtube.com/watch?v=s4DPM8ct1pI
+        
+        beats 76.71%
+        """
+        l, r = 0, len(nums) - 1
+        
+        while l <= r:
+            m = (l + r) // 2
+            if nums[m] == target:
+                return m
+            elif nums[m] < target:
+                l = m + 1
+            else:
+                r = m -1
+        return -1
