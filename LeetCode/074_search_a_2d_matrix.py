de3a9cdcf5ee9ad88d27f1f822c208e4e7f9d57f
Yao Li <myspiritcrazy@gmail.com>
2024-04-04 23:09:50 -0700

    Update 074_search_a_2d_matrix.py

--- LeetCode/074_search_a_2d_matrix.py
+++ LeetCode/074_search_a_2d_matrix.py
@@ -25,3 +25,36 @@ class Solution(object):
                 high = mid - 1
 
         return False
+
+    def searchMatrixV0(self, matrix: List[List[int]], target: int) -> bool:
+        """
+        https://www.youtube.com/watch?v=Ber2pi2C0j0
+
+        beats 91.07%
+        """
+        ROWS, COLS = len(matrix), len(matrix[0])
+
+        top, bot = 0, ROWS - 1
+        while top <= bot: # binary search row first
+            row = (top + bot) // 2
+            if target > matrix[row][-1]:
+                top = row + 1
+            elif target < matrix[row][0]:
+                bot = row - 1
+            else:
+                break
+
+        if not (top <= bot): # not found
+            return False
+        row = (top + bot) // 2
+        l, r = 0, COLS - 1
+        while l <= r: # binary search col then
+            m = (l + r) // 2
+            if target > matrix[row][m]:
+                l = m + 1
+            elif target < matrix[row][m]:
+                r = m - 1
+            else:
+                return True
+        return False
+
