552b8aa682591df3f42013975ce16f24c833bfe0
Yao Li <myspiritcrazy@gmail.com>
2024-04-05 23:35:25 -0700

    Create 700_search_in_a_binary_search_tree.py

---
+++ LeetCode/700_search_in_a_binary_search_tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def searchBST(self, root: Optional[TreeNode], val: int) -> Optional[TreeNode]:
+        """
+        beats 65.40%
+        """
+        if not root:
+            return root
+        if root.val == val:
+            return root
+        elif root.val < val:
+            return self.searchBST(root.right, val)
+        else:
+            return self.searchBST(root.left, val)
