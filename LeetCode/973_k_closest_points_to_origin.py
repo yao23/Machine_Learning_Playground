4bc07466fdcbcddf2d63a30ee4be9ccb684adf43
Yao Li <myspiritcrazy@gmail.com>
2024-04-06 12:38:04 -0700

    Update 973_k_closest_points_to_origin.py

--- LeetCode/973_k_closest_points_to_origin.py
+++ LeetCode/973_k_closest_points_to_origin.py
@@ -1,5 +1,24 @@
 class Solution:
   def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
+      """
+      https://www.youtube.com/watch?v=rI2EBUEMfTk
+      
+      beats 65.80%
+      """
+      minHeap = []
+      for x, y in points:
+          dist = (x ** 2) + (y ** 2)
+          minHeap.append((dist, x, y))
+      
+      heapq.heapify(minHeap)
+      res = []
+      for _ in range(k):
+          _, x, y = heapq.heappop(minHeap)
+          res.append((x, y))
+      return res
+
+  
+  def kClosest(self, points: List[List[int]], k: int) -> List[List[int]]:
     # https://walkccc.me/LeetCode/problems/0973/#__tabbed_3_3
     # https://github.com/yao23/Java_Playground/blob/master/src/com/leetcode/www/KClosestPointsToOrigin.java
     
