e1c8220f31cddb19f226f30bbc1bce518afad083
Yao Li <myspiritcrazy@gmail.com>
2024-04-07 11:37:36 -0700

    Update 062_unique_paths.py

--- LeetCode/062_unique_paths.py
+++ LeetCode/062_unique_paths.py
@@ -1,5 +1,33 @@
 class Solution(object):
-    def uniquePaths(self, m, n):
+    def uniquePaths(self, m: int, n: int) -> int:
+        """
+        beats 75.15%
+        """
+        preRow = [0] * n
+        for i in range(m - 1, -1, -1): # start from last row
+            curRow = [0] * n
+            curRow[n - 1] = 1
+            for j in range(n - 2, -1, -1):
+                curRow[j] = preRow[j] + curRow[j + 1] # sum down and right
+            preRow = curRow
+        return preRow[0]
+
+    def uniquePaths(self, m: int, n: int) -> int:
+        """
+        https://www.youtube.com/watch?v=IlEsdxuD4lY
+        
+        beats 69.61%
+        """
+        row = [1] * n
+
+        for i in range(m - 1):
+            newRow = [1] * n
+            for j in range(n - 2, -1, -1):
+                newRow[j] = newRow[j + 1] + row[j]
+            row = newRow
+        return row[0]
+        
+    def uniquePathsV0(self, m, n):
         """
         :type m: int
         :type n: int
