f116aa95516517be9a2b92a4c395835c1cf2fcae
Yao Li <myspiritcrazy@gmail.com>
2024-04-07 17:39:02 -0700

    Update 190_reverse_bits.py

--- LeetCode/190_reverse_bits.py
+++ LeetCode/190_reverse_bits.py
@@ -1,5 +1,24 @@
 class Solution:
-    def reverseBits(self, n):
+    def reverseBits(self, n: int) -> int:
+        """
+        https://www.youtube.com/watch?v=UcoN6UjAI64
+
+        beats 39.41%
+        """
+        res = 0
+        maxLen = 32
+        for i in range(maxLen):
+            bit = (n >> i) & 1 # get most right bit
+            res += (bit << (maxLen - 1 - i)) # move bit to the left position
+        return res
+    
+        # res = 0
+        # for i in range(32):
+        #     bit = (n >> i) & 1
+        #     res += (bit << (31 - i))
+        # return res
+    
+    def reverseBitsV0(self, n):
         """
         :param n:
         :return:
