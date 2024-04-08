b3eae1129da2325d558d14256ca854c601d6322b
Yao Li <myspiritcrazy@gmail.com>
2024-04-07 17:22:32 -0700

    Update 338_counting_bits.py

--- LeetCode/338_counting_bits.py
+++ LeetCode/338_counting_bits.py
@@ -1,5 +1,19 @@
 class Solution(object):
-    def countBits(self, num):
+    def countBits(self, n: int) -> List[int]:
+        """
+        https://www.youtube.com/watch?v=RyBM56RIWrM    
+
+        beats 69.18%
+        """
+        dp = [0] * (n + 1)
+        offset = 1
+        for i in range(1, n + 1):
+            if i == offset * 2: # update offset when i is power of 2 (i.e. 2, 4, 8, 16)
+                offset = i
+            dp[i] = 1 + dp[i - offset]
+        return dp
+    
+    def countBitsV0(self, num):
         """
         :type num: int
         :rtype: List[int]
