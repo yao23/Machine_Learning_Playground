1ab58dda53a0d92c03fbfd16070d0f2e2e9a1b58
Yao Li <myspiritcrazy@gmail.com>
2024-04-11 00:10:29 -0700

    Update 424_longest_repeating_character_replacement.py

--- LeetCode/424_longest_repeating_character_replacement.py
+++ LeetCode/424_longest_repeating_character_replacement.py
@@ -18,3 +18,23 @@ class Solution:
                 l += 1
 
         return (r - l + 1)
+
+    def characterReplacementV0(self, s: str, k: int) -> int:
+        """
+        beats 29.75%
+        """
+        count = {}
+        res = 0
+        l = 0
+
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+
+            while (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+
+            res = max(res, r - l + 1)
+
+        return res
+
