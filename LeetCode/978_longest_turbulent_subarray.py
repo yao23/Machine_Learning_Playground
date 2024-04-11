2ef32d194db934afd03d64e6b2c187fa5d2bc518
Yao Li <myspiritcrazy@gmail.com>
2024-04-10 23:23:19 -0700

    Update 978_longest_turbulent_subarray.py

--- LeetCode/978_longest_turbulent_subarray.py
+++ LeetCode/978_longest_turbulent_subarray.py
@@ -32,6 +32,33 @@ class Solution:
                     cur = 1
                 else: # inconsistent and keep cur and last number
                     res = max(cur, res)
+
+        return res
+
+
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        """
+        https://www.youtube.com/watch?v=V_iHUhR8Dek
+
+        beats 42.76%
+        """
+        l, r = 0, 1
+        res, prev = 1, ""
+
+        while r < len(arr):
+            if arr[r - 1] > arr[r] and prev != ">":
+                res = max(res, r - l + 1)
+                r += 1
+                prev = ">"
+            elif arr[r - 1] < arr[r] and prev != "<":
+                res = max(res, r - l + 1)
+                r += 1
+                prev = "<"
+            else:
+                r = r + 1 if arr[r] == arr[r - 1] else r
+                l = r - 1
+                prev = ""
+        return res
+
                     cur = 2
             
-        return res
