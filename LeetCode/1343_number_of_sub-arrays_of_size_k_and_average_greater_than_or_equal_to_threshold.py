787fd46a9a2a0b5eff989b3daf2cd471a79666af
Yao Li <myspiritcrazy@gmail.com>
2024-04-10 23:35:34 -0700

    Update 1343_number_of_sub-arrays_of_size_k_and_average_greater_than_or_equal_to_threshold.py

--- LeetCode/1343_number_of_sub-arrays_of_size_k_and_average_greater_than_or_equal_to_threshold.py
+++ LeetCode/1343_number_of_sub-arrays_of_size_k_and_average_greater_than_or_equal_to_threshold.py
@@ -14,3 +14,18 @@ class Solution:
                 res += 1
             curSum -= arr[L]
         return res
+
+    def numOfSubarraysV0(self, arr: List[int], k: int, threshold: int) -> int:
+        """
+        beats 94.42%
+        """
+        res = 0
+        curSum = sum(arr[:k-1])
+        threshold *= k # easier to compare with total sum instead of average
+
+        for L in range(len(arr) - k + 1):
+            curSum += arr[L + k - 1]
+            if curSum >= threshold:
+                res += 1
+            curSum -= arr[L]
+        return res
