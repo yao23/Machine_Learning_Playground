65c5f0daf4a0c175d409ab32b249f4b1ebc4bdf0
Yao Li <myspiritcrazy@gmail.com>
2024-04-11 21:57:11 -0700

    Update 080_remove_duplicates_from_sorted_array_ii.py

--- LeetCode/080_remove_duplicates_from_sorted_array_ii.py
+++ LeetCode/080_remove_duplicates_from_sorted_array_ii.py
@@ -14,3 +14,24 @@ class Solution(object):
                 nums[i] = n
                 i += 1
         return i
+
+    def removeDuplicates(self, nums: List[int]) -> int:
+        """
+        https://www.youtube.com/watch?v=ycAq8iqh0TI
+        
+        beats 14.09%
+        """
+        l, r = 0, 0
+
+        while r < len(nums):
+            count = 1
+            while r + 1 < len(nums) and nums[r] == nums[r + 1]:
+                r += 1
+                count += 1
+            
+            for i in range(min(2, count)):
+                nums[l] = nums[r]
+                l += 1
+            r += 1
+        return l
+
