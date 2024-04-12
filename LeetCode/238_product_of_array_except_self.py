8ef812f347d39c2bee218fa1a02f6e298e8db556
Yao Li <myspiritcrazy@gmail.com>
2024-04-12 09:57:22 -0700

    Update 238_product_of_array_except_self.py

--- LeetCode/238_product_of_array_except_self.py
+++ LeetCode/238_product_of_array_except_self.py
@@ -1,4 +1,20 @@
 class Solution(object):
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        """
+        https://www.youtube.com/watch?v=bNvIQI2wAjk
+        
+        beats 22.67%
+        """
+        res = [1] * (len(nums))
+
+        for i in range(1, len(nums)):
+            res[i] = res[i-1] * nums[i-1]
+        postfix = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= postfix
+            postfix *= nums[i]
+        return res
+
     def productExceptSelf(self, nums):
         """
         :type nums: List[int]
