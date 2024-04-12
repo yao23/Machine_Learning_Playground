33e15d83ef4c8d8fd27c274cb3330ce9b72e6d82
Yao Li <myspiritcrazy@gmail.com>
2024-04-12 10:48:40 -0700

    Update 269_alien_dictionary.py

--- LeetCode/269_alien_dictionary.py
+++ LeetCode/269_alien_dictionary.py
@@ -2,7 +2,47 @@ import collections
 
 
 class Solution(object):
-    def alienOrder(self, words):
+    def alienOrder(self, words: List[str]) -> str:
+        """
+        https://www.youtube.com/watch?v=6kTZYvNNyps
+        """
+        adj = {char: set() for word in words for char in word}
+
+        for i in range(len(words) - 1):
+            w1, w2 = words[i], words[i + 1]
+            minLen = min(len(w1), len(w2))
+            if len(w1) > len(w2) and w1[:minLen] == w2[:minLen]:
+                return ""
+            for j in range(minLen):
+                if w1[j] != w2[j]:
+                    print(w1[j], w2[j])
+                    adj[w1[j]].add(w2[j])
+                    break
+
+        visited = {}  # {char: bool} False visited, True current path
+        res = []
+
+        def dfs(char):
+            if char in visited:
+                return visited[char]
+
+            visited[char] = True
+
+            for neighChar in adj[char]:
+                if dfs(neighChar):
+                    return True
+
+            visited[char] = False
+            res.append(char)
+
+        for char in adj:
+            if dfs(char):
+                return ""
+
+        res.reverse()
+        return "".join(res)
+
+    def alienOrderV0(self, words):
         """
         :type words: List[str]
         :rtype: str
