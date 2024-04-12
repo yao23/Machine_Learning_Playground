eb3977afc1c734fce5fdd148c41a13fb5a31a41c
Yao Li <myspiritcrazy@gmail.com>
2024-04-12 10:24:30 -0700

    Update 211_add_and_search_word.py

--- LeetCode/211_add_and_search_word.py
+++ LeetCode/211_add_and_search_word.py
@@ -1,7 +1,52 @@
 import collections
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}  # a : TrieNode
+        self.word = False
 
-class WordDictionary(object):
+class WordDictionary:
+    """
+    https://www.youtube.com/watch?v=BTf05gs_8iU
+    
+    beats 72.82%
+    """
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.word = True
+
+    def search(self, word: str) -> bool:
+        def dfs(j, root):
+            cur = root
+
+            for i in range(j, len(word)):
+                c = word[i]
+                if c == ".":
+                    for child in cur.children.values():
+                        if dfs(i + 1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.word
+
+        return dfs(0, self.root)
+        
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
+# param_2 = obj.search(word)
+
+class WordDictionaryV0(object):
     """
     beats 89.10%
     """
