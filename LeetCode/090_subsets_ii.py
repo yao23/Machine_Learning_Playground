1c3fbd12cfdb945bbf14a39a869333e03cbec399
Yao Li <myspiritcrazy@gmail.com>
2024-04-12 10:30:12 -0700

    Update 090_subsets_ii.py

--- LeetCode/090_subsets_ii.py
+++ LeetCode/090_subsets_ii.py
@@ -1,4 +1,30 @@
 class Solution(object):
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        """
+        https://www.youtube.com/watch?v=Vn2v6ajA7U0
+        
+        beats 39.49%
+        """
+        res = []
+        nums.sort()
+
+        def backtrack(i, subset):
+            if i == len(nums):
+                res.append(subset[::])
+                return
+
+            # All subsets that include nums[i]
+            subset.append(nums[i])
+            backtrack(i + 1, subset)
+            subset.pop()
+            # All subsets that don't include nums[i]
+            while i + 1 < len(nums) and nums[i] == nums[i + 1]:
+                i += 1
+            backtrack(i + 1, subset)
+
+        backtrack(0, [])
+        return res
+    
     def subsetsWithDup(self, nums):
         """
         :type nums: List[int]
