2c70e851c5bebe8addb001602485169e2df850e0
Yao Li <myspiritcrazy@gmail.com>
2024-04-11 23:04:10 -0700

    Update 303_range_sum_query_immutable.py

--- LeetCode/303_range_sum_query_immutable.py
+++ LeetCode/303_range_sum_query_immutable.py
@@ -1,4 +1,24 @@
-class NumArray(object):
+class NumArray:
+    """
+    https://www.youtube.com/watch?v=2pndAmo_sMA
+    
+    beats 66.70%
+    """
+    def __init__(self, nums: List[int]):
+        self.prefix = [] # prefix sum, sum from first to current
+        cur = 0
+        for n in nums:
+            cur += n
+            self.prefix.append(cur)
+        
+        
+    def sumRange(self, left: int, right: int) -> int:
+        rightSum = self.prefix[right] 
+        leftSum = self.prefix[left - 1] if left > 0 else 0
+        return rightSum - leftSum
+
+
+class NumArrayV0(object):
     """
     Your NumArray object will be instantiated and called as such:
     obj = NumArray(nums)
