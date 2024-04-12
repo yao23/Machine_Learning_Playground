0ac2fbfe2a6f2e2090622efb88883134021ac458
Yao Li <myspiritcrazy@gmail.com>
2024-04-11 23:23:50 -0700

    Update 304_range_sum_query_2d_immutable.py

--- LeetCode/304_range_sum_query_2d_immutable.py
+++ LeetCode/304_range_sum_query_2d_immutable.py
@@ -1,4 +1,46 @@
-class NumMatrix(object):
+class NumMatrix:
+    """
+    https://www.youtube.com/watch?v=KE8MQuwE2yA
+    
+    beats 96.04%
+    """
+    def __init__(self, matrix):
+        self.sum_ = [[0] * (len(matrix[0]) + 1) for _ in range(len(matrix) + 1)]
+        for i, line in enumerate(matrix):
+            previous = 0
+            for j, num in enumerate(line):
+                previous += num
+                above = self.sum_[i][j + 1]
+                self.sum_[i + 1][j + 1] = previous + above
+
+    def sumRegion(self, row1, col1, row2, col2):
+        r1, c1, r2, c2 = row1 + 1, col1 + 1, row2 + 1, col2 + 1
+        bottomRight = self.sum_[r2][c2]
+        above = self.sum_[r1 -1][c2]
+        left = self.sum_[r2][c1 - 1]
+        topLeft = self.sum_[r1 -1][c1 - 1]
+        return bottomRight - above - left + topLeft
+
+class NumMatrixV1:
+    """
+    beats 89.40%
+    """
+    def __init__(self, matrix):
+        self.sum_ = [[0] * (len(matrix[0]) + 1) for _ in range(len(matrix) + 1)]
+        for i, line in enumerate(matrix):
+            previous = 0
+            for j, num in enumerate(line):
+                previous += num
+                above = self.sum_[i][j + 1]
+                self.sum_[i + 1][j + 1] = previous + above
+
+    def sumRegion(self, row1, col1, row2, col2):
+        sum_col2 = self.sum_[row2 + 1][col2 + 1] - self.sum_[row1][col2 + 1]
+        sum_col1 = self.sum_[row2 + 1][col1] - self.sum_[row1][col1]
+        return sum_col2 - sum_col1
+
+
+class NumMatrixV0(object):
     def __init__(self, matrix):
         """
         :type matrix: List[List[int]]
