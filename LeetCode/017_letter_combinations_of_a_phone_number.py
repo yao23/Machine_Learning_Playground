6bfef0061e242d05c16da3dc2bed28994beb35e3
Yao Li <myspiritcrazy@gmail.com>
2024-04-12 10:38:05 -0700

    Update 017_letter_combinations_of_a_phone_number.py

--- LeetCode/017_letter_combinations_of_a_phone_number.py
+++ LeetCode/017_letter_combinations_of_a_phone_number.py
@@ -1,5 +1,35 @@
 class Solution(object):
-    def letterCombinations(self, digits):
+    def letterCombinations(self, digits: str) -> List[str]:
+        """
+        https://www.youtube.com/watch?v=0snEunUacZY
+        
+        beats 57.07%
+        """
+        res = []
+        digitToChar = {
+            "2": "abc",
+            "3": "def",
+            "4": "ghi",
+            "5": "jkl",
+            "6": "mno",
+            "7": "qprs",
+            "8": "tuv",
+            "9": "wxyz",
+        }
+
+        def backtrack(i, curStr):
+            if len(curStr) == len(digits):
+                res.append(curStr)
+                return
+            for c in digitToChar[digits[i]]:
+                backtrack(i + 1, curStr + c)
+
+        if digits:
+            backtrack(0, "")
+
+        return res
+        
+    def letterCombinationsV0(self, digits):
         """
         :type digits: str
         :rtype: List[str]
