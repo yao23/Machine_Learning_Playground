c6add3b8c64a5454cf52beaea96a907fac7ddb92
Yao Li <myspiritcrazy@gmail.com>
2024-04-13 11:08:56 -0700

    Update 287_find_the_duplicate_number.py

--- LeetCode/287_find_the_duplicate_number.py
+++ LeetCode/287_find_the_duplicate_number.py
@@ -1,6 +1,15 @@
 class Solution(object):
+    """
+    Given an array of integers nums containing n + 1 integers where each integer is in the range [1, n] inclusive.
+
+    There is only one repeated number in nums, return this repeated number.
+
+    You must solve the problem without modifying the array nums and uses only constant extra space.
+    """
     def findDuplicate(self, nums: List[int]) -> int:
         """
+        The reason we can use Floyd's algorithm (rabbit-hare / slow - fast pointers) is that index maps to value like next pointer in LinkedList
+        
         https://www.youtube.com/watch?v=wjYnzkAhcNk
 
         beats 55.57%
