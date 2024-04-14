f984ffc49dc814eb7f4d8a6002419a8debadb8a8
Yao Li <myspiritcrazy@gmail.com>
2024-04-14 09:16:06 -0700

    Create FlatMap.py

---
+++ CoderPadHackerRank/FlatMap.py
@@ -0,0 +1,42 @@
+# Your previous Plain Text content is preserved below:
+
+# This is just a simple shared plaintext pad, with no execution capabilities.
+
+# When you know what language you'd like to use for your interview,
+# simply choose it from the dots menu on the tab, or add a new language
+# tab using the Languages button on the left.
+
+# You can also change the default language your pads are created with
+# in your account settings: https://app.coderpad.io/settings
+
+# Enjoy your interview!
+
+
+# input: [1, [2], [[4, 3, 5]], 6, 7, 8, 9]
+
+# output: [1, 2, 4, 3, 5, 6, 7, 8, 9]
+
+def solution(input):
+    output = []
+
+    def dfs(element):
+        # res = []
+        if type(element) is list:
+            for e in element:
+                # res.append(dfs(e))
+                dfs(e)
+        else:
+            # res.append(element)
+            # return res
+            output.append(element)
+
+    for element in input:
+        tmp = dfs(element)
+        # if tmp:
+        #     for t in tmp:
+        #         output.append(t)
+    return output
+
+input = [1, [2], [[4, 3, 5]], 6, 7, 8, 9]
+output = solution(input)
+print(output)
