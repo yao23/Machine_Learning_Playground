a3b06b2b2d8a877c148ac9003e37fcf52f8a1c69
Yao Li <myspiritcrazy@gmail.com>
2024-04-19 23:22:53 -0700

    Update 072_edit_distance.py

--- LeetCode/072_edit_distance.py
+++ LeetCode/072_edit_distance.py
@@ -1,9 +1,40 @@
 class Solution(object):
-    def minDistance(self, word1, word2):
+    """
+    https://www.youtube.com/watch?v=XYi2-LPrwm4
+
+    Memozization
+    
+    beats 86.63%
+    """
+    def minDistanceV0(self, word1: str, word2: str) -> int:
+        dp = {}
+
+        def dfs(i, j):
+            if i == len(word1): 
+                return len(word2) - j
+            if j == len(word2):
+                return len(word1) - i
+            if (i, j) in dp:
+                return dp[(i, j)]
+            if word1[i] == word2[j]:
+                dp[(i, j)] = dfs(i + 1, j + 1)
+            else:
+                dp[(i, j)] = 1 + min(
+                    dfs(i, j + 1),    # insert
+                    dfs(i + 1, j),    # delete
+                    dfs(i + 1, j + 1) # replace
+                )
+            return dp[(i, j)]
+
+        return dfs(0, 0)
+        
+    def minDistanceV2(self, word1, word2):
         """
         :type word1: str
         :type word2: str
         :rtype: int
+
+        DP (top down)
 
         O(m*n) space
         beats 54.30%
@@ -18,6 +49,28 @@ class Solution(object):
             for j in range(1, l2):
                 dp[i][j] = min(dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + (word1[i - 1] != word2[j - 1]))
         return dp[-1][-1]
+
+    """
+    DP (bottom up) - converted from V0 (memozization)
+    
+    beats 66.45%
+    """
+    def minDistanceV3(self, word1: str, word2: str) -> int:
+        dp = [[float("inf")] * (len(word2) + 1) for i in range(len(word1) + 1)]
+
+        for j in range(len(word2) + 1):
+            dp[len(word1)][j] = len(word2) - j  # left to right in bottom row
+        for i in range(len(word1) + 1):
+            dp[i][len(word2)] = len(word1) - i  # up to down in right column
+
+        for i in range(len(word1) - 1, -1, -1):
+            for j in range(len(word2) - 1, -1, -1):
+                if word1[i] == word2[j]:
+                    dp[i][j] = dp[i + 1][j + 1]
+                else:
+                    dp[i][j] = 1 + min(dp[i + 1][j], dp[i][j + 1], dp[i + 1][j + 1])
+        return dp[0][0]
+
 
     def minDistance1(self, word1, word2):
         """
