172aa25f922048970b727ddd18e895ea84cce880
Yao Li <myspiritcrazy@gmail.com>
2024-04-22 12:14:29 -0700

    Update 283_move_zeroes.py

--- LeetCode/283_move_zeroes.py
+++ LeetCode/283_move_zeroes.py
@@ -12,3 +12,50 @@ class Solution(object):
             if nums[i] != 0:
                 nums[i], nums[zero] = nums[zero], nums[i]
                 zero += 1
+
+    """
+    beats 57.93%
+    """
+    def moveZeroesV1(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        if not nums or length == 0:
+            return nums
+
+        l = 0
+        for r in range(length):
+            if nums[r] != 0: # find non-zero
+                nums[l], nums[r] = nums[r], nums[l]
+                l += 1
+
+        return nums
+        
+    """
+    beats 26.72%
+    """
+    def moveZeroesV0(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        length = len(nums)
+        if not nums or length == 0:
+            return nums
+
+        l, r = 0, 0
+        while l < length and r < length:
+            while l < length and nums[l] != 0: # find zero
+                l += 1
+
+            while r < length and nums[r] == 0: # find non-one
+                r += 1
+
+            if l < length and r < length and l < r: # switch if zero element in left and non-zero right
+                nums[l], nums[r] = nums[r], nums[l]
+            elif r <= l: # moveing right if non-zero element in left and zero right
+                r += 1
+                while r < length and nums[r] == 0: # find non-one
+                    r += 1
+
+        return nums
