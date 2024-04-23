234acb59fd7c339839a1570cba1cc7e7bbb3054f
Yao Li <myspiritcrazy@gmail.com>
2024-04-22 22:19:38 -0700

    Update 026_remove_duplicates_from_sorted_array.py

--- LeetCode/026_remove_duplicates_from_sorted_array.py
+++ LeetCode/026_remove_duplicates_from_sorted_array.py
@@ -1,4 +1,18 @@
 class Solution(object):
+    """
+    https://www.youtube.com/watch?v=DEJAZBq0FDA
+    
+    beats 87.43%
+    """
+    def removeDuplicates(self, nums: List[int]) -> int:
+        L = 1
+        
+        for R in range(1, len(nums)):
+            if nums[R] != nums[R - 1]:
+                nums[L] = nums[R]
+                L += 1
+        return L
+        
     def removeDuplicates(self, nums):
         """
         :type nums: List[int]
