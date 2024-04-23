6f75f2cc9d713e801f6e62f682fc394a07dc0c1a
Yao Li <myspiritcrazy@gmail.com>
2024-04-22 22:06:18 -0700

    Update 424_longest_repeating_character_replacement.py

--- LeetCode/424_longest_repeating_character_replacement.py
+++ LeetCode/424_longest_repeating_character_replacement.py
@@ -13,7 +13,7 @@ class Solution:
             count[s[r]] = 1 + count.get(s[r], 0)
             maxf = max(maxf, count[s[r]])
 
-            if (r - l + 1) - maxf > k:
+            if (r - l + 1) - maxf > k: # windowLength - frequencyOfMostAppearedLetter, replace others instead of most appeared one (at most k times), so move left point to shrink window
                 count[s[l]] -= 1
                 l += 1
 
@@ -30,7 +30,7 @@ class Solution:
         for r in range(len(s)):
             count[s[r]] = 1 + count.get(s[r], 0)
 
-            while (r - l + 1) - max(count.values()) > k: # windowLength - frequencyOfMostAppearedLetter
+            while (r - l + 1) - max(count.values()) > k: # windowLength - frequencyOfMostAppearedLetter, replace others instead of most appeared one (at most k times)
                 count[s[l]] -= 1
                 l += 1
 
