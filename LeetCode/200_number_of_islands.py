c965d408991bad2dc6b88cab1020605b230a2507
Yao Li <myspiritcrazy@gmail.com>
2024-04-22 18:12:20 -0700

    Update 200_number_of_islands.py

--- LeetCode/200_number_of_islands.py
+++ LeetCode/200_number_of_islands.py
@@ -1,5 +1,41 @@
 class Solution(object):
-    def numIslands(self, grid):
+    """
+    https://www.youtube.com/watch?v=pV2kpPD66nE
+    
+    DFS
+
+    beats 5.18%
+    """
+    def numIslands(self, grid: List[List[str]]) -> int:
+        if not grid or not grid[0]:
+            return 0
+
+        numIslands = 0
+        visit = set()
+        rows, cols = len(grid), len(grid[0])
+
+        def dfs(r, c):
+            if (
+                r not in range(rows)
+                or c not in range(cols)
+                or grid[r][c] == "0"
+                or (r, c) in visit
+            ):
+                return
+
+            visit.add((r, c))
+            directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+        for r in range(rows):
+            for c in range(cols):
+                if grid[r][c] == "1" and (r, c) not in visit:
+                    numIslands += 1
+                    dfs(r, c)
+        return numIslands
+        
+    def numIslandsV2(self, grid):
         """
         :type grid: List[List[str]]
         :rtype: int
@@ -51,6 +87,11 @@ class Solution(object):
                     dfs(x, y)
         return cnt
 
+    """
+    BFS 
+
+    beats 6.31%
+    """
     def numIslands(self, grid: List[List[str]]) -> int:
         if not grid:
             return
