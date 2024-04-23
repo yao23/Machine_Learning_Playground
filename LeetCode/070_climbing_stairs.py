15a172bc28ed269030a77925f765ab7b2c81b01b
Yao Li <myspiritcrazy@gmail.com>
2024-04-22 21:15:43 -0700

    Update 070_climbing_stairs.py

--- LeetCode/070_climbing_stairs.py
+++ LeetCode/070_climbing_stairs.py
@@ -50,3 +50,19 @@ class Solution(object):
         if dic[n] < 0:
             dic[n] = self.helper(n - 1, dic) + self.helper(n - 2, dic)
         return dic[n]
+
+    """
+    https://www.youtube.com/watch?v=Y0lT9Fck7qI
+    
+    beats 59.08%
+    """
+    def climbStairsV2(self, n: int) -> int:
+        if n <= 3:
+            return n
+        n1, n2 = 2, 3
+
+        for i in range(4, n + 1):
+            temp = n1 + n2
+            n1 = n2
+            n2 = temp
+        return n2
