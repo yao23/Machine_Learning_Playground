72df41754c2a290d24b1ea503728b3e451ac36f9
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-12 09:17:25 -0800

    Create SlidingWindowMaximum.py

---
+++ CodingPractice/SlidingWindowMaximum.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        q = deque()  # index
+        l = r = 0
+
+        while r < len(nums):
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
+
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+
+        return output
