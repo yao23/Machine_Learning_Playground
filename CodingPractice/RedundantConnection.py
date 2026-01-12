ee76b3ed217ab5b806a85738e8849612fddc933e
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-12 09:19:31 -0800

    Create RedundantConnection.py

---
+++ CodingPractice/RedundantConnection.py
@@ -0,0 +1,25 @@
+class Solution:
+    def findRedundantConnection(self, edges: List[List[int]]) -> List[int]:
+        n = len(edges)
+        adj = [[] for _ in range(n + 1)]
+
+        def dfs(node, par):
+            if visit[node]:
+                return True
+
+            visit[node] = True
+            for nei in adj[node]:
+                if nei == par:
+                    continue
+                if dfs(nei, node):
+                    return True
+            return False
+
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+            visit = [False] * (n + 1)
+
+            if dfs(u, -1):
+                return [u, v]
+        return []
