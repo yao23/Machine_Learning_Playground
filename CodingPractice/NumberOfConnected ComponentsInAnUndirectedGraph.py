4347a25662bdc36fccdee7f1f3556b092eada87b
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-13 10:27:52 -0800

    Create NumberOfConnected ComponentsInAnUndirectedGraph.py

---
+++ CodingPractice/NumberOfConnected ComponentsInAnUndirectedGraph.py
@@ -0,0 +1,21 @@
+class Solution:
+    def countComponents(self, n: int, edges: List[List[int]]) -> int:
+        adj = [[] for _ in range(n)]
+        visit = [False] * n
+        for u, v in edges:
+            adj[u].append(v)
+            adj[v].append(u)
+
+        def dfs(node):
+            for nei in adj[node]:
+                if not visit[nei]:
+                    visit[nei] = True
+                    dfs(nei)
+
+        res = 0
+        for node in range(n):
+            if not visit[node]:
+                visit[node] = True
+                dfs(node)
+                res += 1
+        return res
