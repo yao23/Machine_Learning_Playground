3bc0e8ec20faf49fa05123510e350293c0c51209
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-13 10:26:18 -0800

    Create Subsets.py

---
+++ CodingPractice/Subsets.py
@@ -0,0 +1,16 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        subset = []
+
+        def dfs(i):
+            if i >= len(nums):
+                res.append(subset.copy())
+                return
+            subset.append(nums[i])
+            dfs(i + 1)
+            subset.pop()
+            dfs(i + 1)
+
+        dfs(0)
+        return res
