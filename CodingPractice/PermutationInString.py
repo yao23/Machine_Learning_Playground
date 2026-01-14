aba808bd700678a7bc7e73d4e2a487c5e61cfdbf
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-14 09:26:24 -0800

    Create PermutationInString.py

---
+++ CodingPractice/PermutationInString.py
@@ -0,0 +1,18 @@
+class Solution:
+    def checkInclusion(self, s1: str, s2: str) -> bool:
+        count1 = {}
+        for c in s1:
+            count1[c] = 1 + count1.get(c, 0)
+
+        need = len(count1)
+        for i in range(len(s2)):
+            count2, cur = {}, 0
+            for j in range(i, len(s2)):
+                count2[s2[j]] = 1 + count2.get(s2[j], 0)
+                if count1.get(s2[j], 0) < count2[s2[j]]:
+                    break
+                if count1.get(s2[j], 0) == count2[s2[j]]:
+                    cur += 1
+                if cur == need:
+                    return True
+        return False
