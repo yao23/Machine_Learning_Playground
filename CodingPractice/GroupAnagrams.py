06ed9670bc86b4b2ba90cccff8a4d8b2edb21d4d
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-14 09:25:55 -0800

    Create GroupAnagrams.py

---
+++ CodingPractice/GroupAnagrams.py
@@ -0,0 +1,10 @@
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        res = defaultdict(list)
+        for s in strs:
+            count = [0] * 26
+            for c in s:
+                count[ord(c) - ord('a')] += 1
+            print(tuple(count))
+            res[tuple(count)].append(s)
+        return list(res.values())
