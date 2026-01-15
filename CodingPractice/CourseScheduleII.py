0da78efc3df866a20d43b01e51b19d49f8d30883
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-15 09:52:05 -0800

    Create CourseScheduleII.py

---
+++ CodingPractice/CourseScheduleII.py
@@ -0,0 +1,26 @@
+class Solution:
+    def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
+        indegree = [0] * numCourses
+        adj = [[] for i in range(numCourses)]
+        for src, dst in prerequisites:
+            indegree[dst] += 1
+            adj[src].append(dst)
+
+        q = deque()
+        for n in range(numCourses):
+            if indegree[n] == 0:
+                q.append(n)
+
+        finish, output = 0, []
+        while q:
+            node = q.popleft()
+            output.append(node)
+            finish += 1
+            for nei in adj[node]:
+                indegree[nei] -= 1
+                if indegree[nei] == 0:
+                    q.append(nei)
+
+        if finish != numCourses:
+            return []
+        return output[::-1]
