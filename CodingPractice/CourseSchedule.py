bfc4736c4bceb3748109f16b7008528e41ee8d56
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-16 09:04:24 -0800

    Create CourseSchedule.py

---
+++ CodingPractice/CourseSchedule.py
@@ -0,0 +1,29 @@
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+        # Map each course to its prerequisites
+        preMap = {i: [] for i in range(numCourses)}
+        for crs, pre in prerequisites:
+            preMap[crs].append(pre)
+
+        # Store all courses along the current DFS path
+        visiting = set()
+
+        def dfs(crs):
+            if crs in visiting:
+                # Cycle detected
+                return False
+            if preMap[crs] == []:
+                return True
+
+            visiting.add(crs)
+            for pre in preMap[crs]:
+                if not dfs(pre):
+                    return False
+            visiting.remove(crs)
+            preMap[crs] = []
+            return True
+
+        for c in range(numCourses):
+            if not dfs(c):
+                return False
+        return True
