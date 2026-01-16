0e4e4ce1a4649ebe5a6da70326791ce5dc5d9cbc
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-16 09:03:43 -0800

    Create BestTimeToBuyAndSellStock.py

---
+++ CodingPractice/BestTimeToBuyAndSellStock.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
