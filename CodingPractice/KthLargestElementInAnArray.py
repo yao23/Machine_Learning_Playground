e6816f348304bf016e3501b5a8fb344f593c1313
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-17 10:38:32 -0800

    Create KthLargestElementInAnArray.py

---
+++ CodingPractice/KthLargestElementInAnArray.py
@@ -0,0 +1,9 @@
+class Solution:
+    def findKthLargest(self, nums: List[int], k: int) -> int:
+        minHeap = []
+        for n in nums:
+            heapq.heappush(minHeap, n)
+            if len(minHeap) > k:
+                heapq.heappop(minHeap)
+
+        return minHeap[0]
