948106bb89fdc33499214a8a2a661183a2aed854
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-17 10:38:00 -0800

    Create SurroundedRegions.py

---
+++ CodingPractice/SurroundedRegions.py
@@ -0,0 +1,33 @@
+class Solution:
+    def solve(self, board: List[List[str]]) -> None:
+        ROWS, COLS = len(board), len(board[0])
+
+        def capture(r, c):
+            if (r < 0 or c < 0 or r == ROWS or
+                c == COLS or board[r][c] != "O"
+            ):
+                return
+            board[r][c] = "T"
+            capture(r + 1, c)
+            capture(r - 1, c)
+            capture(r, c + 1)
+            capture(r, c - 1)
+
+        for r in range(ROWS):
+            if board[r][0] == "O":
+                capture(r, 0)
+            if board[r][COLS - 1] == "O":
+                capture(r, COLS - 1)
+
+        for c in range(COLS):
+            if board[0][c] == "O":
+                capture(0, c)
+            if board[ROWS - 1][c] == "O":
+                capture(ROWS - 1, c)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if board[r][c] == "O":
+                    board[r][c] = "X"
+                elif board[r][c] == "T":
+                    board[r][c] = "O"
