a456220f2871e0e8ae82db734ff9226855db42ef
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-21 20:46:41 -0800

    Create CloneGraph.py

---
+++ CodingPractice/CloneGraph.py
@@ -0,0 +1,26 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val = 0, neighbors = None):
+        self.val = val
+        self.neighbors = neighbors if neighbors is not None else []
+"""
+
+class Solution:
+    def cloneGraph(self, node: Optional['Node']) -> Optional['Node']:
+        if not node:
+            return None
+
+        oldToNew = {}
+        oldToNew[node] = Node(node.val)
+        q = deque([node])
+
+        while q:
+            cur = q.popleft()
+            for nei in cur.neighbors:
+                if nei not in oldToNew:
+                    oldToNew[nei] = Node(nei.val)
+                    q.append(nei)
+                oldToNew[cur].neighbors.append(oldToNew[nei])
+
+        return oldToNew[node]
