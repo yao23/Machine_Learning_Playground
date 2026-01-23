f2d7cc5cdc9f1bcf3148393ec50758b1648e4bf2
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-23 09:35:53 -0800

    Create NumberOfIslands.py

---
+++ CodingPractice/NumberOfIslands.py
@@ -0,0 +1,23 @@
+class Solution:
+    def numIslands(self, grid: List[List[str]]) -> int:
+        directions = [[1, 0], [-1, 0], [0, 1], [0, -1]]
+        ROWS, COLS = len(grid), len(grid[0])
+        islands = 0
+
+        def dfs(r, c):
+            if (r < 0 or c < 0 or r >= ROWS or
+                c >= COLS or grid[r][c] == "0"
+            ):
+                return
+
+            grid[r][c] = "0"
+            for dr, dc in directions:
+                dfs(r + dr, c + dc)
+
+        for r in range(ROWS):
+            for c in range(COLS):
+                if grid[r][c] == "1":
+                    dfs(r, c)
+                    islands += 1
+
+        return islands
