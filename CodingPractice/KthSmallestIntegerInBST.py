9263161894f1db072475e663429f8f3ca267db55
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-24 09:25:40 -0800

    Create KthSmallestIntegerInBST.py

---
+++ CodingPractice/KthSmallestIntegerInBST.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
+        stack = []
+        cur = root
+
+        while stack or cur:
+            while cur:
+                stack.append(cur)
+                cur = cur.left
+            cur = stack.pop()
+            k -= 1
+            if k == 0:
+                return cur.val
+            cur = cur.right
