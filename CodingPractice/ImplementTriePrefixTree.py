858df401473ce2925abf543984b020d5fd572327
Yao Li <3107449+yao23@users.noreply.github.com>
2026-01-26 09:07:18 -0800

    Create ImplementTriePrefixTree.py

---
+++ CodingPractice/ImplementTriePrefixTree.py
@@ -0,0 +1,35 @@
+class TrieNode:
+    def __init__(self):
+        self.children = [None] * 26
+        self.endOfWord = False
+
+class PrefixTree:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        for c in word:
+            i = ord(c) - ord("a")
+            if cur.children[i] == None:
+                cur.children[i] = TrieNode()
+            cur = cur.children[i]
+        cur.endOfWord = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        for c in word:
+            i = ord(c) - ord("a")
+            if cur.children[i] == None:
+                return False
+            cur = cur.children[i]
+        return cur.endOfWord
+
+    def startsWith(self, prefix: str) -> bool:
+        cur = self.root
+        for c in prefix:
+            i = ord(c) - ord("a")
+            if cur.children[i] == None:
+                return False
+            cur = cur.children[i]
+        return True
