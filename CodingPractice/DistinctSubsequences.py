bf358e859e58e213191d44a64a68bd57f65eda70
Yao Li <3107449+yao23@users.noreply.github.com>
2026-02-02 09:12:34 -0800

    Implement Distinct Subsequences solution

---
+++ CodingPractice/DistinctSubsequences.py
@@ -0,0 +1,21 @@
+class Solution:
+    def numDistinct(self, s: str, t: str) -> int:
+        if len(t) > len(s):
+            return 0
+
+        dp = {}
+        def dfs(i, j):
+            if j == len(t):
+                return 1
+            if i == len(s):
+                return 0
+            if (i, j) in dp:
+                return dp[(i, j)]
+
+            res = dfs(i + 1, j)
+            if s[i] == t[j]:
+                res += dfs(i + 1, j + 1)
+            dp[(i, j)] = res
+            return res
+
+        return dfs(0, 0)
