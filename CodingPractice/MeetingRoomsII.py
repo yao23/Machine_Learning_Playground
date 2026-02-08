f1588963f55560d13d19ad890e396e685eb2e5fb
Yao Li <3107449+yao23@users.noreply.github.com>
2026-02-07 17:43:47 -0800

    Implement minMeetingRooms function for scheduling

---
+++ CodingPractice/MeetingRoomsII.py
@@ -0,0 +1,19 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key=lambda x: x.start)
+        min_heap = []
+
+        for interval in intervals:
+            if min_heap and min_heap[0] <= interval.start:
+                heapq.heappop(min_heap)
+            heapq.heappush(min_heap, interval.end)
+
+        return len(min_heap)
