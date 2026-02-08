e33abd931fa46602146dcb0933915320db0710b5
Yao Li <3107449+yao23@users.noreply.github.com>
2026-02-07 17:35:18 -0800

    Implement solution for stock buying with cooldown

---
+++ CodingPractice/BestTimeToBuyAndSellStockWithCooldown.py
@@ -0,0 +1,16 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+
+        def dfs(i, buying):
+            if i >= len(prices):
+                return 0
+
+            cooldown = dfs(i + 1, buying)
+            if buying:
+                buy = dfs(i + 1, not buying) - prices[i]
+                return max(buy, cooldown)
+            else:
+                sell = dfs(i + 2, not buying) + prices[i]
+                return max(sell, cooldown)
+
+        return dfs(0, True)
