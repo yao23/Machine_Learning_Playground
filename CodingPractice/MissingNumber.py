011f7faf4ad9f51d19bcab576f064944cc14de77
Yao Li <3107449+yao23@users.noreply.github.com>
2026-02-13 10:28:24 -0800

    Implement missing number algorithm in MissingNumber.py

---
+++ CodingPractice/MissingNumber.py
@@ -0,0 +1,7 @@
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        xorr = n
+        for i in range(n):
+            xorr ^= i ^ nums[i]
+        return xorr
