d9215a2c27b1b2ea2acae9b0d374a48f99923ebf
Yao Li <3107449+yao23@users.noreply.github.com>
2026-02-14 09:34:04 -0800

    Add CountingBits class with countBits method
    Implement a solution to count bits for numbers from 0 to n.

---
+++ CodingPractice/CountingBits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = [0] * (n + 1)
+        for i in range(1, n + 1):
+            num = i
+            while num != 0:
+                res[i] += 1
+                num &= (num - 1)
+        return res
